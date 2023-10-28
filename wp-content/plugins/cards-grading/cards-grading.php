08660a31597c9de7d7a81ac29887e8de910b6043
arhizsx <arhizsx@gmail.com>
2023-10-29 00:02:02 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -111,53 +111,6 @@
     public function load_scripts()
     { ?>
     
-    <script>
-
-        alert('test');
-        function showAddCardModal( what_type ){
-                
-            $(document).find(".dxmodal").modal("show");
-            $(document).find(".dxmodal").find("input[name='grading']").val(what_type);
-            
-        }
-
-
-
-        $(document).on("click", ".5star_btn", function(e){
-            e.preventDefault();
-
-            if($(this).hasClass("add_card")){			
-
-                switch( $(this).data("type") ){
-
-                    case "psa-value_bulk":
-                        showAddCardModal("psa-value_bulk");
-                        break;
-
-                    case "psa-value_bulk":
-                        showAddCardModal("psa-value_plus");
-                        break;
-
-                    case "psa-regular":
-                        showAddCardModal("psa-regular");
-                        break;
-
-                    case "psa-express":
-                        showAddCardModal("psa-express");
-                        break;
-
-                    case "psa-super_express":
-                        showAddCardModal("psa-super_express");
-                        break;
-
-                    default:
-
-                }
-            }
-
-        });
-    </script>
-
     <?php }
 
 
