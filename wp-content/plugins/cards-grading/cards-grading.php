2927d59456df014b6e827fbb4b5aeb1387020de4
arhizsx <arhizsx@gmail.com>
2023-10-28 23:46:04 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -95,6 +95,7 @@
         );
 
         wp_enqueue_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"');
+        wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ),'',true );
         wp_enqueue_script( 'bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', array( 'jquery' ),'',true );
         
     }
