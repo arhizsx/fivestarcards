ff8c12840f203143ef2c6a1ac18973ca01ce9075
arhizsx <arhizsx@gmail.com>
2023-10-29 07:49:54 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -1,1 +1,47 @@
-alert("Divi Sucks");
+function showAddCardModal( what_type ){
+		
+    $(document).find(".dxmodal").modal("show");
+    $(document).find(".dxmodal").find("input[name='grading']").val(what_type);
+    
+}
+
+jQuery(document).ready(function(){
+    alert("Document Loaded");
+});
+
+$(document).on("click", ".5star_btn", function(e){
+
+    e.preventDefault();
+
+    alert("test");
+
+    if($(this).hasClass("add_card")){			
+
+        switch( jQuery(this).data("type") ){
+
+            case "psa-value_bulk":
+                showAddCardModal("psa-value_bulk");
+                break;
+
+            case "psa-value_bulk":
+                showAddCardModal("psa-value_plus");
+                break;
+
+            case "psa-regular":
+                showAddCardModal("psa-regular");
+                break;
+
+            case "psa-express":
+                showAddCardModal("psa-express");
+                break;
+
+            case "psa-super_express":
+                showAddCardModal("psa-super_express");
+                break;
+
+            default:
+
+        }
+    }
+
+});
