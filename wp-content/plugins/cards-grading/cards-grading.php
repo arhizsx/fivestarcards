4ce0fd395f7d92daea1725b9411409d8b7eb9bb5
arhizsx <arhizsx@gmail.com>
2023-10-28 19:27:08 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -98,52 +98,8 @@
 
     function cards_grading_shortcodes() 
     {?>
-        <table class='table 5star_logged_cards'>
-            <thead>
-                <tr>
-                <th>Quantity</th>
-                <th>Year</th>
-                <th>Brand</th>
-                <th>Card #</th>
-                <th>Player Name</th>
-                <th>Attribute S/N</th>
-                <th>Declared Value</th>
-                <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>1</td>
-                <td>1956</td>
-                <td>Topps</td>
-                <td>FTAAP321</td>
-                <td>Test Player</td>
-                <td>232321</td>
-                <td>20000</td>
-                <td></td>
-                </tr>
-                <tr>
-                <td>1</td>
-                <td>1956</td>
-                <td>Topps</td>
-                <td>FTAAP321</td>
-                <td>Test Player</td>
-                <td>232321</td>
-                <td>20000</td>
-                <td></td>
-                </tr>
-                <tr>
-                <td>1</td>
-                <td>1956</td>
-                <td>Topps</td>
-                <td>FTAAP321</td>
-                <td>Test Player</td>
-                <td>232321</td>
-                <td>20000</td>
-                <td></td>
-                </tr>
-            </tbody>
-        </table>
+
+        shortcode
 
     <?php }
 
