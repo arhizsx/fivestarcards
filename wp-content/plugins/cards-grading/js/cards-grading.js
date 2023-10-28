31acf513725cf8c0b3e7b3756f4fe4dc02b57a01
arhizsx <arhizsx@gmail.com>
2023-10-28 23:16:00 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -5,25 +5,38 @@ function showAddCardModal( what_type ){
     
 }
 
+
+
 $(document).on("click", ".5star_btn", function(e){
     e.preventDefault();
-    
+
     if($(this).hasClass("add_card")){			
-        if( $(this).hasClass("psa_value_bulk") ){				
-            showAddCardModal("psa_value_bulk");
-        }			
-        else if( $(this).hasClass("psa_value_plus") ){				
-            showAddCardModal("psa_value_plus");
-        }			
-        else if( $(this).hasClass("psa_regular") ){				
-            showAddCardModal("psa_regular");
-        }			
-        else if( $(this).hasClass("psa_express") ){				
-            showAddCardModal("psa_express");
-        }			
-        else if( $(this).hasClass("psa_super_express") ){	
-            showAddCardModal("psa_super_express");
-        }			
+
+        switch( $(this).data("type") ){
+
+            case "psa-value_bulk":
+                showAddCardModal("psa-value_bulk");
+                break;
+
+            case "psa-value_bulk":
+                showAddCardModal("psa-value_plus");
+                break;
+
+            case "psa-regular":
+                showAddCardModal("psa-regular");
+                break;
+
+            case "psa-express":
+                showAddCardModal("psa-express");
+                break;
+
+            case "psa-super_express":
+                showAddCardModal("psa-super_express");
+                break;
+
+            default:
+
+        }
     }
-    
+
 });
