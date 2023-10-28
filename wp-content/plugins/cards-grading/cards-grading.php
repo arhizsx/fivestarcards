9ac8ce02ca75f978ae80f6a7dea3011ac2abbdee
arhizsx <arhizsx@gmail.com>
2023-10-29 07:09:05 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -27,7 +27,7 @@
     public function __construct() 
     {
         // Add Plugin Menu
-        add_action('admin_menu', array($this, 'plugin_menu'));
+        // add_action('admin_menu', array($this, 'plugin_menu'));
 
         // Create Custom Post Type
         add_action('init', array($this, 'create_custom_post_type') );        
@@ -35,7 +35,7 @@
 
         // Add Assets
         add_action('wp_enqueue_scripts', array( $this, 'load_assets') );
-        add_action( 'init', array( $this, 'register_plugin_styles' ) ); // front and admin
+        // add_action( 'init', array( $this, 'register_plugin_styles' ) ); // front and admin
 
         // Add Shortcodes
         add_shortcode('cards-grading', array( $this, 'cards_grading_shortcodes' ));
@@ -44,23 +44,10 @@
         add_action('wp_footer', array($this, 'load_scripts'));
     }
 
-    public function plugin_menu()
-    {
-        add_menu_page(
-            'Cards Grading',
-            'Cards Grading',
-            'manage_options',
-            plugin_dir_path( __FILE__ ) . 'admin/dashboard.php',
-            null,
-            'dashicons-media-spreadsheet'
-        );
-    }
-
-
     public function create_custom_post_type()
     {
         $args = array(
-            'public' => false,
+            'public' => true,
             'has_archive' => false,
             'supports' => array('title'),
             'exclude_from_search' => true,
@@ -77,11 +64,25 @@
 
     }
 
+    // public function plugin_menu()
+    // {
+    //     add_menu_page(
+    //         'Cards Grading',
+    //         'Cards Grading',
+    //         'manage_options',
+    //         plugin_dir_path( __FILE__ ) . 'admin/dashboard.php',
+    //         null,
+    //         'dashicons-media-spreadsheet'
+    //     );
+    // }
 
-    public function register_assets(){
-		wp_register_style( 'cards-grading', plugin_dir_url(__FILE__) . 'css/cards-grading.css' );        
-		wp_register_script( 'cards-grading', plugin_dir_url(__FILE__) . 'js/cards-grading.js' );        
-    }
+
+
+    // public function register_assets(){
+	// 	wp_register_style( 'cards-grading', plugin_dir_url(__FILE__) . 'css/cards-grading.css' );        
+	// 	wp_register_script( 'cards-grading', plugin_dir_url(__FILE__) . 'js/cards-grading.js' );        
+    // }
+
     public function load_assets() {
 
         wp_enqueue_style(
@@ -115,48 +116,7 @@
     <script>
 
         alert('test');
-        function showAddCardModal( what_type ){
-                
-            $(document).find(".dxmodal").modal("show");
-            $(document).find(".dxmodal").find("input[name='grading']").val(what_type);
-            
-        }
 
-
-
-        $(document).on("click", ".5star_btn", function(e){
-            e.preventDefault();
-
-            if($(this).hasClass("add_card")){			
-
-                switch( $(this).data("type") ){
-
-                    case "psa-value_bulk":
-                        showAddCardModal("psa-value_bulk");
-                        break;
-
-                    case "psa-value_bulk":
-                        showAddCardModal("psa-value_plus");
-                        break;
-
-                    case "psa-regular":
-                        showAddCardModal("psa-regular");
-                        break;
-
-                    case "psa-express":
-                        showAddCardModal("psa-express");
-                        break;
-
-                    case "psa-super_express":
-                        showAddCardModal("psa-super_express");
-                        break;
-
-                    default:
-
-                }
-            }
-
-        });
     </script>
 
     <?php }
