cf82a51f5852aba5a9c785b3227a231159b1c577
arhizsx <arhizsx@gmail.com>
2023-10-28 17:48:01 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/dashboard.php
+++ wp-content/plugins/cards-grading/admin/dashboard.php
@@ -1,2 +1,12 @@
-<H1>Cards Grading - Five Star Cards</H1>
+<div class="container">
+    <H1>Cards Grading - Five Star Cards</H1>
+    <div class="row">
+        <div class="col">
+            X
+        </div>
+        <div class="col">
+            Y
+        </div>
+    </div>
+</div>
 
