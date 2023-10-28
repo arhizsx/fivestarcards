a4e477608a1c408ba9956077cf57ea0399c2f083
arhizsx <arhizsx@gmail.com>
2023-10-28 08:45:35 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -15,3 +15,40 @@
  * Text Domain:       cards-grading
  * Domain Path:       /languages
  */
+
+ if( !defined('ABSPATH') )
+ {
+    echo 'You are not allowed';
+    exit;
+ }
+
+ class CardsGrading {
+
+    public function __construct() 
+    {
+        add_action('init', array($this, 'create_custom_post_type') );
+    }
+
+    public function create_custom_post_type()
+    {
+        $args = array(
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title'),
+            'exclude_from_search' => true,
+            'publicly_queryable' => false,
+            'capability' => 'manage_options',
+            'labels' => array(
+                'name' => 'Cards Grading',
+                'singular_name' => 'Card Grading Entry'
+            ),
+            'menu_icon' => 'dashicons-media-text'
+        );
+
+        register_post_type("cards-grading", $args);
+
+    }
+
+ }
+
+ new CardsGrading;
