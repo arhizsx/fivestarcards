97b23ede034a45f6261f9aa5b6cce396c72e7dd2
arhizsx <arhizsx@gmail.com>
2023-10-29 08:32:26 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -12,33 +12,52 @@ $(document).on("click", ".5star_btn", function(e){
 
     e.preventDefault();
 
-    if($(this).data("action")== "add_card" ){			
+    switch($(this).data("action")== "add_card" ){		
+        
+        case "add_card" :
 
-        switch( $(this).data("type") ){
+            switch( $(this).data("type") ){
 
-            case "psa-value_bulk":
-                showAddCardModal("psa-value_bulk", 19, 499);
-                break;
+                case "psa-value_bulk":
+                    showAddCardModal("psa-value_bulk", 19, 499);
+                    break;
 
-            case "psa-value_bulk":
-                showAddCardModal("psa-value_plus", 40, 499);
-                break;
+                case "psa-value_bulk":
+                    showAddCardModal("psa-value_plus", 40, 499);
+                    break;
 
-            case "psa-regular":
-                showAddCardModal("psa-regular", 75, 1499);
-                break;
+                case "psa-regular":
+                    showAddCardModal("psa-regular", 75, 1499);
+                    break;
 
-            case "psa-express":
-                showAddCardModal("psa-express", 165, 2499);
-                break;
+                case "psa-express":
+                    showAddCardModal("psa-express", 165, 2499);
+                    break;
 
-            case "psa-super_express":
-                showAddCardModal("psa-super_express", 330, 4999);
-                break;
+                case "psa-super_express":
+                    showAddCardModal("psa-super_express", 330, 4999);
+                    break;
 
-            default:
+                default:
 
-        }
+            }
+
+            break;
+
+        case "clear_table" :
+
+            console.log("Clear Table");
+
+            break;
+
+        case "checkout" :
+
+            console.log("Checkout");
+
+            break;
+
+        default:
+            console.log("Button not configured");
     }
 
 });
