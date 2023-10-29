591096ffc3b38fbaae95812355c7e8414d33cdd6
arhizsx <arhizsx@gmail.com>
2023-10-29 08:23:53 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -17,23 +17,23 @@ $(document).on("click", ".5star_btn", function(e){
         switch( $(this).data("type") ){
 
             case "psa-value_bulk":
-                showAddCardModal("psa-value_bulk", 19, 100);
+                showAddCardModal("psa-value_bulk", 19, 499);
                 break;
 
             case "psa-value_bulk":
-                showAddCardModal("psa-value_plus", 19, 100);
+                showAddCardModal("psa-value_plus", 40, 499);
                 break;
 
             case "psa-regular":
-                showAddCardModal("psa-regular", 19, 100);
+                showAddCardModal("psa-regular", 75, 1499);
                 break;
 
             case "psa-express":
-                showAddCardModal("psa-express", 19, 100);
+                showAddCardModal("psa-express", 165, 2499);
                 break;
 
             case "psa-super_express":
-                showAddCardModal("psa-super_express", 19, 100);
+                showAddCardModal("psa-super_express", 330, 4999);
                 break;
 
             default:
