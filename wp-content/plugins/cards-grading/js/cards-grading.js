4d0f8a79e1b418d0d6073736f9a9a7ba9dbcbce7
arhizsx <arhizsx@gmail.com>
2023-10-29 21:05:20 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -12,6 +12,97 @@ function showAddCardModal( what_type, per_card, max_dv ){
 
 }
 
+function addCardToTable(card){
+
+    if( checkIfAddIsStillValid( card ) )
+    {
+    
+        var card_total_charge = parseFloat(card["quantity"]) * parseFloat(card["per_card"]);
+        var card_total_dv = parseFloat(card["quantity"]) * parseFloat(card["dv"]);
+    
+        if( $(document).find(".5star_logged_cards tbody tr td:first-child").text() == "Empty"){
+            $(document).find(".5star_logged_cards tbody").empty();
+        }
+
+        $(document).find(".5star_logged_cards tbody").append(
+            "<tr>" +
+                "<td>" + card["quantity"] + "</td>" +
+                "<td>" + card["year"] + "</td>" +
+                "<td>" + card["brand"] + "</td>" +
+                "<td>" + card["card_number"] + "</td>" +
+                "<td>" + card["player"] + "</td>" +
+                "<td>" + card["attribute"] + "</td>" +
+                "<td><span class='dollar'>" + parseFloat(card["dv"]).toFixed(2) + "</span></td>" +
+                "<td><span class='dollar'>" + card_total_dv.toFixed(2) + "</span></td>" +
+                "<td><span class='dollar'>" + card_total_charge.toFixed(2) + "</span></td>" +
+                "<td>Remove</td>" +
+            "</tr>"
+        );
+    
+        clearModalForm();  
+        setTotals(card_total_dv, card_total_charge)  
+
+        var nonce = $(document).find(".5star_logged_cards").data("nonce");
+        var url = $(document).find(".5star_logged_cards").data("endpoint");
+
+        $.ajax({
+            method: 'post',
+            url: '',
+            headers: {'X-WP-Nonce': nonce },
+            data: card
+        });
+
+
+    } else {
+
+        $(document).find("div#add_card_form_box").addClass("d-none");
+        $(document).find("div#maxed-out").removeClass("d-none");
+        $(document).find("div#maxed-out").find(".message").html(
+            "<strong>Maximum allowed DV is only " + ( parseFloat(card["max_dv"]) - 1)  + "</strong>"
+        );
+
+        console.log( "Max DV Reached" );
+
+    }
+    
+
+}
+
+function setTotals( total_dv, total_charge ){
+
+    current_dv = parseFloat( $(document).find("#total_dv").text().replace("$",""));
+    current_charge = parseFloat($(document).find("#total_charges").text().replace("$",""));
+
+    new_total_dv = total_dv + current_dv;
+    new_total_charge = total_charge + current_charge;
+    new_grand_total = new_total_dv + new_total_charge;
+
+    $(document).find("#total_dv").text( "$" + new_total_dv.toFixed(2) );
+    $(document).find("#total_charges").text( "$" + new_total_charge.toFixed(2) );
+    $(document).find("#grand_total").text( "$" + new_grand_total.toFixed(2) );
+
+}
+
+function checkIfAddIsStillValid( card ){
+
+    var card_total_dv = parseFloat(card["quantity"]) * parseFloat(card["dv"]);
+
+    var current_total_dv = parseFloat( $(document).find("#total_dv").text().replace("$","") );
+    var max_dv = card["max_dv"];
+
+    if( current_total_dv + card_total_dv < max_dv ) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function clearModalForm(){
+    $(document).find(".dxmodal").find('#add_card_form *').filter(':input:visible:enabled').each(function(k, v){
+        $(v).val("");
+    });    
+}
 
 $(document).on("click", ".5star_btn", function(e){
 
