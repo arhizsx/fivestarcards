73fb541b897eb9ac4e89ba900cc9829bcd6cc89b
arhizsx <arhizsx@gmail.com>
2023-10-30 07:49:38 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/modal.php
+++ wp-content/plugins/cards-grading/admin/modal.php
@@ -124,7 +124,7 @@ $posts = get_posts($args);
         <?php 
             if( $posts ){
                 foreach($posts as $post){
-                    print_r($post);
+                    print_r($post->ID);
         ?>
         <tr>
             <td>
