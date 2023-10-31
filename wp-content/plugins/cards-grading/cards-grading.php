b59a79d496929aaf9e952588e79cdef7ff1fba0d
arhizsx <arhizsx@gmail.com>
2023-10-31 11:10:04 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -159,42 +159,7 @@
         return $post_id;
     }
 
-    public function doClearTable( $params){
 
-        $user_id = get_current_user_id();
-        
-
-        $args = array(
-            'meta_query' => array(
-                'relations' =>  'AND',    
-                array(
-                    'key' => 'grading',
-                    'value' => $params['type']
-                ),
-                array(
-                    'key' => 'user_id',
-                    'value' => $user_id
-                ),
-                array(
-                    'key' => 'status',
-                    'value' => 'pending'
-                )
-            ),
-            'post_type' => 'cards-grading-card',
-            'posts_per_page' => -1
-        );
-        
-        $posts = get_posts($args);
-
-
-        foreach($posts as $post)
-        {
-            wp_delete_post( $post->ID, true );
-        }
-
-        return "Deleted pending cards";
-        
-    }
 
     public function doCheckout($params){
 
@@ -212,12 +177,13 @@
 
         if($params["action"] == "clear"){
 
-            doClearTable($params);
+            return "Delete pending cards";
+
 
         }
         elseif($params["action"] == "checkout"){
 
-            doClearTable($params);
+            return "Checkout pending cards";
 
         }
 
