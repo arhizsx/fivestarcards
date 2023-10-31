ff1f3caeece03cb7081f2c7b5be53e0911281960
arhizsx <arhizsx@gmail.com>
2023-10-31 12:24:03 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -42,9 +42,9 @@
         add_action("rest_api_init", array($this, 'register_endpoint'));
 
 
-        add_filter( 'manage_cards-grading-card_posts_columns', 'add_cards_grading_card_columns');
+        add_filter( 'manage_cards-grading-card_posts_columns', array($this, 'add_cards_grading_card_columns'));
 
-    } 
+    }
 
 
     public function create_custom_post_type()
