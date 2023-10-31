135ca213f0465608372dc1602d8035887dc98515
arhizsx <arhizsx@gmail.com>
2023-10-31 12:58:54 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -88,7 +88,7 @@
             $user_id = get_post_meta( $post_id , 'user_id' , true );
             $user = get_user_by( "id", $user_id );
 
-            echo $user->email;
+            echo $user_id;
 
             break;
           case 'grading':
