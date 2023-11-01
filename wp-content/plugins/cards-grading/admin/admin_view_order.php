6fa7e44050e4081ffe30009a6d26834dd267d14b
arhizsx <arhizsx@gmail.com>
2023-11-01 22:12:36 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/admin_view_order.php
@@ -173,33 +173,11 @@ foreach($posts as $post)
         <?php if( $posts ){ ?>
         <div class="row bottom_buttons">
             <div class="col-lg-12 text-end border-top pt-2 ">
-                <?php if( $checkout_meta["status"][0] == "To Ship" ) { ?>
-                <button class='5star_btn btn btn-primary' data-action="shipped">
-                    Items Shipped
+                <?php if( $checkout_meta["status"][0] == "Shipped" ) { ?>
+                <button class='5star_btn btn btn-primary' data-action="order_received">
+                    Received
                 </button>      
                 <?php } ?>
-            </div>
-        </div>
-        <?php } ?>
-        <?php if( $checkout_meta["status"][0] == "To Ship" ) { ?>
-        <div class="row mx-5">
-            <div class="col-lg-12">
-                <H3 style="color: black !important;">Ship Your Items To</H3>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <div><strong>USPS</strong></div>
-                <div>Matt Sellers</div>
-                <div>PO Box 263</div>
-                <div>Hartland, WI 53029</div>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <div><strong>FedEx / UPS / DHL</strong></div>
-                <div>PMB 394</div>
-                <div>1256 Capitol Drive - Suite 700</div>
-                <div>Pewaukee, WI 53072</div>
-            </div>
-            <div class="col-lg-4">
-
             </div>
         </div>
         <?php } ?>
