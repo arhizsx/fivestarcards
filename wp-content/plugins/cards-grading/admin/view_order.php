5f5e34a504a5100a17e289b369459791d8c41395
arhizsx <arhizsx@gmail.com>
2023-11-01 14:46:36 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/view_order.php
+++ wp-content/plugins/cards-grading/admin/view_order.php
@@ -179,7 +179,7 @@ foreach($posts as $post)
         </div>
         <?php } ?>
         <?php if( $checkout_meta["status"][0] == "To Ship" ) { ?>
-        <div class="row mx-5" style="margin-top: -30px;">
+        <div class="row mx-5">
             <div class="col-lg-12">
                 <H3 style="color: black !important;">Ship Your Items To</H3>
             </div>
