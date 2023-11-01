8803cb46734ce6de7cdd0c0ecd9c9f4b2c2b43bd
arhizsx <arhizsx@gmail.com>
2023-11-01 09:51:25 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/my_orders.php
+++ wp-content/plugins/cards-grading/admin/my_orders.php
@@ -39,10 +39,11 @@ $posts = get_posts($args);
                             $meta = get_post_meta($post->ID);
 
                             $date_format = get_option( 'date_format' );
+                            $time_format = get_option( 'time_format' );
                                                         
                 ?>
                 <tr class="card-row" data-post_id="<?php echo $post->ID; ?>">
-                    <td><?php echo get_the_date( $date_format, $post->ID ) ?><br><span style='font-size:.7em !important;'><?php echo get_the_time( $date_format, $post->ID ); ?></span></td>
+                    <td><?php echo get_the_date( $date_format, $post->ID ) ?><br><span style='font-size:.7em !important;'><?php echo get_the_time( $time_format, $post->ID ); ?></span></td>
                     <td><?php echo $meta["order_number"][0]; ?></td>
                     <td><?php echo $meta["service_type"][0]; ?><br><span style='font-size:.7em !important;'><?php echo $meta["grading_type"][0]; ?></span></td>
                     <td><?php echo $meta["status"][0]; ?></td>
