f435697a54dc4a18bf2686b282fe02423d5fc0c9
arhizsx <arhizsx@gmail.com>
2023-11-02 10:34:19 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -299,7 +299,7 @@ function cardAction(action, value, post_id, parent_element ){
 
                 if( value == "Received" ){
                     $(parent_element).closest("table").find("tr").each( function(k, v){
-                            console.log( v.find("td:eq(6)").text() );
+                            console.log( $(v).find("td:eq(6)").text() );
                     });
                 }
                 else if( value == "Not Available" ){
