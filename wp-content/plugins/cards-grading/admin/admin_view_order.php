ce3e88dba5fb75f2f906935b5add515a3b088d7b
arhizsx <arhizsx@gmail.com>
2023-11-02 08:07:30 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/admin_view_order.php
@@ -107,7 +107,6 @@ foreach($posts as $post)
         <table class='table 5star_logged_cards table-bordered table-striped' data-endpoint="<?php echo get_rest_url(null, "cards-grading/v1/order-action") ?>" data-nonce="<?php echo wp_create_nonce("wp_rest"); ?>">
             <thead>
                 <tr>
-                <th>Qty</th>
                 <th>Year</th>
                 <th>Brand</th>
                 <th>Card #</th>
@@ -135,11 +134,11 @@ foreach($posts as $post)
 
                 ?>
                 <tr class="card-row" data-post_id="<?php echo $post->ID; ?>" data-card='<?php echo json_encode($card) ?>'>
-                    <td><?php echo $card["quantity"]; ?></td>
                     <td><?php echo $card["year"]; ?></td>
                     <td><?php echo $card["brand"]; ?></td>
                     <td><?php echo $card["card_number"]; ?><br><small><?php echo $card["attribute"]; ?></small></td>
                     <td><?php echo $card["player"]; ?></td>
+                    <td><?php echo $meta["status"][0]; ?></td>
                     <td class='text-end'><?php echo "$" . number_format((float)$card["dv"], 2, '.', ''); ?></td>
                     <td class='text-end'><?php echo "$" . number_format((float) $card_total_dv, 2, '.', ''); ?></td>
                     <td class='text-end'><?php echo "$" . number_format((float) $card_grading_charge, 2, '.', ''); ?></td>
