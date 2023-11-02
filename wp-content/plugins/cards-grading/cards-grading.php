e0c8898f96762123a3c75a89c0f5ea099b2eaa68
arhizsx <arhizsx@gmail.com>
2023-11-02 14:24:53 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -259,7 +259,7 @@
             'cards-grading',
             plugin_dir_url(__FILE__) . 'js/cards-grading.js',
             array('jquery'),
-            27,
+            28,
             true
         );
 
