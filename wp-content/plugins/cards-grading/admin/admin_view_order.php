d51e7ef49cd73e14fd77969205029efc8e651c9a
arhizsx <arhizsx@gmail.com>
2023-11-02 18:41:10 +0800

    Cards Grading Plugin

--- wp-content/plugins/cards-grading/admin/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/admin_view_order.php
@@ -252,7 +252,7 @@ $processed_status = array("Processing Order", "Cards Graded");
                     <td><?php echo $card["player"]; ?></td>
                     <td class=".card_status"><?php echo $meta["status"][0]; ?></td>
                     <?php if( in_array( $checkout_meta["status"][0], $processed_status ) ){ ?>
-                    <td class=".grade"><?php echo $meta["grade"]; ?></td>
+                    <td class=".grade"><?php echo $meta["grade"][0]; ?></td>
                     <?php }?>
                     <td class='text-end'><?php echo "$" . number_format((float)$card["dv"], 2, '.', ''); ?></td>
                     <td class='text-end'><?php echo "$" . number_format((float) $card_grading_charge, 2, '.', ''); ?></td>
