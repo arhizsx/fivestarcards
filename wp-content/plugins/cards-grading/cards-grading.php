556868f61aa483ba322d19b34d2c55fcd7900de8
arhizsx <arhizsx@gmail.com>
2023-11-15 10:11:42 +0800

    update js

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -260,7 +260,7 @@
             'cards-grading',
             plugin_dir_url(__FILE__) . 'js/cards-grading.js',
             array('jquery'),
-            35,
+            36,
             true
         );
 
