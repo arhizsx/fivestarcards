ef0fa0774b93bc0381a6d596487737083b775286
arhizsx <arhizsx@gmail.com>
2023-12-15 04:39:27 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/admin_view_order.php
@@ -49,6 +49,7 @@ $processed_status = array("Processing Order", "Cards Graded");
                 <div class="col-xl-12 mb-3">
                     <div class='order-label'><?php echo $params['title'] ?></div>
                     <div class='order-data'><?php echo $params['order_number'] ?></div>
+                    <div class='order-data'><?php echo $checkout_meta["status"][0] ?></div>
                 </div>
                 <div class="col-xl-12 mb-3">
                     <div class='order-label'>User</div>
