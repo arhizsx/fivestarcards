68a1d2760f174cc8db188439b22cd43f9e0fd294
arhizsx <arhizsx@gmail.com>
2023-12-15 18:36:25 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/my_for_payment.php
+++ wp-content/plugins/cards-grading/admin/my_for_payment.php
@@ -25,7 +25,7 @@ $posts = get_posts($args);
 ?>
 
 <div class="m-0 p-0">
-    <H1 style="color: black;">Consigned Orders</H1>
+    <H1 style="color: black;">For Payment</H1>
     <div class="table-responsive">    
         <table class='table 5star_my_orders table-bordered table-striped'>
             <thead>
