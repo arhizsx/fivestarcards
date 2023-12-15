57e2d58ec0d5e8e92a5c966cc17b6b2762c24bfb
arhizsx <arhizsx@gmail.com>
2023-12-15 08:24:47 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -701,6 +701,28 @@ $(document).on("click",".card-row", function(e){
 
 });
 
+$(document).on("click",".admin-card-row", function(e){
+
+    $(document).find(".view_card").find("div#view_card_form_box").removeClass("d-none");
+    $(document).find(".view_card").appendTo('body').modal("show");
+
+    $(document).find("input[name='quantity']").val($(this).data("card").quantity);
+    $(document).find("input[name='year']").val($(this).data("card").year);
+    $(document).find("input[name='brand']").val($(this).data("card").brand);
+    $(document).find("input[name='card_number']").val($(this).data("card").card_number);
+    $(document).find("input[name='player']").val($(this).data("card").player);
+    $(document).find("input[name='attribute']").val($(this).data("card").attribute);
+    $(document).find("input[name='dv']").val($(this).data("card").dv);
+    $(document).find("input[name='per_card']").val($(this).data("card").per_card);
+    $(document).find("input[name='grading']").val($(this).data("card").grading);
+    $(document).find("input[name='max_dv']").val($(this).data("card").max_dv);
+    $(document).find("input[name='post_id']").val($(this).data("post_id"));
+    $(document).find("input[name='card']").val( JSON.stringify($(this).data("card")) );
+
+
+});
+
+
 $(document).on("click",".my-order-row", function(e){
 
     window.location.href = "/view-order?id=" + $(this).data("post_id") ;
