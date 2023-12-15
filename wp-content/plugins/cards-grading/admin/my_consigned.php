84ab9bd003f7b7f589779bc614d72ae0bdad8f8b
arhizsx <arhizsx@gmail.com>
2023-12-16 06:11:42 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/my_consigned.php
+++ wp-content/plugins/cards-grading/admin/my_consigned.php
@@ -11,7 +11,7 @@ $args = array(
         ),
         array(
             'key' => 'status',
-            'value' => array("Order To Pay", "Consignment Paid" ),
+            'value' => array("Order To Pay", "Consignment Paid", "Shipped", "To Ship" ),
             'compare' => 'NOT IN'
         )
 
