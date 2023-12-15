5f172188f4a02f1ac19963805924eaf4d49ee452
arhizsx <arhizsx@gmail.com>
2023-12-15 18:00:51 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/admin_view_consignment.php
+++ wp-content/plugins/cards-grading/admin/admin_view_consignment.php
@@ -162,7 +162,7 @@ $payment_status = array( "Ready For Payment" );
                             $card_total_dv = $card["dv"] * $card["quantity"];
                             $card_grading_charge = $card["per_card"] * $card["quantity"];
 
-                            if( $meta["status"][0] ==  "To Pay - Grade Only" ){
+                            if( in_array($meta["status"][0], array("To Pay - Grade Only","Deducted - Grade Only")) ){
                                 $grading_charge = $grading_charge + $card_grading_charge;
                             }
 
