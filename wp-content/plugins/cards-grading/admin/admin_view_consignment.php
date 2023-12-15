0b10235c060cf7e8ec54fe166c155b1c5ba334d6
arhizsx <arhizsx@gmail.com>
2023-12-15 16:25:42 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/admin_view_consignment.php
+++ wp-content/plugins/cards-grading/admin/admin_view_consignment.php
@@ -266,6 +266,14 @@ $payment_status = array( "Ready For Payment" );
                         $<?php echo number_format((float)$grading_charge, 2, '.', ''); ?>
                     </div>
                 </div>
+                <div class="row mb-2">
+                    <div class="col text-end">
+                       To Pay
+                    </div>
+                    <div class="col text-end"  id="grading_charges">
+                        $<?php echo number_format((float) $to_receive_total - $grading_charge, 2, '.', ''); ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
