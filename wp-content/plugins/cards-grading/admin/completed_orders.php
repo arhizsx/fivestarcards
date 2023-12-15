4a6d09ba77145f6a3187e15a2e26e67f29a1e332
arhizsx <arhizsx@gmail.com>
2023-12-15 17:34:07 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/completed_orders.php
+++ wp-content/plugins/cards-grading/admin/completed_orders.php
@@ -90,7 +90,7 @@ $posts = get_posts($args);
                             $total_to_pay = $total_to_receive - $total_grading_charge;
                                                         
                 ?>
-                            <tr class="admin-consigned-row" data-post_id="<?php echo $post->ID; ?>">
+                            <tr class="admin-completed-row" data-post_id="<?php echo $post->ID; ?>">
                                 <td><?php echo get_the_date( $date_format, $post->ID ) ?><br><span style='font-size:.7em !important;'><?php echo get_the_time( $time_format, $post->ID ); ?></span></td>
                                 <td><?php echo $user->display_name; ?></td>
                                 <td><?php echo $meta["order_number"][0]; ?></td>
