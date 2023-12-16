9c8a88223ee04ad32e1f37397c4cf9c54833f68f
arhizsx <arhizsx@gmail.com>
2023-12-16 08:57:15 +0800

    Consigned Orders

--- wp-content/plugins/cards-grading/admin/view_order.php
+++ wp-content/plugins/cards-grading/admin/view_order.php
@@ -49,7 +49,7 @@ foreach($posts as $post)
 $processed_status = array("Cards Graded");
 
 
-$consignment_status = array("Order Partial Consignment", "Order Consigned", "Ready For Payment");
+$consignment_status = array("Order Partial Consignment", "Order Consigned", "Ready For Payment", "Consignment Paid");
 
 ?>
 
