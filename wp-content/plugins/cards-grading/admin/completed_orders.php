9cf5e378941b8fee5c1456a8c57ae6e600746f49
arhizsx <arhizsx@gmail.com>
2023-12-26 13:08:32 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/completed_orders.php
+++ wp-content/plugins/cards-grading/admin/completed_orders.php
@@ -6,7 +6,7 @@ $args = array(
     'meta_query' => array(
         array(
             'key' => 'status',
-            'value' => array("Consignment Paid", "Grading Paid", "Order Cancelled", "Order Paid"),
+            'value' => array("Consignment Paid", "Grading Paid", "Order Paid"),
             'compare' => 'IN'
         )
     ),
