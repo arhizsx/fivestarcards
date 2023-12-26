a305a2ffb2ee041cbeafe5deb913b3fa90a2aad4
arhizsx <arhizsx@gmail.com>
2023-12-26 14:59:48 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/admin_view_order.php
@@ -80,6 +80,10 @@ $processed_status = array("Processing Order", "Cards Graded");
                     <div class='order-data'><?php echo $cards_count; ?></div>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                    <div class='order-label'>Consigned Cards</div>
+                    <div class='order-data'>0</div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
                     <div class='order-label'>Carrier</div>
                     <div class='order-data'><?php echo $checkout_meta["carrier"][0] ?></div>
                 </div>
