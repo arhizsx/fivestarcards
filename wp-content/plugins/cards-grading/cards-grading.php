5061ce5d8f843c2e2f09d0ce173b6050fb402a49
arhizsx <arhizsx@gmail.com>
2023-12-27 07:12:11 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -89,7 +89,7 @@
             'cards-grading',
             plugin_dir_url(__FILE__) . 'js/cards-grading.js',
             array('jquery'),
-            103,
+            104,
             true
         );
 
