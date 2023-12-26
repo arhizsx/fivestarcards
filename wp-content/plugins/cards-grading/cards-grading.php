950fbd0aa90670344aa5a7fcebaa0d9260fabddf
arhizsx <arhizsx@gmail.com>
2023-12-26 15:12:31 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -56,6 +56,7 @@
         add_shortcode('cards-grading-for_payment', array( $this, 'cards_grading_for_payment_shortcode' ));
         add_shortcode('cards-grading-completed', array( $this, 'cards_grading_completed_shortcode' ));
         add_shortcode('cards-grading-cancelled', array( $this, 'cards_grading_cancelled_shortcode' ));
+        add_shortcode('cards-grading-processing', array( $this, 'cards_grading_processing_shortcode' ));
         
         add_shortcode('cards-grading-open_orders', array( $this, 'cards_grading_open_orders_shortcode' ));
         add_shortcode('cards-grading-admin_view_order', array( $this, 'cards_grading_admin_view_order_shortcode' ));
@@ -633,6 +634,25 @@
         return $output ;
     }
     
+    public function cards_grading_processing_shortcode($atts) 
+    {
+        $order_number = $_GET['id'];
+
+        $default = array(
+            'title' => 'Order Number',
+            'order_number' => $order_number
+        );
+        
+        $params = shortcode_atts($default, $atts);
+        ob_start();
+
+        include( plugin_dir_path( __FILE__ ) . 'admin/processing_orders.php' );
+        
+        $output = ob_get_clean(); 
+        
+        return $output ;
+    }
+
     public function cards_grading_dashbox_shortcode($atts) 
     {
 
