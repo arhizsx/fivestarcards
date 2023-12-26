e2287f0cfde0d35e13620f96b45af20b0ad86078
arhizsx <arhizsx@gmail.com>
2023-12-26 14:30:14 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/awaiting_payment.php
+++ wp-content/plugins/cards-grading/admin/awaiting_payment.php
@@ -75,7 +75,7 @@ $posts = get_posts($args);
                             if( $total_grading_charge > 0 ) {
                                                         
                 ?>
-                            <tr class="admin-order-row" data-post_id="<?php echo $post->ID; ?>">
+                            <tr class="" data-post_id="<?php echo $post->ID; ?>">
                                 <td><?php echo get_the_date( $date_format, $post->ID ) ?><br><span style='font-size:.7em !important;'><?php echo get_the_time( $time_format, $post->ID ); ?></span></td>
                                 <td><?php echo $user->display_name; ?></td>
                                 <td><?php echo $meta["order_number"][0]; ?></td>
