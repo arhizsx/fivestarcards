9a644941466ffeea3f90a7c9be46a3be215508b3
arhizsx <arhizsx@gmail.com>
2023-12-26 23:04:16 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/view_order.php
+++ wp-content/plugins/cards-grading/admin/view_order.php
@@ -203,7 +203,7 @@ $consignment_status = array("Order Partial Consignment", "Order Consigned", "Rea
                                 $card_total_dv = $card["dv"] * $card["quantity"];
                                 $card_grading_charge = $card["per_card"] * $card["quantity"];
 
-                                if( in_array( $meta["status"][0], array("To Pay - Grade Only", "To Ship", "Shipped", "Received", "Paid - Grade Only", "Graded") ) ){
+                                if( in_array( $meta["status"][0], array("Pay Grading", "To Pay - Grade Only", "To Ship", "Shipped", "Received", "Paid - Grade Only", "Graded") ) ){
                                     $grading_charge = $grading_charge + $card_grading_charge;
                                 }
         
