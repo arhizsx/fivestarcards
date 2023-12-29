f9ad9a7f6a68dcce594e220bbefb5b0cffd2b510
arhizsx <arhizsx@gmail.com>
2023-12-29 14:24:25 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/open_orders.php
+++ wp-content/plugins/cards-grading/admin/open_orders.php
@@ -73,8 +73,8 @@ $posts = get_posts($args);
                 <tr>
                     <th>Date</th>
                     <th>
-                        <select name="multi_update_status_select " class="mt-3" style="font-size: 14px; border: 0px;" >
-                            <option value="">Select New Status</option>
+                        <select name="multi_update_status_select " class="mt-3" style="font-size: 16px; font-weight: bold; border: 0px;" >
+                            <option value="">Customer</option>
                             <option value="Processing Order">Processing Order</option>
                             <option value="Shipped to PSA / SGC">Shipped to PSA / SGC</option>
                             <option value="Research">Research</option>
