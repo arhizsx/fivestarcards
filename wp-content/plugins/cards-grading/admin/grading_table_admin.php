624e2eff0a7b1936008d98b12782f9ddfa773080
arhizsx <arhizsx@gmail.com>
2023-12-30 22:49:39 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/grading_table_admin.php
+++ wp-content/plugins/cards-grading/admin/grading_table_admin.php
@@ -266,7 +266,7 @@ $total_dv = 0;
             Grading Type
             <input type="text" class="form-control">
         </div>
-        <div class="col-xl-3">
+        <div class="col-xl-3 pt-4">
             <button class="form-control btn btn-sm border btn-primary 5star_btn" data-action='confirm_add' data-type=''>
                 Create Order
             </button>
