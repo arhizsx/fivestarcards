e528cee8321de8d76dbd6dd2b6128bf8f3422cf2
arhizsx <arhizsx@gmail.com>
2023-12-30 16:34:14 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/grading_table_admin.php
+++ wp-content/plugins/cards-grading/admin/grading_table_admin.php
@@ -203,7 +203,7 @@ $total_dv = 0;
     <div class="row">
         <div class="col-xl-12" >
             Customer
-            <select class=''>
+            <select class='btn-sm'>
                 <option>US</option>
             </select>
             <button class='5star_btn btn btn-success btn-sm' data-type="" data-action="add_card">
