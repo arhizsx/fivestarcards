335344cd82ecb94688a4f8edc95f8794caa793dd
arhizsx <arhizsx@gmail.com>
2023-12-30 15:59:26 +0800

    5STarCards Updates

--- wp-content/plugins/cards-grading/admin/grading_table_admin.php
+++ wp-content/plugins/cards-grading/admin/grading_table_admin.php
@@ -205,14 +205,14 @@ $total_dv = 0;
             <H1 style="color: black !important;"><?php echo $params['title'] ?></H1>
         </div>
         <div class="col-xl-6 col-lg-6 col-md-6 text-end">
-            <button class='5star_btn btn btn-success mb-3' data-type="<?php echo $params['type'] ?>" data-action="add_card">
+            <button class='5star_btn btn btn-success mb-3' data-type="" data-action="add_card">
                 Log Card
             </button>
         </div>
     </div>
     <div class="table-responsive">
     
-    <table class='table 5star_logged_cards table-bordered table-striped' data-grading_type="<?php echo $params['type'] ?>" data-endpoint="<?php echo get_rest_url(null, "cards-grading/v1/add-card") ?>" data-table_action_endpoint="<?php echo get_rest_url(null, "cards-grading/v1/table-action") ?>" data-nonce="<?php echo wp_create_nonce("wp_rest"); ?>">
+    <table class='table 5star_logged_cards table-bordered table-striped' data-grading_type="" data-endpoint="<?php echo get_rest_url(null, "cards-grading/v1/add-card") ?>" data-table_action_endpoint="<?php echo get_rest_url(null, "cards-grading/v1/table-action") ?>" data-nonce="<?php echo wp_create_nonce("wp_rest"); ?>">
     <thead>
         <tr>
         <th>Year</th>
@@ -224,39 +224,17 @@ $total_dv = 0;
         </tr>
     </thead>
     <tbody>
-        <?php 
-            if( $posts ){
-
-                foreach($posts as $post)
-                {
-                    $meta = get_post_meta($post->ID);
-                    $card = json_decode($meta['card'][0], true);
-
-                    $card_total_dv = $card["dv"] * $card["quantity"];
-                    $card_grading_charge = $card["per_card"] * $card["quantity"];
-
-                    $grading_charge = $grading_charge + $card_grading_charge;
-                    $total_dv = $total_dv + $card_total_dv;
-
-        ?>
-        <tr class="card-row" data-post_id="<?php echo $post->ID; ?>" data-card='<?php echo json_encode($card) ?>'>
-            <td><?php echo $card["year"]; ?></td>
-            <td><?php echo $card["brand"]; ?></td>
-            <td><?php echo $card["card_number"]; ?><br><small><?php echo $card["attribute"]; ?></small></td>
+        <tr class="card-row" data-post_id="" data-card=''>
+            <td></td>
+            <td></td>
+            <td></td>
             <td><?php echo $card["player"]; ?></td>
-            <td class='text-end'><?php echo "$" . number_format((float)$card["dv"], 2, '.', ''); ?></td>
-            <td class='text-end'><?php echo "$" . number_format((float) $card_grading_charge, 2, '.', ''); ?></td>
+            <td class='text-end'></td>
+            <td class='text-end'></td>
         </tr>
-        <?php          
-                }
-            } else {
-        ?>
         <tr>
             <td class="text-center" colspan="9">Empty</td>
         </tr>
-        <?php          
-            }
-        ?>
     </tbody>
     </table>
     </div>
@@ -267,38 +245,29 @@ $total_dv = 0;
                 <div class="col-lg-6 text-end pb-2 fw-bold cards_charge_total">
             <div class="row mb-2">
                 <div class="col text-end">
-                            Total DV          
+                    Total DV          
                 </div>
                 <div class="col text-end" id="total_dv">
-                    $<?php echo number_format((float)$total_dv, 2, '.', ''); ?>
+                    
                 </div>
             </div>
             <div class="row">
                 <div class="col text-end">
-                            Grading Charge    
+                    Grading Charge    
                 </div>
                 <div class="col text-end"  id="grading_charges">
-                $<?php echo number_format((float)$grading_charge, 2, '.', ''); ?>
+                
                 </div>
             </div>
             </div>
         </div>
-        <?php 
-            if( $posts )
-            {
-                $show_bottom_buttons = "";
-            } 
-            else {
-                $show_bottom_buttons = "d-none";
-            }
-        ?>
-        <div class="row bottom_buttons <?php echo $show_bottom_buttons ?>">
+        <div class="row bottom_buttons>
             <div class="col-lg-12 text-end border-top pt-2 ">
-                <button class='5star_btn btn btn-danger' data-type="<?php echo $params['type'] ?>" data-action="clear_table">
+                <button class='5star_btn btn btn-danger' data-type="" data-action="clear_table">
                     Clear List
                 </button>
                 
-                <button class='5star_btn btn btn-primary' data-type="<?php echo $params['type'] ?>" data-action="checkout">
+                <button class='5star_btn btn btn-primary' data-type="" data-action="checkout">
                     Checkout
                 </button>      
             </div>
