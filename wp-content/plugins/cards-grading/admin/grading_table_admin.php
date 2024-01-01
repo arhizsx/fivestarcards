1c4a47bf9780f7ee5ec53119c9601fcc917adef4
arhizsx <arhizsx@gmail.com>
2024-01-02 05:47:18 +0800

    Cards Grading Updates

--- wp-content/plugins/cards-grading/admin/grading_table_admin.php
+++ wp-content/plugins/cards-grading/admin/grading_table_admin.php
@@ -210,6 +210,12 @@
                             </tr>
                         <?php 
                             }
+                        } else {
+                        ?>
+                            <tr>
+                                <td colspan="6">Empty</td>
+                            </tr>
+                        <?php
                         }
                         ?>
                         </tbody>
