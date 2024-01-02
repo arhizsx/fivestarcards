f5d7ee0a994c052fd2a9240edf9b4f47c24a4059
arhizsx <arhizsx@gmail.com>
2024-01-02 11:27:23 +0800

    Cards Grading Updates

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -725,13 +725,13 @@ function orderAction(action, data, order_number){
 
     var override  = ["confirm_admin_delete_order", "admin_assign_order"];
 
-    alert($.inArray(action, override));
 
     if(  $.inArray(action, override)  != -1 ){
         url = $(document).find(".5star_logged_cards").data("order_endpoint");
     } else {
         url = $(document).find(".5star_logged_cards").data("endpoint");
     }
+    alert( url );
 
     $.ajax({
         method: 'post',
