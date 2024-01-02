00e80c68b88769b7d6b41dd7a90361b5e8b19d30
arhizsx <arhizsx@gmail.com>
2024-01-02 13:03:43 +0800

    Cards Grading Updates

--- wp-content/plugins/cards-grading/admin/tables/members.php
+++ wp-content/plugins/cards-grading/admin/tables/members.php
@@ -44,7 +44,7 @@
                             <?php  echo $user->user_email ?>
                         </td>
                         <td>
-                            <?php  echo $user->roles[0] ?>
+                            <?php  echo str_replace("um_", "", $user->roles[0]) ?>
                         </td>
                         <td class="text-end">
                             AAA
