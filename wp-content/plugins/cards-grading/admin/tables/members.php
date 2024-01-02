dbed40d2c8663a675167b39748356956ab6c0f26
arhizsx <arhizsx@gmail.com>
2024-01-02 13:11:47 +0800

    Cards Grading Updates

--- wp-content/plugins/cards-grading/admin/tables/members.php
+++ wp-content/plugins/cards-grading/admin/tables/members.php
@@ -1,4 +1,71 @@
 <div class="m-0 p-0">
+<div class="row">
+        <div class="col-xl-6">
+            <H1 style="color: black;">Admins</H1>            
+        </div>
+        <div class="col-xl-6 text-end">
+            
+        </div>
+    </div>
+    <div class="table-responsive">    
+        <table class='table 5star_my_orders table-bordered table-striped'>
+            <thead>
+                <tr>
+                    <th>Customer #</th>
+                    <th>Customer</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th class='text-end'>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+
+                    $args = array(
+                        'orderby'    => 'display_name',
+                        'order'      => 'ASC'
+                    );   
+
+                    $users = get_users( $args );
+
+
+                    if($users){
+                        foreach($users as $user){
+                            if($user->roles[0] == "um_member"){
+
+                ?>
+                    <tr>
+                        <td>
+                            <?php  echo $user->ID + 1000 ?>
+                        </td>
+                        <td>
+                            <?php  echo $user->display_name ?>
+                        </td>
+                        <td>
+                            <?php  echo $user->user_email ?>
+                        </td>
+                        <td>
+                            <?php  echo ucwords(str_replace("um_", "", $user->roles[0])) ?>
+                        </td>
+                        <td class="text-end">
+                            AAA
+                        </td>
+                    </tr>
+                <?php    
+                            }    
+                        }
+                    } else {
+                ?>
+                    <tr>
+                        <td class="text-center" colspan="4">Empty</td>
+                    </tr>
+                <?php                  
+                    }
+                ?>                
+            </tbody>
+        </table>
+    </div>
+
     <div class="row">
         <div class="col-xl-6">
             <H1 style="color: black;">Members</H1>            
