f73ad662cc94af62a430dd7bb38388dd2ca3bc4c
arhizsx <arhizsx@gmail.com>
2024-01-07 12:02:16 +0800

    dompdf

--- wp-content/plugins/cards-grading/pdf/index.php
+++ wp-content/plugins/cards-grading/pdf/index.php
@@ -1,11 +1,5 @@
 <?php 
 
-require_once ( 'wp-content/plugins/cards-grading/autoload.inc.php');
-use Dompdf\Dompdf; 
- 
-
-// instantiate and use the dompdf class
-$dompdf = new Dompdf();
 
 echo "test";
 
