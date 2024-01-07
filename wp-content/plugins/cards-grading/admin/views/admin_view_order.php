4bceb39af4815a94b3b5082e0a43c4681d3ba427
arhizsx <arhizsx@gmail.com>
2024-01-07 14:44:12 +0800

    dompdf

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -116,6 +116,9 @@ $processed_status = array("Processing Order", "Cards Graded");
             <H3 style="color: black !important;">Cards List</H3>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 text-end">
+            <button class='5star_btn btn btn-primary mb-3' data-action="view_pdf"  data-order_number="<?php echo $params['order_number'] ?>">
+                PDF
+            </button>      
             <?php if( $checkout_meta["status"][0] == "Shipped" ) { ?>
             <button class='5star_btn btn btn-primary mb-3' data-action="package_received"  data-order_number="<?php echo $params['order_number'] ?>">
                 Package Received
