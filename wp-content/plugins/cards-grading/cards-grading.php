0716428e9d6b191308f9938932c56eca9456c29d
arhizsx <arhizsx@gmail.com>
2024-02-11 21:11:27 +0800

    Ebay Integrator

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -703,7 +703,7 @@
     public function handle_notification($data){
         $headers = $data->get_headers();
         $params = $data->get_params();
-        return $headers;
+        return print_r($data);
     }
 
     //*********** ENDPOINTS *********** //
