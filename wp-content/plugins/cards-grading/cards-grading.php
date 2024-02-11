b7f35f1f0ce96eb423186bd77b4e0cedb82c3fa2
arhizsx <arhizsx@gmail.com>
2024-02-11 21:50:20 +0800

    Ebay Integrator

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -705,14 +705,9 @@
         $params = $data->get_params();
         $body = $data->get_body();
 
-        $data = <<< XML
-                $body
-                XML;
+        $xml = simplexml_load_string($body);
 
-        $xml = simplexml_load_string($data);
-        $data = $xml->xpath("//soap:Body/*")[0];
-
-        return $data;
+        return $xml;
     }
 
     //*********** ENDPOINTS *********** //
