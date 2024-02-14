f0b9cde9da0772ff0294b416989d8079b1a8740d
arhizsx <arhizsx@gmail.com>
2024-02-14 17:13:10 +0800

    Ebay

--- ebay/index.php
+++ ebay/index.php
@@ -18,18 +18,18 @@ $headers = array (
     'Content-Type'  => 'application/x-www-form-urlencoded'
 );
 
-$post_data = json_encode(array(
+$post_data = array(
         'grant_type' => 'authorization_code',
         'code' => $auth_code,
         'redirect_uri' => 'Fernando_Salvad-Fernando-5starc-qxmeny'
-));
+);
 
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $apiURL);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data) );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($ch);
 if(curl_errno($ch)){
