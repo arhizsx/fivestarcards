c84cc431dade016c2e5fa202cc29968c6ab87653
arhizsx <arhizsx@gmail.com>
2024-02-14 10:43:36 +0800

    Ebay

--- ebay/index.php
+++ ebay/index.php
@@ -4,7 +4,7 @@ echo  urldecode($_GET["code"]);
 echo "<br>";
 echo "<hr>";
 echo "<br>";
-echo "Basic " . base64_encode("<Fernando-5starcar-PRD-a81fdd189-a762dfc7>:<PRD-81fdd189cbf1-f472-4131-93a2-1990>");
+echo "Basic " . base64_encode("Fernando-5starcar-PRD-a81fdd189-a762dfc7:PRD-81fdd189cbf1-f472-4131-93a2-1990");
 echo "<br>";
 echo "<hr>";
 echo "<br>";
