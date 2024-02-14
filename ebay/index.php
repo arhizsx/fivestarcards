1f2f570b7daf6aa821a8a83000fe6248f0e3b3f3
arhizsx <arhizsx@gmail.com>
2024-02-14 10:31:53 +0800

    Ebay

--- ebay/index.php
+++ ebay/index.php
@@ -4,4 +4,12 @@ echo  urldecode($_GET["code"]);
 echo "<hr>";
 echo "Basic " . base64_encode("<Fernando-5starcar-PRD-a81fdd189-a762dfc7>:<PRD-81fdd189cbf1-f472-4131-93a2-1990>");
 
+
+  $curl = curl_init('https://api.sandbox.ebay.com/identity/v1/oauth2/token');
+  curl_setopt($curl, CURLOPT_POST, true);
+  $response = curl_exec($curl);
+  curl_close($curl);
+  print_r( $response );
+
 ?>
+
