d45db9fe129bbc61eb1880245a0fbf108555f0f5
arhizsx <arhizsx@gmail.com>
2024-02-14 09:59:27 +0800

    Ebay

--- ebay/index.php
+++ ebay/index.php
@@ -2,6 +2,6 @@
 
 echo  urldecode($_GET["code"]);
 echo "<hr>";
-echo "Basic " . base64_encode("<Fernando-5starcar-SBX-07f9b21e9-07eaeb73>:<SBX-7f9b21e930f4-69ef-46fe-92f6-0bce>");
+echo "Basic " . base64_encode("Fernando-5starcar-SBX-07f9b21e9-07eaeb73:SBX-7f9b21e930f4-69ef-46fe-92f6-0bce");
 
 ?>
