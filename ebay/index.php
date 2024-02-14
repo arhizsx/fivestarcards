8f1a8e0088879adc63b1555da10d1ee726881214
arhizsx <arhizsx@gmail.com>
2024-02-14 10:56:44 +0800

    Ebay

--- ebay/index.php
+++ ebay/index.php
@@ -16,7 +16,7 @@ $headers = array (
     'Content-Type'  => 'application/x-www-form-urlencoded'
 );
 
-echo sprintf('Basic <%s>',base64_encode(sprintf('%s:%s', $client_id, $client_secret)));
+echo 'Basic <' . base64_encode("Fernando-5starcar-PRD-a81fdd189-a762dfc7:PRD-81fdd189cbf1-f472-4131-93a2-1990") . '>';
 
 echo "<hr>";
 
