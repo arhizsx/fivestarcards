817d2160f4ab115f0d21152ca9d750778a90627e
arhizsx <arhizsx@gmail.com>
2024-02-14 16:37:37 +0800

    Ebay

--- ebay/index.php
+++ ebay/index.php
@@ -1,50 +1,57 @@
-<?php 
-$auth_code = urldecode($_GET["code"]);
+<html>
+    <head>
 
-// PRODUCTION
-$client_id = "Fernando-5starcar-PRD-a81fdd189-a762dfc7";
-$client_secret = "PRD-81fdd189cbf1-f472-4131-93a2-1990";
-$redirect_uri  = "Fernando_Salvad-Fernando-5starc-qxmeny";
-$apiURL = "https://api.ebay.com/identity/v1/oauth2/token";
-$grant_type = "authorization_code";
-$authorization = 'Basic ' . base64_encode($client_id . ":" . $client_secret);
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    
+    </head>
+    <body>
 
-echo '<div class="ebay" 
-    data-code="' . $auth_code . '" 
-    data-client_id="' . $client_id . '" 
-    data-client_secret="' . $client_secret . '" 
-    data-redirect_uri="' . $redirect_uri . '" 
-    data-apiURL="' . $apiURL . '" 
-    data-grant_type="' . $grant_type . '" 
-    data-authorization="' . $authorization . '"'. 
-    '><eBay Integration/div>';
+        <?php 
+        $auth_code = urldecode($_GET["code"]);
 
-?>
+        // PRODUCTION
+        $client_id = "Fernando-5starcar-PRD-a81fdd189-a762dfc7";
+        $client_secret = "PRD-81fdd189cbf1-f472-4131-93a2-1990";
+        $redirect_uri  = "Fernando_Salvad-Fernando-5starc-qxmeny";
+        $apiURL = "https://api.ebay.com/identity/v1/oauth2/token";
+        $grant_type = "authorization_code";
+        $authorization = 'Basic ' . base64_encode($client_id . ":" . $client_secret);
 
-<script>
+        echo '<div class="ebay" 
+            data-code="' . $auth_code . '" 
+            data-client_id="' . $client_id . '" 
+            data-client_secret="' . $client_secret . '" 
+            data-redirect_uri="' . $redirect_uri . '" 
+            data-apiURL="' . $apiURL . '" 
+            data-grant_type="' . $grant_type . '" 
+            data-authorization="' . $authorization . '"'. 
+            '><eBay Integration/div>';
 
-$.ajax({
-    method: 'post',
-    url: apiURL,
-    headers: {
-        'Centent-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic RmVybmFuZG8tNXN0YXJjYXItUFJELWE4MWZkZDE4OS1hNzYyZGZjNzpQUkQtODFmZGQxODljYmYxLWY0NzItNDEzMS05M2EyLTE5OTA=' 
-    },
-    data: {
-        code: '<?php echo $auth_code; ?>',
-        grant_type: '<?php echo $grant_type; ?>',
-        redirect_uri: '<?php echo $redirect_uri; ?>',
-    },
-    success: function(resp){
+        ?>
+        <script>
 
-        console.log(resp)
+            $.ajax({
+                method: 'post',
+                url: apiURL,
+                headers: {
+                    'Centent-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic RmVybmFuZG8tNXN0YXJjYXItUFJELWE4MWZkZDE4OS1hNzYyZGZjNzpQUkQtODFmZGQxODljYmYxLWY0NzItNDEzMS05M2EyLTE5OTA=' 
+                },
+                data: {
+                    code: '<?php echo $auth_code; ?>',
+                    grant_type: '<?php echo $grant_type; ?>',
+                    redirect_uri: '<?php echo $redirect_uri; ?>',
+                },
+                success: function(resp){
 
-    },
-    error: function(){
-        console.log("ebay error");
-    }
-});
+                    console.log(resp)
 
+                },
+                error: function(){
+                    console.log("ebay error");
+                }
+            });
 
-</script>
-
+        </script>
+    </body>
+</html>
