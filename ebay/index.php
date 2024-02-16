8783c426ec73a114ccc989c941e5e1f2741c8fbb
arhizsx <arhizsx@gmail.com>
2024-02-17 05:38:03 +0800

    PU updates

--- ebay/index.php
+++ ebay/index.php
@@ -1,22 +1,16 @@
 <?php 
 $auth_code = urldecode($_GET["code"]);
 
-// PRODUCTION
 $client_id = "Fernando-5starcar-PRD-a81fdd189-a762dfc7";
 $client_secret = "PRD-81fdd189cbf1-f472-4131-93a2-1990";
 $redirect_uri  = "Fernando_Salvad-Fernando-5starc-qxmeny";
 $apiURL = "https://api.ebay.com/identity/v1/oauth2/token";
 $grant_type = "authorization_code";
 
-echo $auth_code;
-echo "<hr>";
-echo 'Basic ' . base64_encode($client_id . ":" . $client_secret);
-echo "<hr>";
-
 $post_data = [
-        'grant_type' => 'authorization_code',
-        'code' => $auth_code,
-        'redirect_uri' => 'Fernando_Salvad-Fernando-5starc-qxmeny'
+    'grant_type' => $grant_type,
+    'code' => $auth_code,
+    'redirect_uri' => $redirect_uri
 ];
 
 $curl = curl_init();
@@ -24,7 +18,7 @@ $curl = curl_init();
 curl_setopt_array(
     $curl,
     [
-        CURLOPT_URL => 'https://api.ebay.com/identity/v1/oauth2/token',
+        CURLOPT_URL => $apiURL,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -52,6 +46,10 @@ print_r($results["access_token"]);
 echo "<hr>";
 
 print_r($results["refresh_token"]);
+
+echo "<hr>";
+
+print_r($results);
 
 ?>
 
