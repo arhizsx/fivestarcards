743a9eaa8d4d2cf226d24f3a040ee17570539e45
arhizsx <arhizsx@gmail.com>
2024-02-18 00:02:07 +0800

    ebay

--- ebay/index.php
+++ ebay/index.php
@@ -18,6 +18,7 @@
 
     </head>
     <body>
+        <div class="container">
         <H1>5 Star Cards eBay Integration</H1>
         <hr>
         <div>
@@ -258,10 +259,9 @@
         <?php
         }
         ?>
-
+    
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    
-
     </body>
 </html>
