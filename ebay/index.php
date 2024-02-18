4579ec15b8e365205c4cd0fa0765a5d5da716dce
arhizsx <arhizsx@gmail.com>
2024-02-18 09:30:20 +0800

    Get Item Details

--- ebay/index.php
+++ ebay/index.php
@@ -262,45 +262,10 @@
         <?php
         }
 
-        $get_item_body = '<?xml version="1.0" encoding="utf-8"?><GetItemRequest xmlns="urn:ebay:apis:eBLBaseComponents"><ErrorLanguage>en_US</ErrorLanguage><WarningLevel>High</WarningLevel><ItemID>256380588293</ItemID></GetItemRequest>';
 
         ?>
     
         </div>
 
-        <?php 
-
-$access_token = $results["access_token"];
-$apiURL = "https://api.ebay.com/buy/browse/v1/item/get_item_by_legacy_id?legacy_item_id=266676499755";
-$post_data = "";
-
-$curl = curl_init();
-
-curl_setopt_array(
-    $curl,
-    [
-        CURLOPT_URL => $apiURL,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        CURLOPT_HTTPHEADER => [
-            'Authorization: Bearer ' . $access_token,
-        ]
-    ]
-);
-
-$response = curl_exec($curl);
-$status = curl_getinfo($curl);
-
-curl_close($curl);
-
-print_r($response);
-
-
-        ?>
     </body>
 </html>
