40bbf190e72d31ddbc8b56324a923a2f66d84e1a
arhizsx <arhizsx@gmail.com>
2024-02-19 13:32:00 +0800

    eBay Updates

--- wp-content/plugins/ebayintegration/assets/js/frontend.js
+++ wp-content/plugins/ebayintegration/assets/js/frontend.js
@@ -16,7 +16,7 @@ jQuery( document ).on("click", ".ebayintegration-btn", function(){
 	
 		jQuery.ajax({
 			method: 'get',
-			url: "wp-json/ebayintegration/v1/ajax",
+			url: "/wp-json/ebayintegration/v1/ajax",
 			data: { 
 				action: "getItems"
 			},
