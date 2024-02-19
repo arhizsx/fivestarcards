9e6784268a68321917aebe153c2e80ef23a76254
arhizsx <arhizsx@gmail.com>
2024-02-19 11:52:33 +0800

    eBay Updates

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -88,7 +88,7 @@ class Ebay_Integration_Ebay_API {
 			}
 		}
 
-		return print_r( json_decode($result) );
+		return print_r( json_decode($result, true) );
 
 
 		if( count($result["ActiveList"]["ItemArray"]["Item"]) == 1){
