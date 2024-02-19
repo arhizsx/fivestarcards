56fcba28475228768561d014cc81f461421f884f
arhizsx <arhizsx@gmail.com>
2024-02-19 17:27:27 +0800

    eBay Updates

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -129,7 +129,7 @@ class Ebay_Integration_Ebay_API {
 
 			if($json["Ack"] == "Failure"){
 
-				return array("error" => true, "data"=> $json);
+				return array("error" => true, "data"=> json_encode($json));
 	
 			} else {
 				
