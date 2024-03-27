b0e3ffa17fd968991bd572314622dfbee6dde777
arhizsx <arhizsx@gmail.com>
2024-03-27 20:19:28 +0800

    sku members lists

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -303,7 +303,7 @@ class Ebay_Integration_Ebay_API {
 
 				
 
-				return array("error" => false, "data"=> $this->processItems($json["ActiveList"]["ItemArray"]["items"]) );
+				return array("error" => false, "data"=> $this->processItems($json["ActiveList"]["ItemArray"]) );
 				
 			}
 	
