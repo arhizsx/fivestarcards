07e748194bdf6fb3ff31f1e6b27772d0ca734f50
arhizsx <arhizsx@gmail.com>
2024-03-27 12:30:53 +0800

    sku members lists

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members.php
@@ -4,68 +4,6 @@
 ?>
 
 <div class="m-0 p-0">
-    <div class="row">
-        <div class="col-xl-6">
-            <H1 style="color: black;">Administrators</H1>            
-        </div>
-        <div class="col-xl-6 text-end">
-            
-        </div>
-    </div>
-    <div class="table-responsive">    
-        <table class='table 5star_my_orders table-bordered table-striped'>
-            <thead>
-                <tr>
-                    <th>Customer #</th>
-                    <th>Customer</th>
-                    <th>Email</th>
-                    <th class='text-end'>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-
-                    $args = array(
-                        'orderby'    => 'display_name',
-                        'order'      => 'ASC'
-                    );   
-
-                    $users = get_users( $args );
-
-
-                    if($users){
-                        foreach($users as $user){
-                            if($user->roles[0] == "um_admin"){
-
-                ?>
-                    <tr>
-                        <td>
-                            <?php  echo $user->ID + 1000 ?>
-                        </td>
-                        <td>
-                            <?php  echo $user->display_name ?>
-                        </td>
-                        <td>
-                            <?php  echo $user->user_email ?>
-                        </td>
-                        <td class="text-end">
-                            <button class="btn border btn-success 5star_btn" data-action='demote_admin' data-user_id='<?php echo $user->ID; ?>'>SKUs</button>
-                        </td>
-                    </tr>
-                <?php    
-                            }    
-                        }
-                    } else {
-                ?>
-                    <tr>
-                        <td class="text-center" colspan="4">Empty</td>
-                    </tr>
-                <?php                  
-                    }
-                ?>                
-            </tbody>
-        </table>
-    </div>
 
     <div class="row">
         <div class="col-6">
