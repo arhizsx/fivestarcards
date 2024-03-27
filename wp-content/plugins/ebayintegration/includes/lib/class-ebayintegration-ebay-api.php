3fe06392ebb9a188fc971fa02d5bc679c1ef174a
arhizsx <arhizsx@gmail.com>
2024-03-27 20:17:44 +0800

    sku members lists

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -318,7 +318,7 @@ class Ebay_Integration_Ebay_API {
 		
 		global $wpdb;     
 
-		return json_encode($items);
+		return json_decode(json_encode($items), true);
 
 		foreach($items["item"] as $item){
 
