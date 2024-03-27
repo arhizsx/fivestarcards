9a3713e56efa4438149ad5b6589fa397e54f7f88
arhizsx <arhizsx@gmail.com>
2024-03-27 12:29:48 +0800

    sku members lists

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/router.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/router.php
@@ -4,12 +4,12 @@
 
         case "get_item_buttons":
 
-			include( plugin_dir_path( __FILE__ ) . 'shortcodes/getitem.php');			
+			include( plugin_dir_path( __FILE__ ) . 'getitem.php');			
             break;
 
         case "members_list":
 
-			include( plugin_dir_path( __FILE__ ) . 'shortcodes/members.php');			
+			include( plugin_dir_path( __FILE__ ) . 'members.php');			
             break;
 
         default:
