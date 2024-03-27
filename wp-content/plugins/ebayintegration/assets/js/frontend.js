780a7330e8bb56e9b20b3844535b4ce3ec46e447
arhizsx <arhizsx@gmail.com>
2024-03-27 15:37:58 +0800

    sku members lists

--- wp-content/plugins/ebayintegration/assets/js/frontend.js
+++ wp-content/plugins/ebayintegration/assets/js/frontend.js
@@ -183,7 +183,7 @@ function getItems(page){
 
 			if(resp.error != true){
 				jQuery.each(resp.data.ActiveList.ItemArray.Item, function(k, v){		
-					jQuery(document).find(".ebayintegration-items_box").append(eBayItemTemplate(v));
+					// jQuery(document).find(".ebayintegration-items_box").append(eBayItemTemplate(v));
 					// getItemInfo(v.ItemID);
 				});	
 			} else {
