5fccb79f7837edc105bb0d141b4480a5fadaae4b
arhizsx <arhizsx@gmail.com>
2024-03-28 10:38:16 +0800

    Add SKU

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -118,14 +118,9 @@ class Ebay_Integration_Ebay_API {
 
 			$in = rtrim($in, ',');
 
-			return $in;
-
-
 			$ebay = $wpdb->get_results ( "
 				SELECT * FROM ebay WHERE sku IN (" . $in . ") ORDER BY sku ASC
 			" );
-
-			$skus = get_user_meta( $user_id, "sku", true );
 
 			return array("error"=> false, "skus" => $skus, "ebay" => $ebay);
 
