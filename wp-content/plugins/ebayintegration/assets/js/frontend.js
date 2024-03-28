7efbc7556fad320e518046468d6411ec41991734
arhizsx <arhizsx@gmail.com>
2024-03-28 10:12:19 +0800

    Add SKU

--- wp-content/plugins/ebayintegration/assets/js/frontend.js
+++ wp-content/plugins/ebayintegration/assets/js/frontend.js
@@ -123,7 +123,7 @@ jQuery( document ).on("click", ".ebayintegration-btn", function(){
 				});
 				$skus = $skus + "</ul>"
 
-				console.log( jQuery(document).find("#members_skus_table").find("tr.user_row").html() );
+				console.log( jQuery(document).find("#members_skus_table").html() );
 
 				jQuery(document).find(".user_row [data-user_id='" + user_id + "'").find(".skus").html( $skus );
 
