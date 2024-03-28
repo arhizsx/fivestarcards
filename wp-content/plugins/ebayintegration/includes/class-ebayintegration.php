3cca398bb3b32f2d3354159ba784e893d249e758
arhizsx <arhizsx@gmail.com>
2024-03-28 09:03:59 +0800

    Add SKU

--- wp-content/plugins/ebayintegration/includes/class-ebayintegration.php
+++ wp-content/plugins/ebayintegration/includes/class-ebayintegration.php
@@ -124,7 +124,7 @@ class Ebay_Integration {
 	 * @param string $version Plugin version.
 	 */
 	public function __construct( $file = '', $version = '1.0.0' ) {
-		$this->_version = '1.0.105';
+		$this->_version = '1.0.106';
 		$this->_token   = 'Ebay_Integration';
 
 		// Load plugin environment variables.
