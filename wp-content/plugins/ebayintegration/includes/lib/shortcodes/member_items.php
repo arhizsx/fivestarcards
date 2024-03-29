208ae16a4a1eca274e700db4ae21ff4f76ca6687
arhizsx <arhizsx@gmail.com>
2024-03-29 23:36:07 +0800

    btn upload

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/member_items.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/member_items.php
@@ -38,7 +38,7 @@ $results = $wpdb->get_results("
     <div class="row">
             <?php 
                 foreach($results as $result){
-                    echo "<div class='col-2 border mb-3' style='min-height: 350px;'>";
+                    echo "<div class='col-3 border mb-3' style='min-height: 350px;'>";
                     echo $result->Title;
                     echo "</div>";
                 }
