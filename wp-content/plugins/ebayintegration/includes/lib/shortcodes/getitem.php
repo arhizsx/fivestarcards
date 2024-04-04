4075e653a791beff11a3228f76cd1ce90a76637c
arhizsx <arhizsx@gmail.com>
2024-04-05 07:50:45 +0800

    Multi Get

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/getitem.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/getitem.php
@@ -66,9 +66,11 @@
                 </button>
             </div>
             <div class="row mb-5">
-                <H5>Active eBay Items</H5>
-                <div class="col-12 p-5" id="items_with_sku">
+                <div class="col-12">
+                    <H5 style="color: black;">Active eBay Items</H5>
+                    <div  id="items_with_sku">
 
+                    </div>
                 </div>
             </div>
 
