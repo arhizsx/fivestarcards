b39b88856369d392f0e2ff5a561d465c8243c236
arhizsx <arhizsx@gmail.com>
2024-04-05 01:17:54 +0800

    Multi Get

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -516,7 +516,7 @@ class Ebay_Integration_Ebay_API {
 
 
 
-		return [ "count" => count( $items ), "items" => $items, "skus" => $all_skus ];
+		return [ "count" => count( $items ), "items" => $items, "skus" => "" ];
 
 
 	}	
