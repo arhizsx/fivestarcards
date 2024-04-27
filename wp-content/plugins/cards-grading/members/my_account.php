efa4ede5bb1bfc5ef84a03b88a6a284cfea023b3
arhizsx <arhizsx@gmail.com>
2024-04-28 06:49:27 +0800

    New My Account Layout

--- wp-content/plugins/cards-grading/members/my_account.php
+++ wp-content/plugins/cards-grading/members/my_account.php
@@ -9,9 +9,9 @@
     <div class="col-xl-4">
     </div>
     <div class="col-xl-8">
-        <nav class="nav nav-pills flex-column flex-sm-row">
-            <a class="flex-sm-fill text-sm-center nav-link py-5 active" aria-current="page" href="#">Active</a>
-            <a class="flex-sm-fill text-sm-center nav-link py-5" href="#">Pending</a>
+        <nav class="nav nav-pills flex-column flex-sm-row mb-4">
+            <a class="flex-sm-fill text-sm-center nav-link py-3 active" aria-current="page" href="#">Active</a>
+            <a class="flex-sm-fill text-sm-center nav-link py-3" href="#">Pending</a>
             <a class="flex-sm-fill text-sm-center nav-link py-5" href="#">Completed</a>
         </nav>
     </div>
