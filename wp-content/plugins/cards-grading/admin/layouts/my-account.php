ac103291c89bb02cdda6d2373bee74c95ae115ea
arhizsx <arhizsx@gmail.com>
2024-04-28 00:51:22 +0800

    Fivestarcards

---
+++ wp-content/plugins/cards-grading/admin/layouts/my-account.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-md-12">
+        <H1>Hello, Someone</H1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xl-4">
+        <H3>ADMIN MENU</H3>
+    </div>
+    <div class="col-xl-8">
+        
+    </div>
+</div>
