b568c23846b10a311524efa0d612d4e36aa2779b
arhizsx <arhizsx@gmail.com>
2024-04-28 06:46:54 +0800

    New My Account Layout

--- wp-content/plugins/cards-grading/members/my_account.php
+++ wp-content/plugins/cards-grading/members/my_account.php
@@ -7,48 +7,13 @@
 </div>
 <div class="row">
     <div class="col-xl-4">
-        <ul>
-            <li>My Listings</li>
-            <li>Buy Credits</li>
-            <li>Redeem Credits</li>
-            <li>Request Cashout</li>
-        </ul>
     </div>
     <div class="col-xl-8">
-
-<div style="padding: 5px;">
-    <div class="mb-3">
-        <ul class="body-tabs body-tabs-layout tabs-animated nav" id="monitoring-tabs">
-            <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="monitoring-tab-0"  data-toggle="tab" data-bs-toggle="tab" data-bs-target="#monitoring-tab-content-0" href="#monitoring-tab-content-0" role="tab" aria-controls="monitoring-tab-content-0" aria-selected="true">
-                Active
-                </a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="monitoring-tab-1"  data-toggle="tab" data-bs-toggle="tab" data-bs-target="#monitoring-tab-content-1" href="#monitoring-tab-content-1" role="tab" aria-controls="monitoring-tab-content-1" aria-selected="false">
-                Pending
-                </a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a class="nav-link" id="monitoring-tab-2"  data-toggle="tab" data-bs-toggle="tab" data-bs-target="#monitoring-tab-content-2" href="#monitoring-tab-content-2" role="tab" aria-controls="monitoring-tab-content-2" aria-selected="false">
-                Completed
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div class="tab-content">
-        <div class="tab-pane fade show active" id="monitoring-tab-content-0" role="tabpanel" aria-labelledby="monitoring-tab-content-0" tabindex="0">
-            Active
-        </div>
-        <div class="tab-pane fade" id="monitoring-tab-content-1" role="tabpanel" aria-labelledby="monitoring-tab-content-1" tabindex="1">
-            Pending
-        </div>
-        <div class="tab-pane fade" id="monitoring-tab-content-2" role="tabpanel" aria-labelledby="monitoring-tab-content-2" tabindex="2">
-            Completed
-        </div>
-    </div>
-</div>
-
-
+        <nav class="nav nav-pills flex-column flex-sm-row">
+            <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
+            <a class="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
+            <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+            <a class="flex-sm-fill text-sm-center nav-link disabled" aria-disabled="true">Disabled</a>
+        </nav>
     </div>
 </div>
