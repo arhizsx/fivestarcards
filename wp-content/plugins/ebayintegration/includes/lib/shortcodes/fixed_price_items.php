f8b66e1885318f44a858355ca3c21cced183be48
arhizsx <arhizsx@gmail.com>
2024-05-01 15:08:46 +0800

    ebay module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items.php
@@ -33,7 +33,7 @@ where status = 'completed'
             ?>
             <tr>
                 <td>
-                    <div class="title"><?php echo $data["Title"]; ?>< /div>
+                    <div class="title"><?php echo $data["Title"]; ?></div>
                     <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
                     <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>
                     <div class="item_id text-small">Listing Type: <?php echo $data["ListingType"] ?></div>
