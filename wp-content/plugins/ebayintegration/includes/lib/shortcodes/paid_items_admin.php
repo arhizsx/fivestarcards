5913ffa773ee9e679622b1122352a213ecba2fde
arhizsx <arhizsx@gmail.com>
2024-05-02 18:37:18 +0800

    ebay module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/paid_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/paid_items_admin.php
@@ -66,7 +66,8 @@ $users = get_users( $args );
                     </div>
                     <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
                     <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>
-                    <div class="item_id text-small">Listing Type: <?php echo $data["ListingType"] ?></div>
+                    <?php $listing = $data["ListingType"] == "Chinese" ? "Auction" : $data["ListingType"]; ?>
+                    <div class="item_id text-small">Listing Type: <?php echo $listing; ?></div>                    
 
                     
                 </td>
