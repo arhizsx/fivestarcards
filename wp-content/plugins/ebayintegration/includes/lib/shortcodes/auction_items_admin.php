66e16aa87b436e59a25ab60a7db233af0ee9ef67
arhizsx <arhizsx@gmail.com>
2024-05-02 22:44:01 +0800

    ebay module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/auction_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/auction_items_admin.php
@@ -28,7 +28,6 @@ $users = get_users( $args );
     <div>
         <i class="fa-brands fa-ebay fa-2xl"></i> AUCTION
     </div>
-    <div class="d-flex justify-content-between mb-3">
         <button class="btn btn-primary">Add Viewer</button>
         <select class="user_list_select form-control">
         <option value="">Filter by User</option>
@@ -41,7 +40,6 @@ $users = get_users( $args );
         ?>
         </select>
         <input class="btn pl-2 search_box" style="margin-left: 15px; text-align: left; padding-left: 10px; padding-bottom:5px; padding-top: 6px;" placeholder="Search" type="text" data-target=".search_table_auction">
-    </div>
 </div>
 
 <div class="table-responsive">
