2c5167b336bbb49dec17c4b112c0fc274d7cf1b4
arhizsx <arhizsx@gmail.com>
2024-05-02 15:38:12 +0800

    ebay module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items_admin.php
@@ -29,7 +29,7 @@ $users = get_users( $args );
         <i class="fa-brands fa-ebay fa-2xl"></i> FIXED PRICE
     </div>
     <div class="d-flex justify-content-between mb-3">
-        <select class="user_list_select form-control">
+        <select class="user_list_select form-control mr-3">
             <?php 
                 foreach($users as $user) {
                     $skus = get_user_meta( $user->ID, "sku", true );
