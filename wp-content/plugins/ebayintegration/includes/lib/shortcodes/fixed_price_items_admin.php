76b7fefa0e8b10d003e870fc5c5a72428aad1ab8
arhizsx <arhizsx@gmail.com>
2024-05-02 16:22:43 +0800

    ebay module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items_admin.php
@@ -44,6 +44,14 @@ $users = get_users( $args );
 </div>
 
 <div class="table-responsive">
+        <?php 
+            foreach($users as $user) {
+                $skus = get_user_meta( $user->ID, "sku", true );
+        ?>
+        <?php echo json_encode($skus) ; ?>
+        <?php                 
+            }
+        ?>
 
     <table class="table table-border table-striped table-sm table-hover search_table_fixed_price">
         <thead>
