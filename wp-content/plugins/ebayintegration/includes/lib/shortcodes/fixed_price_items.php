ca114516526160cbd70aecc6aff7fe83ee27df7b
arhizsx <arhizsx@gmail.com>
2024-05-14 21:30:10 +0800

    ebay

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items.php
@@ -52,7 +52,7 @@ $skus = get_user_meta( get_current_user_id(), "sku", true );
                     <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>                    
                 </td>
                 <td class="text-center">
-                    <?php echo $data["WatchCount"]; ?>
+                    <?php echo $data["WatchCount"] * 1; ?>
                 </td>
                 <td class="text-end">$<?php 
                     echo number_format(( $data["SellingStatus"]["CurrentPrice"]), 2, '.', ',');
