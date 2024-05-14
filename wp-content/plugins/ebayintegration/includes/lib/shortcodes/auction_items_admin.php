d153f8fe3f202f57333b2b323080d67d06fdaf19
arhizsx <arhizsx@gmail.com>
2024-05-14 16:12:40 +0800

    getEbayItems

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/auction_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/auction_items_admin.php
@@ -29,16 +29,6 @@ $users = get_users( $args );
         <i class="fa-brands fa-ebay fa-2xl"></i> AUCTION
     </div>
     <div>
-        <select class="user_list_select" style="margin-left: 15px; padding-left: 10px; padding-bottom:8px; padding-top: 7px; margin-top: 10px;">
-        <option value="">Filter by User</option>
-        <?php 
-            foreach($users as $user) {
-        ?>
-        <option value="<?php echo $user->ID; ?>"> <?php echo $user->display_name; ?></option>
-        <?php                 
-            }
-        ?>
-        </select>
         <input class="btn pl-2 search_box" style="margin-left: 15px; text-align: left; padding-left: 10px; padding-bottom:5px; padding-top: 6px;" placeholder="Search" type="text" data-target=".search_table_auction">
     </div>
 </div>
--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/fixed_price_items_admin.php
@@ -29,16 +29,6 @@ $users = get_users( $args );
         <i class="fa-brands fa-ebay fa-2xl"></i> FIXED PRICE
     </div>
     <div class="d-flex justify-content-between mb-3">
-        <select class="user_list_select form-control">
-        <option value="">Filter by User</option>
-        <?php 
-            foreach($users as $user) {
-        ?>
-        <option value="<?php echo $user->ID; ?>"> <?php echo $user->display_name; ?></option>
-        <?php                 
-            }
-        ?>
-        </select>
         <input class="btn pl-2 search_box" style="margin-left: 15px; text-align: left; padding-left: 10px; padding-bottom:5px; padding-top: 6px;" placeholder="Search" type="text" data-target=".search_table_auction">
     </div>
 </div>
