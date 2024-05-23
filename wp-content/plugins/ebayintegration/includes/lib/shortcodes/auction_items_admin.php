bb111e74d5423240d00eff0b13aab91796572d67
arhizsx <arhizsx@gmail.com>
2024-05-23 15:38:12 +0800

    Update Item Replacement SQL

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/auction_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/auction_items_admin.php
@@ -55,7 +55,7 @@ $users = get_users( $args );
             <tr>
                 <td>
                     <div class="title">
-                        <span class='pe-2'><?php $i ?></span>
+                        <span class='pe-2'><?php echo $i ?></span>
                         <a href="<?php echo $data['ListingDetails']['ViewItemURL'] ?>" target="_blank">
                         <?php echo  $data["Title"]; ?>
                         </a>
