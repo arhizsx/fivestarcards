c8282ab4960d5ba714945051f2292ba369d3ec20
arhizsx <arhizsx@gmail.com>
2024-05-23 15:55:18 +0800

    Update Item Replacement SQL

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/pending_payout_items_admin.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/pending_payout_items_admin.php
@@ -68,6 +68,7 @@ $users = get_users( $args );
                     <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>
                     <?php $listing = $data["Item"]["ListingType"] == "Chinese" ? "Auction" : $data["Item"]["ListingType"]; ?>
                     <div class="item_id text-small">Listing Type: <?php echo $listing; ?></div>                    
+                    <div class="item_id text-small">ID: <?php echo $item->id ?></div>                    
 
                     
                 </td>
