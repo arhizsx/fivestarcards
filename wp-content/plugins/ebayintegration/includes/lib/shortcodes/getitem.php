7b6073f6118b81549daa280c5677b3dbc1d9a3ea
arhizsx <arhizsx@gmail.com>
2024-06-02 11:36:07 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/getitem.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/getitem.php
@@ -30,7 +30,7 @@
 ?>
 
 <div>
-    <button class="ebayintegration-btn" data-action="getItems" data-per_page="<?php echo $this->per_page ?><">Refresh Active eBay Items</button>
+    <!-- <button class="ebayintegration-btn" data-action="getItems" data-per_page="<?php echo $this->per_page ?><">Refresh Active eBay Items</button> -->
     <!-- <button class="ebayintegration-btn" data-action="refreshToken">Reconnect to eBay</button> -->
 </div>		
 <div class="ebayintegration-items_box">
