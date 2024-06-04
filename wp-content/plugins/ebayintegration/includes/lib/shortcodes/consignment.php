240ec6139da97a5585bf365cc92f5c723594bff6
arhizsx <arhizsx@gmail.com>
2024-06-04 14:16:03 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
@@ -8,10 +8,10 @@
 
 <ul class="nav nav-pills nav-fill">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
+      <a class="nav-link" href="#">Much longer nav link</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Much longer nav link</a>
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Link</a>
