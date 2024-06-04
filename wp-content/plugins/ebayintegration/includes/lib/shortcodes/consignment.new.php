dc612234666ebd3c380ff2898d787d3dd7a7fd9d
arhizsx <arhizsx@gmail.com>
2024-06-04 17:13:53 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.new.php
@@ -1,7 +1,7 @@
 <button class="5star_btn btn btn-success mb-3" data-type="psa-value_bulk" data-action="add_card">
     Log Card
 </button>
-<button class="5star_btn btn btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
+<button class="5star_btn btn btn-primary mb-3" data-type="psa-value_bulk" data-action="add_card">
     Ship Batch
 </button>
 <table class="table table-sm table-bordered">
