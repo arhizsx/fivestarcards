5783d8219ee824cb65800648ee185124c2775712
arhizsx <arhizsx@gmail.com>
2024-06-04 18:13:39 +0800

    ebay items update

---
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/css.php
@@ -0,0 +1,48 @@
+<style>
+
+    .shortcode_tab_box {
+        box-shadow: 0px 2px 18px 0px rgba(0,0,0,0.3);
+    }
+
+    .shortcode_tab_box H1 {
+        color: black;
+    }
+    
+    .shortcode_tab_box ul {
+        list-style-type: none;
+        padding: 0 !important;
+        line-height: inherit !important;        
+        background-color: #f4f4f4;
+        height: auto !important;
+        word-wrap: break-word;
+        box-sizing: border-box;
+        margin-top: 0;
+        margin-bottom: 1rem;
+    }
+
+    .shortcode_tab_box li {
+        float: left;
+        border-right: none;
+        border-bottom: none;
+        display: table;
+        height: 72px;
+        position: relative;
+        line-height: 4em;
+        font-weight: bold;
+    }
+    .shortcode_tab_box li a {
+        text-decoration: none;
+        padding: 4px 30px;
+        vertical-align: middle;
+        display: table-cell;
+        color: #666;
+    }
+
+    .shortcode_tab_box li.active {
+        background-color: #fff;
+        font-weight: bold;
+        border-bottom: none;
+
+    }
+
+</style>
