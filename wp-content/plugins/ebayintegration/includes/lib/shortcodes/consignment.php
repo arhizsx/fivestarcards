38b0930b944631725ff318e0bd8a7fc3d6f19a69
arhizsx <arhizsx@gmail.com>
2024-06-04 15:53:52 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
@@ -61,7 +61,7 @@
 
   
 <div class="shortcode_tab_box">
-    <ul class="">
+    <ul class="clearfix">
         <li class="">
             <a class="" href="#">Much longer nav link</a>
         </li>
