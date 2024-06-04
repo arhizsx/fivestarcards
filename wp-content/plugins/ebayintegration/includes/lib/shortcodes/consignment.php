63526759a8a215d462f48e59666ee89e74030709
arhizsx <arhizsx@gmail.com>
2024-06-04 14:34:26 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
@@ -1,24 +1,30 @@
 <style>
-    ul {
-        list-style-type: none !important;
+
+    .shortcode_tab_box {
         box-shadow: 0px 2px 18px 0px rgba(0,0,0,0.3);
     }
+    
+    .shortcode_tab_box ul {
+        list-style-type: none;
+    }
+
 </style>
 
 
   
-
-<ul class="nav nav-pills nav-fill">
-  <li class="nav-item">
-      <a class="nav-link" href="#">Much longer nav link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
+<div class="shortcode_tab_box">
+    <ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+        <a class="nav-link" href="#">Much longer nav link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </li>
+    </ul>
+</div>
