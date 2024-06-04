fa5bca1411fc6ad53d3487e2562198cf7f60a183
arhizsx <arhizsx@gmail.com>
2024-06-04 17:00:10 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.sold.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.sold.php
@@ -1,1 +1,18 @@
-<H1>Sold</H1>
+<table class="table table-sm table-bordered">
+    <thead>
+        <tr>
+            <th>Qty</th>
+            <th>Year</th>
+            <th>Brand</th>
+            <th>Card Number</th>
+            <th>Player Name</th>
+            <th>Attribute S/N</th>
+            <th>Declared Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td colspan="7"></td>
+        </tr>
+    </tbody>
+</table>
