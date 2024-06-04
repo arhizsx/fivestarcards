19803ab748b972b693c04544a56593311266d706
arhizsx <arhizsx@gmail.com>
2024-06-04 16:54:58 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
@@ -3,6 +3,10 @@
     .shortcode_tab_box {
         box-shadow: 0px 2px 18px 0px rgba(0,0,0,0.3);
     }
+
+    .shortcode_tab_box H1 {
+        color: black;
+    }
     
     .shortcode_tab_box ul {
         list-style-type: none;
