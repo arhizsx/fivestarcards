da9e0a8f4838cfd4c955840d18091523393ae7e2
arhizsx <arhizsx@gmail.com>
2024-06-04 17:26:47 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.php
@@ -66,37 +66,46 @@
 
 
 ?>
-  
-<div class="shortcode_tab_box">
-    <ul class="clearfix d-none d-lg-block">
-        <li class="<?php echo Activate("new"); ?>">
-            <a class="" href="/my-account/my-consignment">New</a>
-        </li>
-        <li class="<?php echo Activate("to-ship"); ?>">
-            <a class="" href="/my-account/my-consignment/?mode=to-ship">To Ship</a>
-        </li>
-        <li class="<?php echo Activate("consigned"); ?>">
-            <a class="" href="/my-account/my-consignment/?mode=consigned">Consigned</a>
-        </li>
-        <li class="<?php echo Activate("sold"); ?>">
-            <a class="" href="/my-account/my-consignment/?mode=sold">Sold</a>
-        </li>
-    </ul>
-    <ul class="clearfix d-lg-none">
-        <li class="">
-            <a class="" href="/my-account/my-consignment">New</a>
-        </li>
 
-    </ul>
-    <div class="content p-3">
-        <?php 
-        if(isset( $_GET['mode']) ){
-			include( plugin_dir_path( __FILE__ ) . "consignment." . $_GET['mode'] . '.php' );			
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-2">
+            Side Menu
+        </div>
+        <div class="col-10">
+            <div class="shortcode_tab_box">
+                <ul class="clearfix d-none d-lg-block">
+                    <li class="<?php echo Activate("new"); ?>">
+                        <a class="" href="/my-account/my-consignment">New</a>
+                    </li>
+                    <li class="<?php echo Activate("to-ship"); ?>">
+                        <a class="" href="/my-account/my-consignment/?mode=to-ship">To Ship</a>
+                    </li>
+                    <li class="<?php echo Activate("consigned"); ?>">
+                        <a class="" href="/my-account/my-consignment/?mode=consigned">Consigned</a>
+                    </li>
+                    <li class="<?php echo Activate("sold"); ?>">
+                        <a class="" href="/my-account/my-consignment/?mode=sold">Sold</a>
+                    </li>
+                </ul>
+                <ul class="clearfix d-lg-none">
+                    <li class="">
+                        <a class="" href="/my-account/my-consignment">New</a>
+                    </li>
 
-        } else {
-			include( plugin_dir_path( __FILE__ ) . 'consignment.new.php' );			
+                </ul>
+                <div class="content p-3">
+                    <?php 
+                    if(isset( $_GET['mode']) ){
+                        include( plugin_dir_path( __FILE__ ) . "consignment." . $_GET['mode'] . '.php' );			
 
-        }
-        ?>
+                    } else {
+                        include( plugin_dir_path( __FILE__ ) . 'consignment.new.php' );			
+
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
