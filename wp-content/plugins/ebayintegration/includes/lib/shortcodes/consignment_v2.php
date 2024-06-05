505c1e9a35eaa4b8c3692c30b7ddca8694ff9e6c
arhizsx <arhizsx@gmail.com>
2024-06-05 08:55:46 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment_v2.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment_v2.php
@@ -22,7 +22,7 @@
             </a>
             <a href="/my-account/request-cashout" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-money-bill me-2"></i>
-                Request Cashout
+                Cashout
             </a>
         </div>
     </div>
