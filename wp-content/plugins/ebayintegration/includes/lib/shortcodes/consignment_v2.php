38ddd507eb5d00dcc9068816be215befd97deb9c
arhizsx <arhizsx@gmail.com>
2024-06-05 08:49:11 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment_v2.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment_v2.php
@@ -7,25 +7,28 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-xl-2 col-lg-3">
-            <a href="/my-account-v2" class="5star_btn btn text-left form-control btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+        <div class="col-12">
+            <a href="/my-account-v2" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-list me-2"></i>
                 My Listing
             </a>
-            <a href="/my-account/grading-order" class="5star_btn btn text-left form-control btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account/grading-order" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-circle-dot me-2"></i>                
                 Grading Orders
             </a>
-            <a href="/my-account/consignment" class="5star_btn btn text-left form-control btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account/consignment" class="5star_btn btn text-left btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-handshake me-2"></i>
                 Consignment
             </a>
-            <a href="/my-account/request-cashout" class="5star_btn btn text-left form-control btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account/request-cashout" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-money-bill me-2"></i>
                 Request Cashout
             </a>
         </div>
-        <div class="col-xl-10 col-lg-9">
+    </div>
+
+    <div class="row">
+        <div class="col-xl-12 col-lg-12">
             <div class="shortcode_tab_box">
                 <ul class="clearfix d-none d-lg-block">
                     <li class="<?php echo Activate("new"); ?>">
