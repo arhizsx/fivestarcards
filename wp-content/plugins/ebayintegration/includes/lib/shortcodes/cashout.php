61b5da5971ac3bc01ba20c6b6fcd265625dad18a
arhizsx <arhizsx@gmail.com>
2024-06-05 16:57:53 +0800

    DMO Update

---
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/cashout.php
@@ -0,0 +1,46 @@
+
+<?php 
+
+    include( plugin_dir_path( __FILE__ ) . "css.php" );			
+
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <H3 style="color: black;">Request Cashout</H3>
+        </div>
+        <div class="col-12">
+            <a href="/my-account" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+                <i class="fa-solid fa-list me-2"></i>
+                My Listings
+            </a>
+            <a href="/my-account/grading" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+                <i class="fa-solid fa-circle-dot me-2"></i>                
+                Grading
+            </a>
+            <a href="/my-account/consignment" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+                <i class="fa-solid fa-handshake me-2"></i>
+                Consignment
+            </a>
+            <a href="/my-account/request-cashout" class="5star_btn btn text-left btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
+                <i class="fa-solid fa-money-bill me-2"></i>
+                Cashout
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-12 col-lg-12">
+            <div class="shortcode_tab_box">
+                <ul class="clearfix d-none d-lg-block">
+                    <li class="active">
+                        <a class="" href="/my-account/cashout">Request Cashout</a>
+                    </li>
+                </ul>
+                <div class="content p-3 text-center">
+                    <H1>Coming Soon</H1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
