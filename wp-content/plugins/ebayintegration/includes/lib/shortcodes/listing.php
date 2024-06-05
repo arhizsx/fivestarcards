565316ee14ab5a12672f16b9ce4c99b4db226fda
arhizsx <arhizsx@gmail.com>
2024-06-05 08:47:13 +0800

    ebay items update

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/listing.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/listing.php
@@ -8,19 +8,19 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <a href="/my-account-v2" class="5star_btn btn text-left form-control btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account-v2" class="5star_btn btn text-left  btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-list me-2"></i>
                 My Listing
             </a>
-            <a href="/my-account/grading-order" class="5star_btn btn text-left form-control btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account/grading-order" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-circle-dot me-2"></i>                
                 Grading Orders
             </a>
-            <a href="/my-account/consignment" class="5star_btn btn text-left form-control btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account/consignment" class="5star_btn btn text-left  mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-handshake me-2"></i>
                 Consignment
             </a>
-            <a href="/my-account/request-cashout" class="5star_btn btn text-left form-control btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/my-account/request-cashout" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-money-bill me-2"></i>
                 Request Cashout
             </a>
