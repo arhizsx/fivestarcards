13f6a01164b0c8464ab6f1174b1bc3f00112b0f6
arhizsx <arhizsx@gmail.com>
2024-06-13 00:51:45 +0800

    Member View

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/css.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/css.php
@@ -105,7 +105,7 @@ function ActivateGrading($page){
 
 function AdministratorGrading($page){
     if( isset( $_GET['mode']) == false ){
-        if( $page == "receiving" ){
+        if( $page == "open" ){
             return "active";
         }
     } 
