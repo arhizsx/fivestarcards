af35e741176cd1b84617fc15feb4a38b2f6b8806
arhizsx <arhizsx@gmail.com>
2024-06-13 02:15:36 +0800

    View Order

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/listing.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/listing.php
@@ -50,7 +50,7 @@
                     </li>
                 </ul>
                 <div class="d-lg-none p-3">
-                    <label>Select Page</label>
+                    <label>Listing Type / Status</label>
                     <select class="form-control" id="mobile_tab_select">
                         <option value="/my-account-v2" <?php echo ActivateListingSelect("auction_items") ?>>Auction</option>
                         <option value="/my-account-v2/?mode=fixed_price_items" <?php echo ActivateListingSelect("fixed_price_items") ?>>Fixed Price</option>
