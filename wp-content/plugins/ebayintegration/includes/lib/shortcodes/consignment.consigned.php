97062a861025343bcc63893fef8bb7a06bd7a864
arhizsx <arhizsx@gmail.com>
2024-06-13 12:28:25 +0800

    Consign,emt Mew

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.consigned.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.consigned.php
@@ -1,19 +1,21 @@
-<table class="table table-sm table-bordered">
-    <thead>
-        <tr>
-            <th>Qty</th>
-            <th>Year</th>
-            <th>Brand</th>
-            <th>Card Number</th>
-            <th>Player Name</th>
-            <th>Attribute S/N</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td colspan="6" class="text-center py-5">
-                Empty
-            </td>
-        </tr>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-sm table-bordered">
+        <thead>
+            <tr>
+                <th>Qty</th>
+                <th>Year</th>
+                <th>Brand</th>
+                <th>Card Number</th>
+                <th>Player Name</th>
+                <th>Attribute S/N</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colspan="6" class="text-center py-5">
+                    Empty
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
