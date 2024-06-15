0d8c0561dcd399d35ff56ee86139f01a77c75332
arhizsx <arhizsx@gmail.com>
2024-06-15 08:29:54 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.listed.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.listed.php
@@ -1,11 +1,13 @@
 
 <div class="container-fluid">
     <div class="row">
+        <div class="col">
         <a class="btn btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=auction">Auction</a>
         <a class="btn btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=fixed_price">Fixed Price</a>
         <a class="btn btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=awaiting_payment">Awaiting Payment</a>
         <a class="btn btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=pending_payout">Pending Payout</a>
         <a class="btn btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=paid_out">Paid Out</a>
+        </div>
     </div>
     <div class="row">
     </div>
