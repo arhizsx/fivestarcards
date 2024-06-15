92f099d14c2d2d78261fc3d056e737d279e6e728
arhizsx <arhizsx@gmail.com>
2024-06-15 08:17:26 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.listings.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.listings.php
@@ -1,27 +1,6 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-12">
-            <H3 style="color: black;">My Listings</H3>
-        </div>
-        <div class="col-12">
-            <a href="/my-account" class="5star_btn btn text-left  btn-dark mb-3">
-                <i class="fa-solid fa-list me-2"></i>
-                My Listings
-            </a>
-            <a href="/my-account/grading" class="5star_btn btn text-left  btn-secondary mb-3">
-                <i class="fa-solid fa-circle-dot me-2"></i>                
-                Grading
-            </a>
-            <a href="/my-account/consignment" class="5star_btn btn text-left btn-secondary mb-3">
-                <i class="fa-solid fa-handshake me-2"></i>
-                Consignment
-            </a>
-            <a href="/my-account/cashout" class="5star_btn btn text-left btn-secondary mb-3">
-                <i class="fa-solid fa-money-bill me-2"></i>
-                Cashout
-            </a>
-        </div>
     </div>
     <div class="row">
     </div>
