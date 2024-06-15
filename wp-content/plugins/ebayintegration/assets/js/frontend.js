b279000be2c06801bd981fd2f335cd3b747bd2e3
arhizsx <arhizsx@gmail.com>
2024-06-15 22:55:35 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/assets/js/frontend.js
+++ wp-content/plugins/ebayintegration/assets/js/frontend.js
@@ -192,7 +192,18 @@ jQuery( document ).on("click", ".ebayintegration-btn", function(e){
 		$.when(card).done( function( card ){
 
 			$.each( card, function(k, v){
-				console.log(v);
+
+				$(document).find("#new_consignment tbody").append(
+					"<tr>" +
+						"<td>" + v.qty + "</td>" +
+						"<td>" + v.year + "</td>" +
+						"<td>" + v.brand + "</td>" +
+						"<td>" + v.card_number + "</td>" +
+						"<td>" + v.player_name + "</td>" +
+						"<td>" + v.attribute_sn + "</td>" +
+					"</tr>"
+				)
+
 			} );
 
 
