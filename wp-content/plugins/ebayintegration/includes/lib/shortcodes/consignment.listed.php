ee181b1ca13c2e8751c5b98b3283af296b94723d
arhizsx <arhizsx@gmail.com>
2024-06-15 08:26:20 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.listed.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/consignment.listed.php
@@ -1,11 +1,11 @@
 
 <div class="container-fluid">
     <div class="row">
-        <a href="/my-account/consignment/?mode=listed&type=auction">Auction</a>
-        <a href="/my-account/consignment/?mode=listed&type=fixed_price">Fixed Price</a>
-        <a href="/my-account/consignment/?mode=listed&type=awaiting_payment">Awaiting Payment</a>
-        <a href="/my-account/consignment/?mode=listed&type=pending_payout">Pending Payout</a>
-        <a href="/my-account/consignment/?mode=listed&type=paid_out">Paid Out</a>
+        <a class="btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=auction">Auction</a>
+        <a class="btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=fixed_price">Fixed Price</a>
+        <a class="btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=awaiting_payment">Awaiting Payment</a>
+        <a class="btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=pending_payout">Pending Payout</a>
+        <a class="btn-pill btn-primary" href="/my-account/consignment/?mode=listed&type=paid_out">Paid Out</a>
     </div>
     <div class="row">
     </div>
