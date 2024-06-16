792b53338087be0d30144e2f77f1f322b98d4335
arhizsx <arhizsx@gmail.com>
2024-06-16 22:55:46 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
@@ -79,7 +79,14 @@ if( $show == "cards" ){
                 <td><?php echo $data["player_name"] ?></td>
                 <td><?php echo $data["card_number"] ?></td>
                 <td><?php echo $data["attribute_sn"] ?></td>
-                <td class="text-end">-</td>
+                <td class="text-end">
+                    <a class="btn btn-pill btn-sm btn-primary">
+                        Received
+                    </a>
+                    <a class="btn btn-pill btn-sm btn-secondary">
+                        Not Avlb
+                    </a>
+                </td>
             </tr>
             <?php 
                     }
