7b6efdd65d435849a7d304d12d3af7c498f6e663
arhizsx <arhizsx@gmail.com>
2024-06-16 22:14:07 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
@@ -1,3 +1,45 @@
+<?php 
+
+global $wpdb;
+
+if( ! isset( $_GET['type'] ) ){
+
+    $consignment = $this->wpdb->get_results ( "
+        SELECT * 
+        FROM consignment
+        where user_id = " . get_current_user_id() . " 
+        and status = 'shipped'
+        order by order_id desc, id desc
+        "
+    );
+
+    $show = "cards";
+    $btn_cards = 'btn-primary';
+    $btn_orders = 'btn-secondary';
+
+} else {
+
+    $orders = $this->wpdb->get_results ( "
+        SELECT * 
+        FROM consignment_orders
+        where user_id = " . get_current_user_id() . " 
+        order by id desc
+        "
+    );
+
+    $show = "orders";
+    $btn_cards = 'btn-secondary';
+    $btn_orders = 'btn-primary';
+}
+?>
+
+<div class="row">
+    <div class="col">
+        <a class="btn btn-pill btn-sm mb-2 <?php echo $btn_cards; ?>" href="/administrator/consignment/?mode=to-ship">Cards</a>
+        <a class="btn btn-pill btn-sm mb-2 <?php echo $btn_orders; ?>" href="/administrator/consignment/?mode=to-ship&type=orders">Orders</a>
+    </div>
+</div>
+
 <div class="table-responsive">
     <table class="table table-sm table-bordered">
         <thead>
