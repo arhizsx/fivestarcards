4b9818e8c704b5e39821c9f37345e04caa0f491a
arhizsx <arhizsx@gmail.com>
2024-06-16 22:04:17 +0800

    Consignment Module

---
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.consigned.php
@@ -0,0 +1,21 @@
+<div class="table-responsive">
+    <table class="table table-sm table-bordered">
+        <thead>
+            <tr>
+                <th>Order ID</th>
+                <th>Year</th>
+                <th>Brand</th>
+                <th>Card Number</th>
+                <th>Player Name</th>
+                <th>Attribute S/N</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colspan="6" class="text-center py-5">
+                    Empty
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
