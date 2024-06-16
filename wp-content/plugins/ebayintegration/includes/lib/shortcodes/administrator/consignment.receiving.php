13dd1aa7e7d3832ea45fa9d211754421a94c0985
arhizsx <arhizsx@gmail.com>
2024-06-16 22:33:29 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
@@ -40,10 +40,15 @@ if( ! isset( $_GET['type'] ) ){
     </div>
 </div>
 
+
+<?php 
+if( $show == "cards" ){
+?>
 <div class="table-responsive">
     <table class="table table-sm table-bordered">
         <thead>
             <tr>
+                <th>User</th>
                 <th>Order ID</th>
                 <th>Year</th>
                 <th>Brand</th>
@@ -62,3 +67,32 @@ if( ! isset( $_GET['type'] ) ){
         </tbody>
     </table>
 </div>
+<?php     
+} else {
+?>
+<div class="table-responsive">
+    <table class="table table-sm table-bordered">
+        <thead>
+            <tr>
+                <th>User</th>
+                <th>Order ID</th>
+                <th>Year</th>
+                <th>Brand</th>
+                <th>Card Number</th>
+                <th>Player Name</th>
+                <th>Attribute S/N</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td colspan="6" class="text-center py-5">
+                    Empty
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<?php     
+}
+?>
