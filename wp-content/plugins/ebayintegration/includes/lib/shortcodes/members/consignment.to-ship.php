f2fd00d2ad81b806f5d0fc90c91a40b33bdd44a6
arhizsx <arhizsx@gmail.com>
2024-06-16 18:47:50 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.to-ship.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.to-ship.php
@@ -1,8 +1,23 @@
+<?php 
+
+global $wpdb;
+
+$consignment = $this->wpdb->get_results ( "
+SELECT * 
+FROM consignment
+where user_id = " . get_current_user_id() . " 
+and status = 'shipped'
+order by order_id desc, id desc
+"
+);
+
+?>
+
 <div class="table-responsive">
     <table class="table table-sm table-bordered">
         <thead>
             <tr>
-                <th>Batch</th>
+                <th>Order ID</th>
                 <th>Year</th>
                 <th>Brand</th>
                 <th>Card Number</th>
