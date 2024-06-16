01c83a8bb1db407a633845f75267c53ea712a973
arhizsx <arhizsx@gmail.com>
2024-06-16 23:14:56 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
@@ -143,7 +143,7 @@ if( $show == "cards" ){
                     <td><?php echo $order->status; ?></td>
                     <td class="fit">
                         <a class="btn btn-pill btn-sm btn-success">
-                            <i class="fa-solid fa-check"></i>
+                            <i class="fa-solid fa-check"></i>All
                         </a>
                     </td>
                 </tr>
