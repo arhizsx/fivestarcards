b2292d02767d0db390465ced16ceb7bc305b0d86
arhizsx <arhizsx@gmail.com>
2024-06-16 22:05:04 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.consigned.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.consigned.php
@@ -8,11 +8,12 @@
                 <th>Card Number</th>
                 <th>Player Name</th>
                 <th>Attribute S/N</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td colspan="6" class="text-center py-5">
+                <td colspan="7" class="text-center py-5">
                     Empty
                 </td>
             </tr>
