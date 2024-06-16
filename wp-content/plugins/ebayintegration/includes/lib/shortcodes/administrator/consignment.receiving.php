2205f06309930658a461cbbd9c18da5dce0768e0
arhizsx <arhizsx@gmail.com>
2024-06-16 23:19:41 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.receiving.php
@@ -140,7 +140,7 @@ if( $show == "cards" ){
                     <td><?php echo $data["shipped_by"]; ?></td>
                     <td><?php echo $data["tracking_number"]; ?></td>
                     <td><?php echo $data["shipping_date"]; ?></td>
-                    <td><?php echo $order->status; ?></td>
+                    <td><?php echo strtoupper($order->status); ?></td>
                     <td class="fit">
                         <a class="btn btn-pill btn-sm btn-success">
                             <i class="fa-solid fa-check me-2"></i>All
