28030b20eabe156f252abc6ea9ecd0c49cda6c26
arhizsx <arhizsx@gmail.com>
2024-06-17 00:20:45 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.consigned.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.consigned.php
@@ -100,7 +100,7 @@ if( $show == "cards" ){
                 <td><?php echo $data["player_name"] ?></td>
                 <td><?php echo $data["card_number"] ?></td>
                 <td><?php echo $data["attribute_sn"] ?></td>
-                <td><?php echo $card->status ?></td>
+                <td><?php echo strtoupper($card->status) ?></td>
                 <td class="fit">
                     <a class="btn btn-pill btn-sm btn-danger ebayintegration-btn" data-action="confirmConsignedCardNotAvailable" data-id="<?php echo $card->id ?>">
                         <i class="fa-solid fa-xmark"></i>
