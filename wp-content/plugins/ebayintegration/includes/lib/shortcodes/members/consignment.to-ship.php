8340178d53df9df27694614e2cd2502e6097e18b
arhizsx <arhizsx@gmail.com>
2024-06-16 18:50:53 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.to-ship.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.to-ship.php
@@ -20,17 +20,38 @@ order by order_id desc, id desc
                 <th>Order ID</th>
                 <th>Year</th>
                 <th>Brand</th>
-                <th>Card Number</th>
                 <th>Player Name</th>
+                <th>Card Number</th>
                 <th>Attribute S/N</th>
             </tr>
         </thead>
         <tbody>
+            <?php
+                if( count( $consignment ) == 0 ){
+            ?>
             <tr>
-                <td colspan="7" class="text-center py-5">
+                <td colspan="6" class="text-center py-5">
                     Empty
                 </td>
             </tr>
+            <?php 
+                } else {
+                    foreach( $consignment as $card ){
+                        $data = json_decode( $card->data, true );
+            ?>
+                <tr>
+                    <td><?php echo $data['order_id'] + 1000; ?></td>
+                    <td><?php echo $data["year"] ?></td>
+                    <td><?php echo $data["brand"] ?></td>
+                    <td><?php echo $data["player_name"] ?></td>
+                    <td class='text-end'><?php echo $data["card_number"] ?></td>
+                    <td class='text-end'><?php echo $data["attribute_sn"] ?></td>
+                </tr>
+            <?php 
+                    }
+                }
+            ?>
+
         </tbody>
     </table>
 </div>
