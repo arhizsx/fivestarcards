42e392020abfee83f10db4a4d3de37a370e4d59a
arhizsx <arhizsx@gmail.com>
2024-06-17 11:00:46 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.consigned.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.consigned.php
@@ -34,7 +34,7 @@ if( ! isset( $_GET['type'] ) ){
 
 <div class="row">
     <div class="col">
-        <a class="btn btn-pill btn-sm mb-2 <?php echo $btn_cards; ?>" href="/my-account/consignment/?mode=cconsigned">Cards</a>
+        <a class="btn btn-pill btn-sm mb-2 <?php echo $btn_cards; ?>" href="/my-account/consignment/?mode=consigned">Cards</a>
         <a class="btn btn-pill btn-sm mb-2 <?php echo $btn_orders; ?>" href="/my-account/consignment/?mode=consigned&type=orders">Orders</a>
     </div>
 </div>
