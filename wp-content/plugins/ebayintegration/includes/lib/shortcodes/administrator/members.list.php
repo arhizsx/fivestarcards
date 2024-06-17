3ab74ac7e55576289ed438da8da9b262ded0d035
arhizsx <arhizsx@gmail.com>
2024-06-17 20:25:17 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/members.list.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/members.list.php
@@ -172,6 +172,10 @@
                                 <label>Email</label>
                                 <input type="text" class="form-control p-2 mb-3">
                             </div>
+                            <div class="col-md-12">
+                                <button class="btn btn-primary">Update</button>
+                                <button class="btn btn-primary">Update</button>
+                            </div>
                         </div>
                     </div>          
                 </div>
