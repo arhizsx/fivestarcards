218b539d9ef10f73733fddb05169e86637d73739
arhizsx <arhizsx@gmail.com>
2024-06-17 15:48:46 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.fixed_price.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.fixed_price.php
@@ -9,7 +9,14 @@ where status = 'ActiveList'
 "  
 );
 
-$skus = get_user_meta( get_current_user_id(), "sku", true );		
+
+$args = array(
+    'orderby'    => 'display_name',
+    'order'      => 'ASC'
+);
+
+$users = get_users( $args );
+
 
 ?>
 <style>
@@ -17,60 +24,64 @@ $skus = get_user_meta( get_current_user_id(), "sku", true );
         font-size: .7em !important;
     }
 </style>
-<div class="d-flex flex-row-reverse mb-3">
-    <input class="btn pl-2 search_box" style="text-align: left; padding-left: 10px; padding-bottom:5px; padding-top: 6px;" placeholder="Search" type="text" data-target=".search_table_fixed_price">
+<div class="d-flex justify-content-between mb-3">
+    <div>
+        <i class="fa-brands fa-ebay fa-2xl"></i> FIXED PRICE
+    </div>
+    <div class="d-flex justify-content-between mb-3">
+        <input class="btn pl-2 search_box" style="margin-left: 15px; text-align: left; padding-left: 10px; padding-bottom:5px; padding-top: 6px;" placeholder="Search" type="text" data-target=".search_table_fixed_price">
+    </div>
 </div>
-<?php 
-    $available = 0;
-    foreach($ebay as $item){ 
-        $data = json_decode($item->data, true);
-        if( $data["ListingType"] != "Chinese"){
-            if( in_array( $item->sku, $skus ) ){
-                $available++;
-            }
-        }
-    }
-?>
 
-<div class="table-responsive">
+<div class="table-responsive">    
     <table class="table table-border table-striped table-sm table-hover search_table_fixed_price">
         <thead>
             <tr>
                 <th>Item</th>
+                <th class="text-end">Sold</th>
+                <th class="text-end">Avlb</th>
                 <th class="text-end">Watchers</th>
                 <th class="text-end">Buy Price</th>
             </tr>
         </thead>
         <tbody>
             <?php 
-            if( $available > 0 ){
+            if( count($ebay) > 0 ){
+
+                $i = 0;
 
                 foreach($ebay as $item){ 
                     $data = json_decode($item->data, true);
 
                     if( $data["ListingType"] != "Chinese"){
-                        if( in_array( $item->sku, $skus ) ){
-
+                        $i++;
             ?>
             <tr>
                 <td>
                     <div class="title">
+                        <span class='pe-2'><strong><?php echo $i ?></strong></span>
                         <a href="<?php echo $data['ListingDetails']['ViewItemURL'] ?>" target="_blank">
                         <?php echo $data["Title"]; ?>
                         </a>
                     </div>
                     <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
                     <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>                    
+                    <div class="item_id text-small">ID: <?php echo $item->id ?></div>                    
                 </td>
-                <td class="text-center">
+                <td class="text-end">
+                <?php echo $data["SellingStatus"]["QuantitySold"] * 1; ?>
+                </td>
+                <td class="text-end">
+                    <?php echo $data["QuantityAvailable"] * 1; ?>
+                </td>
+                <td class="text-end">
                     <?php echo $data["WatchCount"] * 1; ?>
                 </td>
                 <td class="text-end">$<?php 
-                    echo number_format(( $data["SellingStatus"]["CurrentPrice"]), 2, '.', ',');
+                echo number_format(( $data["SellingStatus"]["CurrentPrice"]), 2, '.', ',');
                 ?></td>
             </tr>
             <?php 
-                        }
                     }
                 }
 
@@ -79,7 +90,7 @@ $skus = get_user_meta( get_current_user_id(), "sku", true );
             ?>
             <tr>
                 <td colspan="2" class="text-center p-5">
-                    Empty
+                    No Item
                 </td>
             </tr>
             <?php 
