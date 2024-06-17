8428a6a850b93af6cb9699b3c8bf2552973f0a14
arhizsx <arhizsx@gmail.com>
2024-06-17 20:06:29 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/members.list.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/members.list.php
@@ -146,8 +146,12 @@
     			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
 			</div>
             <div class="modal-body py-2 px-3">
-                <div class="row formbox">
-                <H1 style="color: black !important;">Member Information</H1>
+                <div class="formbox">
+                    <div class="btn-group" role="group" aria-label="Basic outlined example">
+                        <button type="button" class="btn btn-outline-primary">Left</button>
+                        <button type="button" class="btn btn-outline-primary">Middle</button>
+                        <button type="button" class="btn btn-outline-primary">Right</button>
+                    </div>                    
                 </div>
                 <div class="d-none p-5 text-center loading">
 
