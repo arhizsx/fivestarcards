a845b9b1b903fadce30e49d871cbba477a929c63
arhizsx <arhizsx@gmail.com>
2024-06-17 22:06:09 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/assets/js/frontend.js
+++ wp-content/plugins/ebayintegration/assets/js/frontend.js
@@ -504,8 +504,8 @@ jQuery( document ).on("click", ".ebayintegration-btn", function(e){
 
 		jQuery(document).find(".member_info_modal").prependTo('body').modal("show");
 
-		$(document).find(".member_info_modal").find(".member_view_menu").find("button").attr("data-user_id", "");
-		$(document).find(".member_info_modal").find(".member_view_menu").find("button").attr("data-user_id", element.data("user_id"));
+		$(document).find(".member_info_modal").find(".member_view_menu").find("button").data("user_id", "");
+		$(document).find(".member_info_modal").find(".member_view_menu").find("button").data("user_id", element.data("user_id"));
 
 	}
 
