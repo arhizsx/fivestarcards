09db115b6c5d201d762c510497ab3d81d6f8af7a
arhizsx <arhizsx@gmail.com>
2024-06-17 14:26:44 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.consigned.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.consigned.php
@@ -8,7 +8,6 @@ if( ! isset( $_GET['type'] ) ){
         SELECT * 
         FROM consignment
         where user_id = " . get_current_user_id() . " 
-        and status IN ('received', 'unavailable')
         order by order_id desc, status asc, id desc
         "
     );
