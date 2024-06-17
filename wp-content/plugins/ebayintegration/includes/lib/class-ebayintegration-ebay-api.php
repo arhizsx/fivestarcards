cadad73995deb409c350e295acd02d05790efa9b
arhizsx <arhizsx@gmail.com>
2024-06-17 21:46:55 +0800

    Consignment Module

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -280,7 +280,7 @@ class Ebay_Integration_Ebay_API {
 
 		$array = implode("','",$skus);
 
-		$sql = "SELECT * FROM ebay WHERE status = 'ActiveList' AND IN (" . $array . ")";
+		$sql = "SELECT * FROM ebay WHERE status = 'ActiveList' AND sku IN ('" . $array . "')";
 
 		$cards = $this->wpdb->get_results ( $sql );
 
