4981ebf5404f7bc1d9f30ee7ded2ca60dd9b0ef5
arhizsx <arhizsx@gmail.com>
2024-06-25 16:59:10 +0800

    Ebay Paid Out Button

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.pending_payout.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.pending_payout.php
@@ -70,7 +70,9 @@ $users = get_users( $args );
                 </td>
                 <td class="text-end">
                     <button class="btn btn-primary btn-sm mb-2 ebayintegration-btn" data-action="consignmentPaidOut" data-id="<?php echo $item->id ?>">PAID</button>
+                    <?php if( get_current_user_id() == 1 ){ ?>
                     <button class="btn btn-dark btn-sm ms-2 mb-2 ebayintegration-btn" data-action="consignmentPaidOutQueue" data-id="<?php echo $item->id ?>">QUEUE</button>
+                    <?php } ?>
                 </td>
             </tr>
             <?php
