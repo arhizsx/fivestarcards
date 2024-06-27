1f541dc2eb4d75136acdb4e05650305005b9a172
arhizsx <arhizsx@gmail.com>
2024-06-28 01:31:53 +0800

    New Grading

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
@@ -485,7 +485,7 @@
     <div class="col-12">
         <H2>SGC Grading</H2>
     </div>
-    <div class="col-md-4 text-center grading_box">
+    <div class="col-md-3 text-center grading_box">
         <table class="table table-bordered">
             <tr>
                 <td class='grading_title' style="background-color: #0c71c3; color: #ffffff;">Value Bulk</td>
