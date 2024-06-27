59e498c9849a09546cc972e925e56eaae3bbb57e
arhizsx <arhizsx@gmail.com>
2024-06-27 23:26:11 +0800

    New Grading

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
@@ -11,17 +11,22 @@
 <div class="row">
     <div class="col text-center grading_box">
         <H3>Value Bulk</H3>
+        <hr>
     </div>
     <div class="col text-center grading_box">
         <H3>Value Plus</H3>
+        <hr>
     </div>
     <div class="col text-center grading_box">
         <H3>Regular</H3>
+        <hr>
     </div>
     <div class="col text-center grading_box">
         <H3>Express</H3>
+        <hr>
     </div>
     <div class="col text-center grading_box">
         <H3>Super Express</H3>
+        <hr>
     </div>
 </div>
