d0bb42be55a6060740669eb855de535a0eaff01e
arhizsx <arhizsx@gmail.com>
2024-06-27 23:20:33 +0800

    New Grading

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
@@ -1,17 +1,17 @@
 <div class="row">
-    <div class="col">
+    <div class="col text-center">
         Value Bulk
     </div>
-    <div class="col">
+    <div class="col text-center">
         Value Plue
     </div>
-    <div class="col">
+    <div class="col text-center">
         Regular
     </div>
-    <div class="col">
+    <div class="col text-center">
         Express
     </div>
-    <div class="col">
+    <div class="col text-center">
         Super Express
     </div>
 </div>
