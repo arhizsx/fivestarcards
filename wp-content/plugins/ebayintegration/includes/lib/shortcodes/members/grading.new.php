3c9e87d202217fa4c7113bc098f2b85afc166cd5
arhizsx <arhizsx@gmail.com>
2024-06-28 09:30:57 +0800

    New Grading

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
@@ -144,7 +144,7 @@
                         <i class='fa-solid fa-lg fa-xmark'></i>
                     </a>
                 </td>
-                <td style="width: 100px;">
+                <td style="width: 100px; padding: 9px;">
                     <div style="width: 100%; height: 120px; background-color: lightgray;">
 
                     </div>
