70fbd79b3d0f1d427552a189a125a25b469e6de9
arhizsx <arhizsx@gmail.com>
2024-06-28 13:30:21 +0800

    New Grading

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
@@ -447,7 +447,7 @@
 	</div>
 </div>
 
-<!-- IMPORT MODAL -->
+<!-- CLEAR MODAL -->
 <div class="modal fade clear_grading_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px;">
 	<div class="modal-dialog" id="dxmodal">
 		<div class="modal-content modal-ajax">
@@ -484,6 +484,42 @@
 		</div>
 	</div>
 </div>
+
+<!-- CHECKOUT MODAL -->
+<div class="modal fade clear_grading_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px;">
+	<div class="modal-dialog" id="dxmodal">
+		<div class="modal-content modal-ajax">
+			<div class="modal-header bg-dark text-white">
+				<h5 class="modal-title">
+					Checkout
+				</h5>
+    			<button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
+					X
+				</button>
+			</div>
+            <div class="modal-body py-2 px-3">
+                <div class="row formbox">
+                    <div class="col-12 p-5">
+                        Are you sure want to checkout the logged cards?
+                    </div>
+                </div>
+                <div class="d-none p-5 text-center loading">
+
+                    Checking Out, please wait...
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn border btn-secondary" data-bs-dismiss="modal" >Cancel</button>
+                <button class="btn border btn-primary ebayintegration-btn" data-action='confirm_grading_table_checkout' >
+                    Yes
+                </button>
+            </div>
+
+		</div>
+	</div>
+</div>
+
 
 <!-- SHIP MODAL -->
 <div class="modal fade ship_batch_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px">
