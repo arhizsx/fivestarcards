9b92cee35df7429cb2ffd84837b9e1355d6909a4
arhizsx <arhizsx@gmail.com>
2024-06-28 09:30:23 +0800

    New Grading

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/grading.new.php
@@ -145,7 +145,7 @@
                     </a>
                 </td>
                 <td style="width: 100px;">
-                    <div style="width: 100%; height: 120px;">
+                    <div style="width: 100%; height: 120px; background-color: lightgray;">
 
                     </div>
                 </td>
