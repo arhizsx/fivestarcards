7e30ec048dbeb064a4fad17c2f2ee11c9cf98221
arhizsx <arhizsx@gmail.com>
2024-07-17 07:27:35 +0800

    Floating Button

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -104,7 +104,7 @@
                     $grd = "";
                 }
             ?>
-            <a href="/my-account/grading/<?php echo $grd; ?>" class="btn btn-outline-dark mb-3 ">Back to Grading Types</a>
+            <a href="/my-account/grading/new<?php echo $grd; ?>" class="btn btn-outline-dark mb-3 ">Back to Grading Types</a>
 
             <button class="btn btn-success mb-3 ebayintegration-btn" data-action="show_log_grading_modal">
                 Log Card
