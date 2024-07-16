0ddaa345623273a4681652d23d7b5302a65ab505
arhizsx <arhizsx@gmail.com>
2024-07-17 03:07:55 +0800

    Floating Button

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading2.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading2.php
@@ -94,9 +94,3 @@
         </div>
     </div>
 </div>
-<div class="floating-button-container text-center">
-    <button type="button" id="float_btn_add_ticket" class="floating-button btn btn-primary" data-action="add_grading" data-toggle="tooltip" data-placement="left" data-original-title="Add a Grading">
-        <i class="fa fa-plus fa-2x fa-w-16 fa-beat" style="margin-top: 10px;"></i>
-    </button>
-    <small style="border: 1px solid white; background-color: black; color: white; padding-top: 4px; padding-bottom: 4px; padding-left: 10px; padding-right: 10px;">NEW</small>
-</div>
