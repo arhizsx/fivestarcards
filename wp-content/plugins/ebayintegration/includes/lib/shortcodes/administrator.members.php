8f3db5c95023a04d739fe3e8673e95fae164a77b
arhizsx <arhizsx@gmail.com>
2024-07-18 06:56:41 +0800

    Photo Upload

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.members.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.members.php
@@ -8,9 +8,6 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <H3 style="color: black;">Members</H3>
-        </div>
-        <div class="col-12">
             <a href="/administrator" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-list me-2"></i>
                 Dashboard
