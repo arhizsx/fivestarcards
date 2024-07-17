b26146199c50a87a6220f0d2ed31ff19367cd56b
arhizsx <arhizsx@gmail.com>
2024-07-18 05:39:23 +0800

    Photo Upload

--- wp-content/plugins/ebayintegration/assets/js/frontend.js
+++ wp-content/plugins/ebayintegration/assets/js/frontend.js
@@ -438,7 +438,7 @@ jQuery( document ).on("click", ".ebayintegration-btn", function(e){
 					console.log(resp.data.file.baseurl);
 					console.log(resp.params.card_id);
 
-					$(document).find(".ebayintegration-btn[data-action='grading_picture_box_click'][data-id='" + resp.params.card_id + "']").css(
+					$(document).find(".ebayintegration-btn[data-action='grading_picture_box_click'][data-id='" + resp.params.card_id + "']").html(
 						"<img src='" + resp.data.file.baseurl + "' style='width:100%;height=100%;object-fit:contain;'>"
 					);
 
