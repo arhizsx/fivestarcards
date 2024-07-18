99a3415da59307fe9e2bcb674f7b20d9ca3a099b
arhizsx <arhizsx@gmail.com>
2024-07-18 18:14:43 +0800

    View Order Photo Add

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.php
@@ -5,6 +5,7 @@
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("fixed_price") ?>" href="/administrator/consignment/?mode=ebay&type=fixed_price">Fixed Price</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("awaiting_payment") ?>" href="/administrator/consignment/?mode=ebay&type=awaiting_payment">Awaiting Payment</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("pending_payout") ?>" href="/administrator/consignment/?mode=ebay&type=pending_payout">Pending Payout</a>
+            <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("cancelled") ?>" href="/administrator/consignment/?mode=ebay&type=cancelled">Cancelled</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("paid_out") ?>" href="/administrator/consignment/?mode=ebay&type=paid_out">Paid Out</a>
         </div>
     </div>
