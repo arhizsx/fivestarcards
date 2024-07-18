44ddfc98229d48304ec117d56cf66078bbb659af
arhizsx <arhizsx@gmail.com>
2024-07-18 22:26:04 +0800

    View Order Photo Add

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/refresh.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/refresh.php
@@ -1,23 +1,23 @@
 <div class="row">
     <div class="col-12">
-        <button class="ebayintegration-btn" data-action="refreshToken">Refresh Token</button>
+        <button class="ebayintegration-btn form-control btn btn-primary" data-action="refreshToken">Refresh Token</button>
     </div>
 </div>
 <div class="row">
     <div class="col-xl-3" id="active">
-        <button class="ebayintegration-btn" data-action="refreshActive">Active</button>
+        <button class="ebayintegration-btn form-control btn btn-primary" data-action="refreshActive">Active</button>
         <div class="pagebox"></div>
     </div>
     <div class="col-xl-3" id="awaiting">
-        <button class="ebayintegration-btn" data-action="refreshAwaiting">Awaiting</button>
+        <button class="ebayintegration-btn form-control btn btn-primary" data-action="refreshAwaiting">Awaiting</button>
         <div class="pagebox"></div>
     </div>
     <div class="col-xl-3" id="sold">
-        <button class="ebayintegration-btn" data-action="refreshSold">Sold</button>
+        <button class="ebayintegration-btn form-control btn btn-primary" data-action="refreshSold">Sold</button>
         <div class="pagebox"></div>
     </div>
     <div class="col-xl-3" id="unsold">
-        <button class="ebayintegration-btn" data-action="refreshUnsold">Unsold</button>
+        <button class="ebayintegration-btn form-control btn btn-primary" data-action="refreshUnsold">Unsold</button>
         <div class="pagebox"></div>
     </div>
 </div>
