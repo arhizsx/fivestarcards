11ea53187b4d6fd772b609ec0e7d3d27bcdde044
arhizsx <arhizsx@gmail.com>
2024-07-18 17:25:06 +0800

    View Order Photo Add

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -300,7 +300,7 @@ $processed_status = array("Processing Order", "Cards Graded");
 
                             $db_card_data = json_decode( $db_card[0]->data, true );
 
-                            print_r($db_card_data);
+                            print_r($db_card_data["file"]);
                     ?>
                     <td>image</td>
                     <td><?php echo $post->ID; ?></td>
