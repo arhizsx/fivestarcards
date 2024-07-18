57f914362190927149ef169f989db2fdbf00fcc4
arhizsx <arhizsx@gmail.com>
2024-07-18 09:47:12 +0800

    Login As

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/members.list.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/members.list.php
@@ -310,7 +310,7 @@
                         <div class="row">
                             <div class="col-md-12 mb-3">
                                 <label>Message</label>
-                                <textarea rows="10" class="form-control" name="message"></textarea>
+                                <textarea rows="5" class="form-control" name="message"></textarea>
                             </div>
                         </div>
                         <div class="row">
