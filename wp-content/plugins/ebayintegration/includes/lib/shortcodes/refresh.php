3f0bb54571c086c70dfa0adb0200ff7ee2b87eaa
arhizsx <arhizsx@gmail.com>
2024-07-18 23:26:31 +0800

    View Order Photo Add

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/refresh.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/refresh.php
@@ -4,21 +4,33 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-xl-3 border" id="active">
+    <div class="col-xl-3 border p-3" id="active">
         <H3 style="color:black;">Active</H3>
         <div class="pagebox"></div>
+        <div class="loading d-none">
+            Loading...
+        </div>
     </div>
-    <div class="col-xl-3  border" id="awaiting">
+    <div class="col-xl-3  border p-3" id="awaiting">
         <H3 style="color:black;">Awaiting</H3>
         <div class="pagebox"></div>
+        <div class="loading d-none">
+            Loading...
+        </div>
     </div>
-    <div class="col-xl-3  border" id="sold">
+    <div class="col-xl-3  border p-3" id="sold">
         <H3 style="color:black;">Sold</H3>
         <div class="pagebox"></div>
+        <div class="loading d-none">
+            Loading...
+        </div>
     </div>
-    <div class="col-xl-3  border" id="unsold">
+    <div class="col-xl-3  border p-3" id="unsold">
         <H3 style="color:black;">Unsold</H3>
         <div class="pagebox"></div>
+        <div class="loading d-none">
+            Loading...
+        </div>
     </div>
 </div>
 
