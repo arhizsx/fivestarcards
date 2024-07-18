0e3ebc3f39593873485647d9a09f340962c19183
arhizsx <arhizsx@gmail.com>
2024-07-18 19:23:13 +0800

    View Order Photo Add

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -1376,6 +1376,7 @@ class Ebay_Integration_Ebay_API {
 									'ebay', 
 									array(
 										'data'=> json_encode($item), 
+										'sku' => $SKU,
 										'status'=>$itemstatus,
 									), 
 									array(
@@ -1391,6 +1392,7 @@ class Ebay_Integration_Ebay_API {
 								'ebay', 
 								array(
 									'data'=> json_encode($item), 
+									'sku' => $SKU,
 									'status'=>$itemstatus,
 								), 
 								array(
