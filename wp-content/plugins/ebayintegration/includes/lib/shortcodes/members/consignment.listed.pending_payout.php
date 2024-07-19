d216d2d3de1aeb130b83495135fde13952c1059b
arhizsx <arhizsx@gmail.com>
2024-07-19 09:14:15 +0800

    Refresh

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.pending_payout.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.pending_payout.php
@@ -85,7 +85,7 @@ $skus = get_user_meta( get_current_user_id(), "sku", true );
             else {
             ?>
             <tr>
-                <td colspan="2" class="text-center p-5">
+                <td colspan="3" class="text-center p-5">
                     No Items
                 </td>
             </tr>
