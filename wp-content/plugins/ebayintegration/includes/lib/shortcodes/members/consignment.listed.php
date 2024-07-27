00cede83a4f4f1fdf2f22b700a5fe2a8cbf9b134
arhizsx <arhizsx@gmail.com>
2024-07-28 07:41:37 +0800

    Listed as default

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.php
@@ -5,7 +5,7 @@
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("auction") ?>" href="/my-account/consignment/?mode=listed&type=auction">Auction</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("fixed_price") ?>" href="/my-account/consignment/?mode=listed&type=fixed_price">Fixed Price</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("awaiting_payment") ?>" href="/my-account/consignment/?mode=listed&type=awaiting_payment">Awaiting Payment</a>
-            <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("cancelled") ?>" href="/my-account/consignment/?mode=listed&type=cancelled">Cancelled</a>
+            <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("unsold") ?>" href="/my-account/consignment/?mode=listed&type=unsold">Unsold</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("pending_payout") ?>" href="/my-account/consignment/?mode=listed&type=pending_payout">Pending Payout</a>
             <a class="btn btn-pill btn-sm mb-2 <?php echo ActivateListing("paid_out") ?>" href="/my-account/consignment/?mode=listed&type=paid_out">Paid Out</a>
         </div>
