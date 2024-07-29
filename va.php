7fe32e787317f473a7e38612035d647dfc51af0c
arhizsx <arhizsx@gmail.com>
2024-07-29 14:28:07 +0800

    RUn

--- va.php
+++ va.php
@@ -117,7 +117,7 @@
             <p id="progress" class="progress-text">0%</p>
         </div>
         <div class="controls">
-            <H1>Process Paid Outs</H1>
+            <H1 class="mb-4">Process Paid Outs</H1>
             <label>Google Drive Folder ID</label>
             <input type="text" id="folderId" class="form-control" placeholder="Enter Google Drive Folder ID" />
             <small>Please input the google drive's Folder ID before clicking on GET FILES</small>
