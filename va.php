4240666a32e6ad9861237aa96f7fd72d7d11251d
arhizsx <arhizsx@gmail.com>
2024-07-29 17:47:10 +0800

    ran

--- va.php
+++ va.php
@@ -56,7 +56,7 @@
             display: none;
         }
     </style>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
 
