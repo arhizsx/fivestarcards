555c8a79efea26d9423181415e1011ab2d9b0735
arhizsx <arhizsx@gmail.com>
2024-07-29 13:19:46 +0800

    RUn

--- status_script.php
+++ status_script.php
@@ -3,5 +3,8 @@
 
     $progress_file = '/home/arhizsx/progress.txt';
     $progress = file_get_contents($progress_file);
+    
+    header('Content-type: application/json');
     print_r( $progress);
+    
 ?>
