2b5ee31ebc64d0394bbfc85cd6d91a3bfca7ee82
arhizsx <arhizsx@gmail.com>
2024-07-29 11:52:55 +0800

    RUn

--- run_script.php
+++ run_script.php
@@ -4,7 +4,7 @@
 if (isset($_POST['folder_id'])) {
     $folder_id = escapeshellarg($_POST['folder_id']);
     $command = "python3 /home/arhizsx/paidout.py {$folder_id} 2>&1"; // Capture both stdout and stderr
-    
+
     // Execute the Python script and capture output and errors
     $output = shell_exec($command);
     
