2d78d72953841c5716e656960b9d2c0cc5d45ea9
arhizsx <arhizsx@gmail.com>
2024-07-29 12:46:33 +0800

    RUn

--- run_script.php
+++ run_script.php
@@ -8,10 +8,6 @@ if (isset($_POST['folder_id'])) {
     // Execute the Python script and capture output and errors
     $output = shell_exec($command);
 
-    print_r($output);
-
-    die(  );
-
     // Log the output to a file for debugging
     file_put_contents('/home/arhizsx/debug_output.txt', $output);
 
