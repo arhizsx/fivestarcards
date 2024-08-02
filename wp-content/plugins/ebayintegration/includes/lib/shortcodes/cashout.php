53119bcee9d90b3505e5f26481b038f7c8663280
arhizsx <arhizsx@gmail.com>
2024-08-03 05:16:21 +0800

    SoldListPaid

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/cashout.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/cashout.php
@@ -80,7 +80,7 @@
                                             <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
                                             <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>
                                             <?php 
-                                                $listing = $data["Item"]["ListingType"] == "Chinese" ? "Auction" : $data["ListingType"]; 
+                                                $listing = $data["Item"]["ListingType"] == "Chinese" ? "Auction" : $data["Item"]["ListingType"]; 
                                             ?>
                                             <div class="item_id text-small">Listing Type: <?php echo $listing; ?></div>                                        
                                         </td>
