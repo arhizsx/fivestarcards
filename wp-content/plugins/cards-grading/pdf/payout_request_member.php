6593e4ef2ab8726e4daeae197f38aa4fca61c39e
arhizsx <arhizsx@gmail.com>
2024-08-03 20:23:02 +0800

    SoldListPaid

--- wp-content/plugins/cards-grading/pdf/payout_request_member.php
+++ wp-content/plugins/cards-grading/pdf/payout_request_member.php
@@ -1,28 +1,49 @@
 <?php
-
 $payout = $data["payout"];
-
-print_r( $payout );
-
 ?>
 
 <html>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
     <body>
-        <div>
-            <img src="https://5starcards.com/wp-content/uploads/2023/09/5-star-cards-logo.png" width="200px;" alt="5 Star Cards">
-        </div>
-        <div>
-            <table>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <img src="https://5starcards.com/wp-content/uploads/2023/09/5-star-cards-logo.png" width="200px;" alt="5 Star Cards">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table class="table table-bordered tabled-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-start">Payment ID</th>
+                                <th class="text-center">Cards</th>
+                                <th class="text-start">Request Date</th>
+                                <th class="text-end">Amount Requested</th>
+                                <th class="text-end">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-start">Payment ID</td>
+                                <td class="text-center">Cards</td>
+                                <td class="text-start">Request Date</td>
+                                <td class="text-end">Amount Requested</td>
+                                <td class="text-end">Status</td>
+                            </tr>
+                            <tr>
+                                <td class="text-start">Payment ID</td>
+                                <td class="text-center">Cards</td>
+                                <td class="text-start">Request Date</td>
+                                <td class="text-end">Amount Requested</td>
+                                <td class="text-end">Status</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </body>
 </html>
