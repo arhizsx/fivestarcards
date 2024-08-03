54e75c3be2b86c126bafe73ca20a0858c80de4a4
arhizsx <arhizsx@gmail.com>
2024-08-03 11:26:15 +0800

    SoldListPaid

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/payout.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/payout.php
@@ -64,8 +64,8 @@
                                 <tr>
                                     <th class="text-start">Request ID</th>
                                     <th class="text-start">Request Date</th>
-                                    <th class="text-center">Cards</th>
                                     <th class="text-start">Payout Date</th>
+                                    <th class="text-center">Cards</th>
                                     <th class="text-end">Amount Requested</th>
                                     <th class="text-end">Status</th>
                                 </tr>
@@ -74,8 +74,8 @@
                                 <tr>
                                     <td class="text-start">-</td>
                                     <td class="text-start">-</td>
-                                    <td class="text-center">-</td>
                                     <td class="text-start">-</td>
+                                    <td class="text-center">-</td>
                                     <td class="text-end">-</td>
                                     <td class="text-end">-</td>
                                 </tr>                                    
