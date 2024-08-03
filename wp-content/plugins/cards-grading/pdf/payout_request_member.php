2e26c2f3321a788ff1a6b4ce317780da04ee4ea0
arhizsx <arhizsx@gmail.com>
2024-08-03 21:20:48 +0800

    SoldListPaid

--- wp-content/plugins/cards-grading/pdf/payout_request_member.php
+++ wp-content/plugins/cards-grading/pdf/payout_request_member.php
@@ -50,7 +50,7 @@
                             <tr>
                                 <td>
                                 <?php
-                                print_r($card_data["Item"]);
+                                print_r($card_data);
                                 ?>
                                 </td>
                                 <td>-</td>
