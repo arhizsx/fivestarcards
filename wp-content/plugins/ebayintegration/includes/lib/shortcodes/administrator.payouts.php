3110cc0b02ef9fb03d0672cc5ea8c81fcdf2bd3c
arhizsx <arhizsx@gmail.com>
2024-08-03 16:25:13 +0800

    SoldListPaid

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.payouts.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.payouts.php
@@ -34,17 +34,25 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <a href="/my-account/grading" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/administrator" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+                <i class="fa-solid fa-list me-2"></i>
+                Dashboard
+            </a>
+            <a href="/administrator/grading" class="5star_btn btn text-left  btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-circle-dot me-2"></i>                
                 Grading
             </a>
-            <a href="/my-account/consignment" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <a href="/administrator/consignment" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
                 <i class="fa-solid fa-handshake me-2"></i>
                 Consignment
             </a>
-            <a href="/my-account/payout" class="5star_btn btn text-left btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
-                <i class="fa-solid fa-money-bill me-2"></i>
-                Payout
+            <a href="/administrator/payouts" class="5star_btn btn text-left btn-secondary mb-3" data-type="psa-value_bulk" data-action="add_card">
+            <i class="fa-solid fa-money-bill me-2"></i>
+            Payouts
+            </a>
+            <a href="/administrator/members" class="5star_btn btn text-left btn-dark mb-3" data-type="psa-value_bulk" data-action="add_card">
+                <i class="fa-solid fa-users me-2"></i>
+                Members
             </a>
         </div>
     </div>
