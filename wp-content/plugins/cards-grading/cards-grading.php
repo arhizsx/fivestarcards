539e6f6d7ac52049ba2ee7aabea90392c33214d3
arhizsx <arhizsx@gmail.com>
2024-08-03 22:19:29 +0800

    SoldListPaid

--- wp-content/plugins/cards-grading/cards-grading.php
+++ wp-content/plugins/cards-grading/cards-grading.php
@@ -1080,7 +1080,7 @@
         $body = 'Please find the attached PDF.';
         $headers = [
             'Content-Type: text/html; charset=UTF-8',
-            'Content-Disposition: attachment; filename="' . $file_prepend . '.pdf"'
+            'Content-Disposition: attachment; filename="' .'5 Star Cards - ' . $file_prepend . '.pdf"'
         ];
         $attachments = [
             [
