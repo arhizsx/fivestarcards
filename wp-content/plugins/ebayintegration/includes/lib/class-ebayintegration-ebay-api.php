75c5bee34f2278d8f709ae0a402c296d0486b58f
arhizsx <arhizsx@gmail.com>
2024-08-03 16:07:36 +0800

    SoldListPaid

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -1525,7 +1525,7 @@ class Ebay_Integration_Ebay_API {
 
 		$user_data = get_userdata($user_id);
 
-		return $user_data;
+		return $user_data->data;
 		$user = [
 			"name" => $user_data["data"]["display_name"],
 			"email" => $user_data["data"]["user_email"],
