fd720db23d63924473721471694307c342b9e9b2
arhizsx <arhizsx@gmail.com>
2024-08-08 03:23:08 +0800

    No Photo

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.payouts.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.payouts.php
@@ -71,6 +71,7 @@
                                     <th class="text-center">Cards</th>
                                     <th class="text-start">Request Date</th>
                                     <th class="text-end">Amount Requested</th>
+                                    <th class="text-end">Paid Date</th>
                                     <th class="text-end">Status</th>
                                     <!-- <th class="text-end">Payout Date</th> -->
                                 </tr>
@@ -97,6 +98,7 @@
                                     <td class="text-center"><?php echo $data["cards_count"] ?></td>
                                     <td class="text-start"><?php echo $card->add_timestamp ?></td>
                                     <td class="text-end">$<?php echo number_format(( $data["requested_amount"] ), 2, '.', ',') ?></td>
+                                    <td class="text-end"><?php echo  $card->update_timestamp ?></td>
                                     <td class="text-end"><?php echo $card->status ?></td>
                                     <!-- <td class="text-end"></td> -->
                                 </tr>  
