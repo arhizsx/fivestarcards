d9153a6274dd4aaf6037c1ee819559a1ca2f1133
arhizsx <arhizsx@gmail.com>
2024-08-08 02:06:14 +0800

    No Photo

--- wp-content/plugins/ebayintegration/includes/class-ebayintegration.php
+++ wp-content/plugins/ebayintegration/includes/class-ebayintegration.php
@@ -124,7 +124,7 @@ class Ebay_Integration {
 	 * @param string $version Plugin version.
 	 */
 	public function __construct( $file = '', $version = '1.0.0' ) {
-		$this->_version = '1.0.354';
+		$this->_version = '1.0.355';
 		$this->_token   = 'Ebay_Integration';
 
 		// Load plugin environment variables.
