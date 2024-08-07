1f9ac112d8af755f343bd454648b6ef30262edff
arhizsx <arhizsx@gmail.com>
2024-08-08 03:29:29 +0800

    No Photo

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.payouts.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator.payouts.php
@@ -97,8 +97,15 @@
                                     <td class="text-start"><?php echo $user["name"]; ?></td>
                                     <td class="text-center"><?php echo $data["cards_count"] ?></td>
                                     <td class="text-start"><?php echo $card->add_timestamp ?></td>
+                                    <?php 
+                                        if($card->update_timestamp == $card->add_timestamp){
+                                            $tt = "";
+                                        } else {
+                                            $tt = $card->update_timestamp;
+                                        }
+                                    ?>
                                     <td class="text-end">$<?php echo number_format(( $data["requested_amount"] ), 2, '.', ',') ?></td>
-                                    <td class="text-end"><?php echo   $card->update_timestamp ?></td>
+                                    <td class="text-end"><?php echo   $tt ?></td>
                                     <td class="text-end"><?php echo $card->status ?></td>
                                     <!-- <td class="text-end"></td> -->
                                 </tr>  
