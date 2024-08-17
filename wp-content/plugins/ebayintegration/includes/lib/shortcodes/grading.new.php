02845d7818b9b0344ee37f319ab267b1a4f53252
arhizsx <arhizsx@gmail.com>
2024-08-17 16:36:49 +0800

    JS

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -258,8 +258,7 @@
                         ?>
                         <tr class="grading_file">
                             <td colspan="7" class="text-left">
-                                <a class="btn btn-danger btn-sm">REMOVE</a>
-                                <a class="btn btn-dark btn-sm me-3">VIEW</a>
+                                <a class="btn btn-danger btn-sm ebayintegration-btn" data-action="remove_grading_file" >REMOVE</a>
                                 <a href="<?php echo $fdata["baseurl"] ?>"><?php echo $fdata["name"] ?></a>
                             </td>
                         </tr>
