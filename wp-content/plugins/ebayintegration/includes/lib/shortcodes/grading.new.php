63fa54a456a2f1d984c287a1f672e2c6d6f076bb
arhizsx <arhizsx@gmail.com>
2024-08-17 16:24:27 +0800

    JS

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -257,12 +257,12 @@
                             foreach( $file_data as $fdata ){
                         ?>
                         <tr class="grading_file">
-                            <td colspan="6" class="text-left">
-                                <a href="<?php echo $fdata["baseurl"] ?>"><?php echo $fdata["name"] ?></a>
-                            </td>
                             <td class="text-right" colspan="1">
                                 <a class="btn btn-primary btn-sm">REMOVE</a>
                                 <a class="btn btn-dark btn-sm">VIEW</a>
+                            </td>
+                            <td colspan="6" class="text-left">
+                                <a href="<?php echo $fdata["baseurl"] ?>"><?php echo $fdata["name"] ?></a>
                             </td>
                         </tr>
                         <?php                                 
