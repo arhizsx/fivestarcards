0328308c919ddae0cf66c9db246f89329986c797
arhizsx <arhizsx@gmail.com>
2024-08-17 16:23:48 +0800

    JS

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -260,7 +260,7 @@
                             <td colspan="6" class="text-left">
                                 <a href="<?php echo $fdata["baseurl"] ?>"><?php echo $fdata["name"] ?></a>
                             </td>
-                            <td class="text-center" colspan="1">
+                            <td class="text-right" colspan="1">
                                 <a class="btn btn-primary btn-sm">REMOVE</a>
                                 <a class="btn btn-dark btn-sm">VIEW</a>
                             </td>
