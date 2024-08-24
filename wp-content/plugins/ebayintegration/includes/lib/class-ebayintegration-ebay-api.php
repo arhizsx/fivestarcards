fc891d49992083fe657044bd2f8fa1100b042749
arhizsx <arhizsx@gmail.com>
2024-08-24 13:24:08 +0800

    Set Grade

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -919,10 +919,8 @@ class Ebay_Integration_Ebay_API {
 		];
 
 
-		$sql = "SELECT * FROM grading where type='". $params["type"] .  "_file' AND user_id = '". $params["user_id"] .  "' AMD status = 'logged'";
+		$sql = "SELECT * FROM grading where type='". $params["type"] .  "_file' AND user_id = '". $params["user_id"] .  "' AND status = 'logged'";
 		$grading_files = $this->wpdb->get_results ( $sql );	
-
-		return $sql;
 
 		if( count($grading_files) > 0){
 			$status = "For Entry";
