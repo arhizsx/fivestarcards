b020c3f061af90bf850a31edcccbb8d8237d1cdb
arhizsx <arhizsx@gmail.com>
2024-08-24 14:31:15 +0800

    Set Grade

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -313,23 +313,28 @@ if( $grading_order_id > 0 ){
                     <td><?php echo $card["card_number"]; ?><br><small><?php echo $card["attribute"]; ?></small></td>
                     <td><?php echo $card["player"]; ?></td>
                     <td class=".card_status"><?php echo $meta["status"][0]; ?></td>
+                    <?php if( $checkout_meta["status"][0] == "Completed - Grades Ready" ) { ?>
                     <td class="text-start">
-                        <?php if( $checkout_meta["status"][0] == "Completed - Grades Ready" ) { ?>
                         <input type="text" value="" name="grade" class="form-control form-control-sm">
-                        <?php } else { ?>
-                        -
-                        <?php } ?>
                     </td>
                     <td class="text-start">
-                        <?php if( $checkout_meta["status"][0] == "Completed - Grades Ready" ) { ?>
                         <input type="text" value="" name="certificate_number" class="form-control form-control-sm">
-                        <?php } else { ?>
-                        -
-                        <?php } ?>
                     </td>
                     <td class="text-start">
                         -    
                     </td>
+                    <?php } else { ?>
+                    <td class="text-start">
+                        -
+                    </td>
+                    <td class="text-start">
+                        -
+                    </td>
+                    <td class="text-start">
+                        -
+                    </td>
+                    <?php } ?>
+
                     <td class='text-end'><?php echo "$" . number_format((float)$card["dv"], 2, '.', ''); ?></td>
                     <td class='text-end'><?php echo "$" . number_format((float) $card_grading_charge, 2, '.', ''); ?></td>
                 </tr>
