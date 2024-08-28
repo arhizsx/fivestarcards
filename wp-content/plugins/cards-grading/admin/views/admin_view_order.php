db5d0120922acd7dfbb5820084500b44023b0eb6
arhizsx <arhizsx@gmail.com>
2024-08-29 02:22:37 +0800

    Grading PSA

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -291,6 +291,11 @@ if( $grading_order_id > 0 ){
 
                 ?>
                 <tr class="admin-card-row" data-post_id="<?php echo $post->ID; ?>" data-card='<?php echo json_encode($card) ?>'>
+
+                    <?php 
+                        if( in_array( $checkout_meta["status"][0], $admin_graded_status ) == false){                             
+                    ?>
+
                     <?php if( in_array( $checkout_meta["status"][0], $admin_action_status ) ){ ?>
                     <td >
                         <?php if( $checkout_meta["status"][0] == "Package Received" ) { ?>
@@ -325,8 +330,7 @@ if( $grading_order_id > 0 ){
                     <?php } ?>
 
                     <?php 
-                        if( in_array( $checkout_meta["status"][0], $admin_graded_status ) ){ 
-                            
+                        if( in_array( $checkout_meta["status"][0], $admin_graded_status ) ){                             
                     ?>
                         <td class="text-start">
                             <input type="text" value="" name="grade"  data-name="grade" data-post_id="<?php echo $post->ID; ?>" data-db_id="<?php echo $card['db_id']; ?>" class="form-control form-control-sm card_grade_saving">
@@ -340,6 +344,56 @@ if( $grading_order_id > 0 ){
 
                     <td class='text-end'><?php echo "$" . number_format((float)$card["dv"], 2, '.', ''); ?></td>
                     <td class='text-end'><?php echo "$" . number_format((float) $card_grading_charge, 2, '.', ''); ?></td>
+
+                    <?php 
+                        } else {
+                    ?>
+                    <!-- Graded / To Grade -->
+
+                    <?php 
+
+                    $sql = "SELECT * FROM grading WHERE id = " . $card["db_id"];
+                    $db_row = $this->wpdb->get_results ( $sql );
+
+                    $db_row_data = json_decode(  $db_row["data"], true );
+                    
+                    if( array_key_exists( "title", $db_row_data ) == false && array_key_exists( "certImgFront", $db_row_data )  == false && array_key_exists( "certImgBack", $db_row_data )  == false ){
+
+                    ?>
+                    <td>
+                        <?php  echo $post->ID; ?>
+                    </td>
+                    <td><?php echo $card["year"]; ?></td>
+                    <td><?php echo $card["brand"]; ?></td>
+                    <td><?php echo $card["card_number"]; ?><br><small><?php echo $card["attribute"]; ?></small></td>
+                    <td><?php echo $card["player"]; ?></td>
+
+                    <?php if( in_array( $checkout_meta["status"][0], $admin_graded_status ) == false ){ ?>
+                        <td class=".card_status"><?php echo $meta["status"][0]; ?></td>
+                    <?php } ?>
+
+                    <?php 
+                        if( in_array( $checkout_meta["status"][0], $admin_graded_status ) ){                             
+                    ?>
+                        <td class="text-start">
+                            <input type="text" value="" name="grade"  data-name="grade" data-post_id="<?php echo $post->ID; ?>" data-db_id="<?php echo $card['db_id']; ?>" class="form-control form-control-sm card_grade_saving">
+                        </td>
+                        <td class="text-start">
+                            <input type="text" value="" name="certificate_number" data-name="certificate_number" data-post_id="<?php echo $post->ID; ?>" data-db_id="<?php echo $card['db_id']; ?>" class="form-control form-control-sm card_grade_saving">
+                        </td>
+                    <?php } else { ?>
+
+                    <?php } ?>
+
+                    <td class='text-end'><?php echo "$" . number_format((float)$card["dv"], 2, '.', ''); ?></td>
+                    <td class='text-end'><?php echo "$" . number_format((float) $card_grading_charge, 2, '.', ''); ?></td>
+
+                    <?php 
+                    }
+
+                        }
+                    ?>
+
                 </tr>
                 <?php          
                         }
