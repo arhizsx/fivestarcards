4ad2ad31f3704e255a08450b3b18f1260d970677
arhizsx <arhizsx@gmail.com>
2024-08-29 01:57:27 +0800

    Grading PSA

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -353,6 +353,7 @@ if( $grading_order_id > 0 ){
                 ?>
             </tbody>
         </table>
+        <H3 style="color: black !important;">Graded Cards</H3>
         <table id="completed_graded" class="table table-bordered table-striped">
             <thead>
                 <th>ID</th>
