ff660299bc2d2b8b18cb25517c60779bd92ce9d8
arhizsx <arhizsx@gmail.com>
2024-08-28 18:08:27 +0800

    Grade Saving

--- wp-content/plugins/cards-grading/js/cards-grading.js
+++ wp-content/plugins/cards-grading/js/cards-grading.js
@@ -1234,13 +1234,13 @@ $(document).on("change", ".card_grade_saving", function(){
 
     var grading = saveGrading( post_id, db_id, name, value );
 
-    // $(document).find(".admin-card-row[data-post_id='" + post_id + "']").find("input").prop("disabled", "disabled");
+    $(document).find(".admin-card-row[data-post_id='" + post_id + "']").find("input").prop("disabled", true);
 
-    console.log($(document).find(".admin-card-row[data-post_id='" + post_id + "']").find("input"));
+    // console.log($(document).find(".admin-card-row[data-post_id='" + post_id + "']").find("input"));
 
     $.when( grading ).done( function( grading ){
 
-        $(document).find(".admin-card-row[data-post_id='" + post_id + "']").removeClass("disabled");
+        // $(document).find(".admin-card-row[data-post_id='" + post_id + "']").removeClass("disabled");
 
         console.log(grading);
 
