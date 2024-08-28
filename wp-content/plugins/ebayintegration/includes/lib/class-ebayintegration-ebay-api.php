73ca9bffdfbb008a0000ef24d1080cb63ecd338a
arhizsx <arhizsx@gmail.com>
2024-08-28 14:39:39 +0800

    Grade Saving

--- wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
+++ wp-content/plugins/ebayintegration/includes/lib/class-ebayintegration-ebay-api.php
@@ -1671,7 +1671,7 @@ class Ebay_Integration_Ebay_API {
 
 				$psxa = json_decode( $psa, true );
 
-				return $psxa;
+				return $psxa["certImgFront"];
 
 				if (json_last_error() === JSON_ERROR_NONE) {
 					// JSON is valid
