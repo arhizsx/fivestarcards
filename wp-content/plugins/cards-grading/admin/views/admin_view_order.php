daa788047e2c6fe19c7aa94f2ac739d1dbf567da
arhizsx <arhizsx@gmail.com>
2024-08-29 08:20:19 +0800

    Grading PSA

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -142,13 +142,13 @@ if( $grading_order_id > 0 ){
         </div>
     </div>
     <div class="row mt-3">
-        <div class="col-lg-6 col-md-6 col-sm-6">
-            <H3 style="color: black !important;">Cards List</H3>
+        <div class="col-lg-6 col-md-6 col-sm-6 d-flex justify-content-evenly">
+            <div style="color: black !important;">Cards List</div>
 
             <?php 
                 if( $checkout_meta["inspection"][0] == "1" ) { 
             ?>
-            <H3 style="color: black !important;">INSPECTION SERVICE</H3>
+            <div style="color: black !important;">INSPECTION SERVICE</div>
             <?php 
                 }
             ?>
