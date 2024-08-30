52df1574634acb67c48250502844d0648e428d90
arhizsx <arhizsx@gmail.com>
2024-08-31 07:53:02 +0800

    Scraper

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -279,7 +279,9 @@ if( $grading_order_id > 0 ){
                     <th>Brand</th>
                     <th>Card #</th>
                     <th>Player Name</th>
+                    <?php if( in_array( $checkout_meta["status"][0], $admin_graded_status ) == false ){ ?>
                     <th>Status</th>
+                    <?php } ?>
                     <?php if( in_array( $checkout_meta["status"][0], $admin_graded_status ) ){ ?>
                     <th style="width: 150px;" class="text-start">Grade</th>
                     <th style="width: 200px;" class="text-start">Cert No.</th>
@@ -339,7 +341,9 @@ if( $grading_order_id > 0 ){
                     <td><?php echo $card["card_number"]; ?><br><small><?php echo $card["attribute"]; ?></small></td>
                     <td><?php echo $card["player"]; ?></td>
 
-                    <td class=".card_status"><?php echo $meta["status"][0]; ?></td>
+                    <?php if( in_array( $checkout_meta["status"][0], $admin_graded_status ) == false ){ ?>
+                        <td class=".card_status"><?php echo $meta["status"][0]; ?></td>
+                    <?php } ?>
 
                     <?php 
                         if( in_array( $checkout_meta["status"][0], $admin_graded_status )){                             
