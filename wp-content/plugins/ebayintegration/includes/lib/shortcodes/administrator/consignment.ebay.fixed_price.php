a814ede7c5dc85abe777e23b1dba59ad566083ba
arhizsx <arhizsx@gmail.com>
2024-09-02 08:06:11 +0800

    Full

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.fixed_price.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.fixed_price.php
@@ -58,10 +58,33 @@ $users = get_users( $args );
                             $i++;
             ?>
             <tr>
-                <td><?php print_r( $item->data ); ?></td>
+                <td>
+                    <div class="title">
+                        <span class='pe-2'><strong><?php echo $i ?></strong></span>
+                        <a href="<?php echo $data['ListingDetails']['ViewItemURL'] ?>" target="_blank">
+                        <?php echo $data["Title"]; ?>
+                        </a>
+                    </div>
+                    <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
+                    <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>                    
+                    <div class="item_id text-small">ID: <?php echo $item->id ?></div>                    
+                </td>
+                <td class="text-end">
+                <?php echo $data["SellingStatus"]["QuantitySold"] * 1; ?>
+                </td>
+                <td class="text-end">
+                    <?php echo $data["QuantityAvailable"] * 1; ?>
+                </td>
+                <td class="text-end">
+                    <?php echo $data["WatchCount"] * 1; ?>
+                </td>
+                <td class="text-end">$<?php 
+                echo number_format(( $data["SellingStatus"]["CurrentPrice"]), 2, '.', ',');
+                ?></td>
             </tr>
             <?php 
-                        }}
+                        }
+                    }
                 }
 
             } 
