8fbc5465242a2215a788bb985b93e6cb277aa6c9
arhizsx <arhizsx@gmail.com>
2024-09-05 22:00:07 +0800

    down

--- checkcurl.php
+++ checkcurl.php
@@ -1,16 +1,9 @@
 <?php
 
-$apiURL = "https://api.ebay.com/identity/v1/oauth2/token";
+function _isCurl(){
+    return function_exists('curl_version');
+}
 
-$post_data = [
-    "grant_type" => "refresh_token",
-    "refresh_token" => $this->refresh_token,
-];
-
-$curl = curl_init();
-
-var_dump($curl);
-
-
+_isCurl();
 
 ?>
