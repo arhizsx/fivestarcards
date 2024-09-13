3ae0c57f8ab8dcdc5f31138ca865bbfdb51be118
arhizsx <arhizsx@gmail.com>
2024-09-13 17:15:54 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -124,6 +124,13 @@
 
         <!-- UPPER BUTTONS -->
         <div>
+            <!-- <?php 
+                if( isset($_GET["grader"]) ){
+                    $grd = "?grader=" . $_GET["grader"];
+                } else {
+                    $grd = "";
+                }
+            ?> -->
             <a href="/my-account/grading/new<?php echo $grd; ?>" class="btn btn-outline-dark mb-3 ">Back to Grading Types</a>
 
             <button class="btn btn-success mb-3 ebayintegration-btn" data-action="show_log_grading_modal">
@@ -665,91 +672,11 @@
         </div>
 
 
-        <!-- SHIP MODAL -->
-        <div class="modal fade ship_batch_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px">
-            <div class="modal-dialog modal-lg" style="margin-bottom: 150px;">
-                <div class="modal-content modal-ajax">
-                    <div class="modal-header bg-dark text-white">
-                        <h5 class="modal-title">
-                            Ship Cards
-                        </h5>
-                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
-                            X
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="formbox">
-
-                            <div class="row">
-                                <div class="col">
-                                    <H5 style="color: black;">Ship your items to</H5>
-                                </div>
-                            </div>
-                            <div class="row border-bottom mb-3">
-                                <div class="col-lg-6 small pb-3">
-                                    <div>USPS</div>
-                                    <div>Matt Sellers</div>
-                                    <div>PO Box 263 Hartland, WI 53029</div>
-                                </div>
-                                <div class="col-lg-6 small pb-3">
-                                    <div>FedEx / UPS / DHL</div>
-                                    <div>Matt Sellers</div>
-                                    <div>203 E Wisconsin Ave Suite 203C Oconomowoc, WI 53066</div>
-                                </div>
-                            </div>
-                            
-                            <forn id="shipping_info_form">
-
-                                    <input type="hidden" name="user_id" value="<?php echo get_current_user_id() ?>">
-                                    <div class="row">
-                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
-                                            <label for="carrier">Carrier</label>
-                                            <select name="carrier" class="form-control" data-field_check="required">
-                                                <option value="">Select Carrier</option>
-                                                <option value="USPS">USPS</option>
-                                                <option value="FedEx">FedEx</option>
-                                                <option value="DHL">DHL</option>
-                                                <option value="UPS">UPS</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
-                                            <label for="shipped_by">Shipped By</label>
-                                            <input type="text" name="shipped_by" class="form-control p-2" data-field_check="required">
-                                        </div>
-                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
-                                            <label for="tracking_number">Tracking Number</label>
-                                            <input type="text" name="tracking_number" class="form-control p-2" data-field_check="required">
-                                        </div>
-                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
-                                            <label for="shipping_date">Shipping Date</label>
-                                            <input type="date" name="shipping_date" class="form-control p-2" data-field_check="required">
-                                        </div>
-                                    </div>
-                            </forn>
-                        </div>
-                        <div class="d-none p-5 text-center loading">
-
-                            Processing cards, please wait...
-
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn border btn-secondary" data-bs-dismiss="modal" >Close</button>
-
-                        <button class="btn border btn-primary ebayintegration-btn" 
-                            data-action='confirmConsignCardsShipping' 
-                        >
-                            Card Shipped
-                        </button>                    
-                    </div>
-                </div>
-            </div>
-        </div>
 
 <!-- GRADING MENU -->
 
 <?php
-    } else {
+    }  else {
 
         if( get_current_user_id() != 0 ){
 
