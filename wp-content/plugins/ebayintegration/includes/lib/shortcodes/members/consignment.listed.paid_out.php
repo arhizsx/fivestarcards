03c6f2778f6732324ae34a59ee5711ce79259a88
arhizsx <arhizsx@gmail.com>
2024-09-13 20:30:47 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.paid_out.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.paid_out.php
@@ -87,7 +87,7 @@ $skus = get_user_meta( get_current_user_id(), "sku", true );
             else {
             ?>
             <tr>
-                <td colspan="2" class="text-center p-5">
+                <td colspan="3" class="text-center p-5">
                     Empty
                 </td>
             </tr>
