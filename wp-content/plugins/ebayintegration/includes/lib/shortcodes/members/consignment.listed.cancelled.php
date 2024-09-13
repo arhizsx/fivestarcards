c93179183e27254f84c4cfda4dc6d3b7cd557766
arhizsx <arhizsx@gmail.com>
2024-09-13 20:41:09 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.cancelled.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.cancelled.php
@@ -2,16 +2,15 @@
 
 global $wpdb;
 
-$maxpage = 500;
+$ebay = $this->wpdb->get_results ( "
+SELECT * 
+FROM  ebay
+where status = 'Cancelled'
+ORDER BY id DESC
+" 
+);
 
 $skus = get_user_meta( get_current_user_id(), "sku", true );		
-
-$array = implode("','",$skus);
-
-$sql = "SELECT * FROM ebay WHERE status = 'Cancelled' AND sku IN ('" . $array . "')";
-$ebay = $this->wpdb->get_results ( $sql );
-
-print_r( $ebay );
 
 ?>
 <style>
@@ -19,91 +18,65 @@ print_r( $ebay );
         font-size: .7em !important;
     }
 </style>
+<div class="d-flex flex-row-reverse mb-3">
+    <input class="btn pl-2 search_box" style="text-align: left; padding-left: 10px; padding-bottom:5px; padding-top: 6px;" placeholder="Search" type="text" data-target=".search_table_paid">
+</div>
+<?php 
+    $available = 0;
+    if($skus != null){
+        foreach($ebay as $item){ 
+            $data = json_decode($item->data, true);
+            if( in_array( $item->sku, $skus ) ){
+                $available++;
+            }
+        }
+    }
+?>
+
 <div class="table-responsive">
     <table class="table table-border table-striped table-sm table-hover search_table_paid">
         <thead>
             <tr>
-                <th>Item</th>
-                <th>Cancel ID</th>
-                <th>Request Date</th>
-                <th>Close Date</th>
-                <th>Reason</th>
+                <th width="60%">Item</th>
+                <th>eBay Pay Date</th>
+                <th class="text-end">Price Sold</th>
             </tr>
         </thead>
         <tbody>
             <?php 
-                $available = 0;
-                if($skus != null){
-                    foreach($ebay as $item){ 
-                        $data = json_decode($item->data, true);
-                        if( in_array( $item->sku, $skus ) ){
-                            $available++;
-                        }
-                    }
-                } else {
-                    echo "NO SKU";
-                }
-            ?>
-
-            <?php 
             if( $available > 0 && $skus != null ){
-
-                $ctr = 0;
-
                 foreach($ebay as $item){ 
-
-                    if( $item->transaction != "Not Sold" ){
-
-
-                        $transaction = json_decode($item->transaction, true);
                         $data = json_decode($item->data, true);
-                        $ctr++;
 
-                        if( array_key_exists("Item", $data) ){
-                            $title = $data["Item"]["Title"];
-                            $url = $data["Item"]['ListingDetails']['ViewItemURL'];
-                        } else {
-                            $title = $data["Title"];
-                            $url = $data['ListingDetails']['ViewItemURL'];
-                        }
+                        if( in_array( $item->sku, $skus ) ){
 
-                        $cancel = json_decode($item->cancellation, true);
+                            $ctr++;
 
             ?>
             <tr>
                 <td>
                     <div class="title">
                         <strong><?php echo $ctr;  ?></strong>&nbsp;
-                        <a href="<?php echo $url ?>" target="_blank">
-                            <?php print_r( $title ); ?>
+                        <a href="<?php echo $data["Item"]['ListingDetails']['ViewItemURL'] ?>" target="_blank">
+                            <?php print_r( $data["Item"]["Title"] ); ?>
                         </a>
-                    </div>
+                    </div> 
                     <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
                     <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>
-                    <?php $listing = $data["Item"]["ListingType"] == "Chinese" ? "Auction" : $data["Item"]["ListingType"]; ?>
-                    <div class="item_id text-small">Listing Type: <?php echo $listing; ?></div>                    
-                    <div class="item_id text-small">ID: <?php echo $item->id ?></div>                                        
-                </td>    
-                <td>
-                    <?php 
-                        print_r($cancel["cancelId"]);
+                    <?php $listing = $data["Item"]["ListingType"] == "Chinese" ? "Auction" : $data["ListingType"]; ?>
+                    <div class="item_id text-small">Listing Type: <?php echo $listing; ?></div>                                        
+                </td>
+                <td class="">
+                    <?php
+                    $paid_time = explode("T",$data["PaidTime"]); 
+                    echo $paid_time[0];
                     ?>
-                </td>    
-                <td>
-                    <?php 
-                        print_r($cancel["cancelRequestDate"]["value"]);
+                </td>
+                <td class="text-end">
+                    $<?php 
+                    echo number_format(( $data["TransactionPrice"]), 2, '.', ',');
                     ?>
-                </td>    
-                <td>
-                    <?php 
-                        print_r($cancel["cancelCloseDate"]["value"]);
-                    ?>
-                </td>    
-                <td>
-                    <?php 
-                        print_r($cancel["cancelStatus"]);
-                    ?>
-                </td>       
+                </td>
             </tr>
             <?php
                     }
@@ -112,7 +85,7 @@ print_r( $ebay );
             else {
             ?>
             <tr>
-                <td colspan="5" class="text-center p-5">
+                <td colspan="3" class="text-center p-5">
                     No Items
                 </td>
             </tr>
