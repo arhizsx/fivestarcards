7f50ffb538206d885d55d93e55c6c263765f91b4
arhizsx <arhizsx@gmail.com>
2024-09-13 17:23:13 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -168,12 +168,29 @@
                         </td>
                     </tr>
                     <?php 
+                        } else {
+
+
+                            foreach( $grading as $card ){
+
+                                $data = json_decode( $card->data, true );
+
+                                if(array_key_exists("file", $data)){
+                                    $img = "<img src='" . ($data["file"]["baseurl"]) . "'>";
+                                } else {
+                                    $img = '<div class="d-flex justify-content-center align-items-center picture_box ebayintegration-btn" data-action="grading_picture_box_click"  data-id="' . $card->id . '"   data-user_id="' . get_current_user_id() . '">' .
+                                            '<i class="fa-solid fa-file-image fa-2x"></i>' . 
+                                        '</div>';
+                                }
 
                     ?>
+                    <tr class='consigned_item_row' data-id='<?php echo $card->id; ?>'>
+                    </tr>
                     <?php 
                                 $total_grading = $total_grading + $data["per_card"];
                                 $total_dv = $total_dv + $data["dv"];
 
+                            }
                         }
                     ?>            
                 </tbody>
