e02dfcc7eb1e32e4a8254fb3f3c2a61129a88569
arhizsx <arhizsx@gmail.com>
2024-09-13 17:17:43 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -124,13 +124,13 @@
 
         <!-- UPPER BUTTONS -->
         <div>
-            <!-- <?php 
+            <?php 
                 if( isset($_GET["grader"]) ){
                     $grd = "?grader=" . $_GET["grader"];
                 } else {
                     $grd = "";
                 }
-            ?> -->
+            ?>
             <a href="/my-account/grading/new<?php echo $grd; ?>" class="btn btn-outline-dark mb-3 ">Back to Grading Types</a>
 
             <button class="btn btn-success mb-3 ebayintegration-btn" data-action="show_log_grading_modal">
@@ -141,7 +141,299 @@
             </button>
         </div>
 
+        <!-- DESKTOP VIEW -->
+        <div class="table-responsive d-none d-lg-block">
+            <table class="table table-sm table-bordered" id="new_grading">                
+                <thead>
+                    <tr>
+                        <th style="width: 20px;"></th>
+                        <th width="30%">Player Name</th>
+                        <th>Year</th>
+                        <th>Brand</th>
+                        <th>Card Number</th>
+                        <th class="text-end">DV</th>
+                        <th class="text-end">Grading</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $total_dv = 0;
+                        $total_grading = 0;
 
+                        if( count( $grading ) == 0 ){
+                    ?>
+                    <tr class="empty_grading">
+                        <td colspan="7" class="text-center py-5">
+                            Empty
+                        </td>
+                    </tr>
+                    <?php 
+                        } else {
+
+
+                            foreach( $grading as $card ){
+
+                                $data = json_decode( $card->data, true );
+
+                                if(array_key_exists("file", $data)){
+                                    $img = "<img src='" . ($data["file"]["baseurl"]) . "'>";
+                                } else {
+                                    $img = '<div class="d-flex justify-content-center align-items-center picture_box ebayintegration-btn" data-action="grading_picture_box_click"  data-id="' . $card->id . '"   data-user_id="' . get_current_user_id() . '">' .
+                                            '<i class="fa-solid fa-file-image fa-2x"></i>' . 
+                                        '</div>';
+                                }
+
+                    ?>
+                    <tr class='consigned_item_row' data-id='<?php echo $card->id; ?>'>
+                        <td>
+                            <a class='text-danger  ebayintegration-btn' data-action="removeGradingCardRow"  data-id='<?php echo $card->id ?>' data-user_id="<?php echo get_current_user_id(); ?>" href='#'>
+                                <i class='fa-solid fa-lg fa-xmark'></i>
+                            </a>
+                        </td>
+                        <td><?php echo $data["player"] ?></td>
+                        <td><?php echo $data["year"] ?></td>
+                        <td><?php echo $data["brand"] ?></td>
+                        <td><?php echo $data["card_number"] ?><br><small><?php echo $data["attribute_sn"] ?></small></td>
+                        <td class='text-end'>$<?php echo $data["dv"] ?></td>
+                        <td class='text-end'>$<?php echo $data["per_card"] ?></td>
+                    </tr>
+                    <?php 
+                                $total_grading = $total_grading + $data["per_card"];
+                                $total_dv = $total_dv + $data["dv"];
+
+                            }
+                        }
+                    ?>            
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th colspan='6' class="text-end">Total DV</th>
+                        <th colspan='1' class="text-end">$<?php echo $total_dv ?></th>
+                    </tr>
+                    <tr>
+                        <th colspan='6' class="text-end">Grading Charge</th>
+                        <th colspan='1' class="text-end">$<?php echo $total_grading ?></th>
+                    </tr>
+                    <tr>
+                            <?php 
+                                if(  count( $grading_addon ) > 0 ){
+                                    $checked = "checked";
+                                    $colspan = "4";                                    
+                                } else {
+                                    $checked = "";
+                                    $colspan = "7";
+                                }
+                            ?>
+
+                        <td colspan="<?php echo $colspan ?>" class="bg-success py-3 text-white">
+                            <input <?php echo $checked ?> type="checkbox" id="service" name="service" class="me-3 grading_inspection_checkbox" data-user_id="<?php echo get_current_user_id() ?>" data-type="<?php echo $_GET["type"] ?>" value="inspection_service"><strong class="">Include Inspection Service</strong> (This will be an additional charge of $3 per card)
+                        </td>
+                        <?php 
+                        if( $checked != "" ){
+                        ?>
+                        <th colspan='2' class="text-end bg-success text-white">
+                            Total Inspection Service
+                        </th>
+                        <th colspan='1' class="text-end bg-success text-white">
+                            $<?php echo count($grading) * 3 ?>
+                        </th>
+                        <?php 
+                        }   
+                        ?>
+                    </tr>
+                    <?php 
+                        if( count( $grading_files ) > 0 ){
+                    ?>
+                    <tr>                        
+                        <th colspan="4" class="text-center">
+                            Uploaded Cards List Files
+                        </th>
+                        <th  class="text-center">
+                            Quantity
+                        </th>
+                        <th class="text-center">
+                            Card Show
+                        </th>
+                    </tr>   
+                    <?php 
+                        foreach($grading_files as $file){
+                    ?>
+                        <?php 
+                            $file_data = json_decode($file->data, true);
+                            foreach( $file_data as $fdata ){
+                        ?>
+                        <tr class="grading_file">
+                            <td colspan="4" class="text-left">
+                                <a class="me-3 btn btn-danger btn-sm ebayintegration-btn" data-action="remove_grading_file" data-id="<?php echo $file->id ?>" data-file="<?php echo $fdata["baseurl"] ?>" >REMOVE</a>
+                                <a href="<?php echo $fdata["baseurl"] ?>" target="_blank"><?php echo $fdata["name"] ?></a>
+                            </td>
+                            <td>
+                                <?php echo $fdata["qty"] ?>
+                            </td>                        
+                            <td>
+                                <?php echo $fdata["card_show"] ?>
+                            </td>                        
+                        </tr>
+                        <?php                                 
+                            }
+                        ?>                        
+                    <?php                             
+                        }
+                    ?> 
+                    <?php 
+                        } 
+                    ?> -->
+                </tfoot>        
+            </table>
+        </div>
+
+        <!-- MOBILE VIEW -->
+        <div class="d-lg-none pb-2">
+            <table class="table table-sm table-bordered" id="new_grading_mobile">
+                <thead>
+                    <tr>
+                        <th colspan="2">Items</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $total_dv = 0;
+                        $total_grading = 0;
+
+                        if( count( $grading ) == 0 ){
+                    ?>
+                    <tr class="empty_grading">
+                        <td colspan="2" class="text-center py-5">
+                            Empty
+                        </td>
+                    </tr>
+                    <?php 
+                        } else {
+                            foreach( $grading as $card ){
+
+                            $data = json_decode( $card->data, true );
+
+                            if(array_key_exists("file", $data)){
+                                $img = "<img src='" . ($data["file"]["baseurl"]) . "'>";
+                            } else {
+                                $img = '<div class="d-flex justify-content-center align-items-center picture_box">' .
+                                            '<i class="fa-solid fa-file-image fa-2x"></i>' . 
+                                        '</div>';
+
+                            }   
+
+                    ?>
+                    <tr class='consigned_item_row' data-id='<?php echo $card->id; ?>'>
+                        <td colspan="2">
+                            <div class='w-100 p-0 text-end' style='position: relative;'>
+                                <a class='text-danger ebayintegration-btn' data-action="removeGradingCardRow" data-id='<?php echo $card->id ?>' data-user_id="<?php echo get_current_user_id(); ?>" href='#' style='position: absolute; right: 0px;'>
+                                    <i class='fa-solid fa-xl fa-xmark'></i>
+                                </a>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-sm-4'>Player</div>
+                                <div class='col-sm-8'>                                    
+                                    <?php echo $data["player"] ?>								
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-sm-4'>Year</div>
+                                <div class='col-sm-8'>
+                                    <?php echo $data["year"] ?>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-sm-4'>Brand</div>
+                                <div class='col-sm-8'>
+                                    <?php echo $data["brand"] ?>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-4'>Card #</div>
+                                <div class='col-sm-8'>
+                                    <?php echo $data["card_number"] ?>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-sm-4'>Attribute SN</div>
+                                <div class='col-sm-8'>
+                                    <?php echo $data["attribute_sn"] ?>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-sm-4'>Declared Value</div>
+                                <div class='col-sm-8'>
+                                    $0.00                        
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='small text-secondary col-sm-4'>Grading</div>
+                                <div class='col-sm-8'>
+                                    $0.00
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php
+                            $total_grading = $total_grading + $data["per_card"];
+                            $total_dv = $total_dv + $data["dv"];
+
+                            }
+                        }
+                    ?>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th colspan='1' class="text-end">
+                            Total Inspection Service
+                        </th>
+                        <th colspan='1' class="text-end">
+                            $<?php echo count($grading) * 3 ?>
+                        </th>
+                    </tr>
+
+                    <tr>
+                        <th colspan='1' class="text-end">Total DV</th>
+                        <th colspan='1' class="text-end">$<?php echo $total_dv ?></th>
+                    </tr>
+                    <tr>
+                        <th colspan='1' class="text-end">Grading Charge</th>
+                        <th colspan='1' class="text-end">$<?php echo $total_grading ?></th>
+                    </tr>
+                    <?php 
+                        if( count( $grading_files ) > 0 ){
+                    ?>
+                    <tr>                        
+                        <th colspan="1" class="text-center">
+                            Uploaded Cards List Files
+                        </th>
+                    </tr>   
+                    <?php 
+                        foreach($grading_files as $file){
+                    ?>
+                        <?php 
+                            $file_data = json_decode($file->data, true);
+                            foreach( $file_data as $fdata ){
+                        ?>
+                        <tr class="grading_file">
+                            <td colspan="1" class="text-left">
+                                <a class="me-3 btn btn-danger btn-sm ebayintegration-btn" data-id="<?php echo $file->id ?>" data-action="remove_grading_file" data-file="<?php echo $fdata["baseurl"] ?>" >REMOVE</a>
+                                <a href="<?php echo $fdata["baseurl"] ?>" target="_blank"><?php echo $fdata["name"] ?></a>
+                            </td>
+                        </tr>
+                        <?php                                 
+                            }
+                        ?>                        
+                    <?php                             
+                        }
+                    ?> 
+                    <?php 
+                        } 
+                    ?>
+
+                </tfoot>        
+            </table>    
+        </div>
 
         <!-- TABLE LOWER BUTTONS -->
         <div class="d-flex justify-content-end">
@@ -380,7 +672,7 @@
         </div>
 
 
-        SHIP MODAL
+        <!-- SHIP MODAL -->
         <div class="modal fade ship_batch_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px">
             <div class="modal-dialog modal-lg" style="margin-bottom: 150px;">
                 <div class="modal-content modal-ajax">
@@ -464,7 +756,7 @@
 <!-- GRADING MENU -->
 
 <?php
-    }  else {
+    } else {
 
         if( get_current_user_id() != 0 ){
 
