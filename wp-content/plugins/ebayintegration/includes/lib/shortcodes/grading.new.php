66859cd0f816ed5b1b0da4718ba9d4d961115850
arhizsx <arhizsx@gmail.com>
2024-09-13 17:49:53 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/grading.new.php
@@ -440,13 +440,317 @@
         </div>
 
 
+        <!-- LOG MODAL -->
+        <div class="modal fade log_grading_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px;">
+            <div class="modal-dialog" id="dxmodal">
+                <div class="modal-content modal-ajax">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 class="modal-title">
+                            Log Card to Grade
+                        </h5>
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
+                            X
+                        </button>
+                    </div>
+                    <div class="modal-body py-2 px-3">
+                        <div class="row formbox">
+
+                            <input type="hidden" name="user_id" value="<?php echo get_current_user_id(); ?>">
+                            <input type="hidden" name="grading_type" value="<?php echo $_GET["type"]; ?>">
+
+                            <div class="col-md-12">
+                                <label>Grading</label>
+                                <input type="text" name="grading" class="form-control p-1" value="<?php echo $grading_title; ?>" disabled>
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Grading Charge Per Card</label>
+                                <input type="text" name="per_card" class="form-control p-1" value="<?php echo $per_card; ?>" disabled>
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Max Declared Value</label>
+                                <input type="text" name="max_dv" class="form-control p-1" value="<?php echo $max_dv; ?>" disabled>
+                            </div>
+                            <div class="col-6">                                                        
+                                <label>Qty</label>
+                                <input type="number" name="quantity" class="form-control checker" data-checker="required" value="">
+                            </div>
+                            
+                            <div class="col-6">
+                                <label>Year</label>
+                                <input type="number" name="year" class="form-control checker" data-checker="required" value="">
+                            </div>
+                            <div class="col-md-12">
+                                <label>Brand</label>
+                                <input type="text" name="brand" class="form-control p-1 checker" data-checker="required" value="">
+                            </div>
+                            <div class="col-md-12">
+                                <label>Player Name</label>
+                                <input type="text" name="player" class="form-control p-1 checker" data-checker="required" value="">
+                            </div>
+                            <div class="col-sm-12">
+                                <label>Card Number</label>
+                                <input type="text" name="card_number" class="form-control p-1 checker" data-checker="optional" value="">
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Attribute S/N</label>
+                                <input type="text" name="attribute_sn" class="form-control p-1 checker" data-checker="optional" value="">
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Declared Value</label>
+                                <input type="text" name="dv" class="form-control p-1 checker" data-checker="required" value="">
+                            </div>
+                        </div>
+                        <div class="d-none p-5 text-center loading">
+
+                            Adding card, please wait...
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn border btn-secondary" data-bs-dismiss="modal" >Close</button>
+
+                        <button class="btn border btn-success ebayintegration-btn" 
+                            data-action='confirmAddGrading' 
+                        >
+                            Log Card
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- IMPORT MODAL -->
+        <div class="modal fade import_grading_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px;">
+            <div class="modal-dialog" id="dxmodal">
+                <div class="modal-content modal-ajax">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 class="modal-title">
+                            Upload Cards List File
+                        </h5>
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
+                            X
+                        </button>
+                    </div>
+                    <div class="modal-body py-2 px-3">
+                        <div class="row formbox">
+                            <form class="form" id="photo_upload_form">
+                                <div class="row">
+                                    <input type="hidden" name="user_id" value="<?php echo get_current_user_id() ?>">
+                                    <input type="hidden" name="action" value="confirmUploadGradingFile">
+                                    <input type="hidden" name="type" value="<?php echo $_GET["type"] ?>">
+
+                                    <div class="col-12">
+                                        <label>Select Cards List File</label>
+                                        <input type="file" name="import_file" class="form-control mb-3 checker" data-checker="required"  accept="image/png,  image/jpeg, .csv, .pdf, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet ">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label>Quantity of Cards</label>
+                                        <input type="number" name="qty" class="form-control p-1 checker" data-checker="required">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label>Card Show Name</label>
+                                        <select name="card_show" class="form-control p-1 checker" data-checker="required">
+                                            <option selected value="NO SHOW">NO SHOW</option>
+                                            <option value="Madison">Madison</option>
+                                            <option value="Janesville">Janesville</option>
+                                            <option value="Rockford">Rockford</option>
+                                            <option value="Kane County">Kane County</option>
+                                            <option value="Dupage">Dupage</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-12 mt-4">
+                                        Accepted formats PNG, JPG, CSV, PDF, XLSX
+                                    </div>
+                                    <div class="col-12">
+                                        Once our team process your list this order will be updated.
+                                    </div>
+                                    <div class="col-12  mb-3">
+                                        You can upload multiple list and it will be consolidated in this order.
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="d-none p-5 text-center loading">
+
+                            Uploading cards list, please wait...
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn border btn-secondary" data-bs-dismiss="modal" >Close</button>
+
+                        <button class="btn border btn-success ebayintegration-btn" 
+                            data-action='confirmUploadGradingFile' 
+                        >
+                            Upload File
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- CLEAR MODAL -->
+        <div class="modal fade clear_grading_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px;">
+            <div class="modal-dialog" id="dxmodal">
+                <div class="modal-content modal-ajax">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 class="modal-title">
+                            Clear Logged Cards
+                        </h5>
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
+                            X
+                        </button>
+                    </div>
+                    <div class="modal-body py-2 px-3">
+                        <div class="row formbox">
+                            <div class="col-12 p-5">
+                                Are you sure want to remove all logged cards?
+                            </div>
+                        </div>
+                        <div class="d-none p-5 text-center loading">
+
+                            Clearing cards list, please wait...
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn border btn-secondary" data-bs-dismiss="modal" >Cancel</button>
+
+                        <button class="btn border btn-danger ebayintegration-btn" 
+                            data-action='confirmGradingTableClearList'  data-user_id="<?php echo get_current_user_id(); ?>" data-grading_type="<?php echo $_GET["type"]; ?>"
+                        >
+                            Yes
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- CHECKOUT MODAL -->
+        <div class="modal fade checkout_grading_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px;">
+            <div class="modal-dialog" id="dxmodal">
+                <div class="modal-content modal-ajax">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 class="modal-title">
+                            Checkout
+                        </h5>
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
+                            X
+                        </button>
+                    </div>
+                    <div class="modal-body py-2 px-3">
+                        <div class="row formbox">
+                            <div class="col-12 p-5">
+                                Are you sure want to checkout the logged cards?
+                            </div>
+                        </div>
+                        <div class="d-none p-5 text-center loading">
+
+                            Checking Out, please wait...
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn border btn-secondary" data-bs-dismiss="modal" >Cancel</button>
+                        <button class="btn border btn-primary ebayintegration-btn" data-action='confirmGradingTableCheckout'  data-user_id="<?php echo get_current_user_id(); ?>" data-grading_type="<?php echo $_GET["type"]; ?>" >
+                            Yes
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+        <!-- SHIP MODAL -->
+        <div class="modal fade ship_batch_modal" tabindex="-1" role="dialog" aria-labelledby="dxmodal" aria-hidden="true"  data-backdrop="static" data-bs-backdrop="static"   data-bs-keyboard="false" data-data='' data-modal='' data-key='' data-modal_size='full' style="margin-top: 120px">
+            <div class="modal-dialog modal-lg" style="margin-bottom: 150px;">
+                <div class="modal-content modal-ajax">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 class="modal-title">
+                            Ship Cards
+                        </h5>
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close">
+                            X
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="formbox">
+
+                            <div class="row">
+                                <div class="col">
+                                    <H5 style="color: black;">Ship your items to</H5>
+                                </div>
+                            </div>
+                            <div class="row border-bottom mb-3">
+                                <div class="col-lg-6 small pb-3">
+                                    <div>USPS</div>
+                                    <div>Matt Sellers</div>
+                                    <div>PO Box 263 Hartland, WI 53029</div>
+                                </div>
+                                <div class="col-lg-6 small pb-3">
+                                    <div>FedEx / UPS / DHL</div>
+                                    <div>Matt Sellers</div>
+                                    <div>203 E Wisconsin Ave Suite 203C Oconomowoc, WI 53066</div>
+                                </div>
+                            </div>
+                            
+                            <forn id="shipping_info_form">
+
+                                    <input type="hidden" name="user_id" value="<?php echo get_current_user_id() ?>">
+                                    <div class="row">
+                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
+                                            <label for="carrier">Carrier</label>
+                                            <select name="carrier" class="form-control" data-field_check="required">
+                                                <option value="">Select Carrier</option>
+                                                <option value="USPS">USPS</option>
+                                                <option value="FedEx">FedEx</option>
+                                                <option value="DHL">DHL</option>
+                                                <option value="UPS">UPS</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
+                                            <label for="shipped_by">Shipped By</label>
+                                            <input type="text" name="shipped_by" class="form-control p-2" data-field_check="required">
+                                        </div>
+                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
+                                            <label for="tracking_number">Tracking Number</label>
+                                            <input type="text" name="tracking_number" class="form-control p-2" data-field_check="required">
+                                        </div>
+                                        <div class="col-xl-6 col-lg-6 col-md-12 mb-3">
+                                            <label for="shipping_date">Shipping Date</label>
+                                            <input type="date" name="shipping_date" class="form-control p-2" data-field_check="required">
+                                        </div>
+                                    </div>
+                            </forn>
+                        </div>
+                        <div class="d-none p-5 text-center loading">
+
+                            Processing cards, please wait...
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn border btn-secondary" data-bs-dismiss="modal" >Close</button>
+
+                        <button class="btn border btn-primary ebayintegration-btn" 
+                            data-action='confirmConsignCardsShipping' 
+                        >
+                            Card Shipped
+                        </button>                    
+                    </div>
+                </div>
+            </div>
+        </div>
 
 <!-- GRADING MENU -->
 
 <?php
     } else {
-
-        if( get_current_user_id() != 0 ){
 
             if( ! isset( $_GET["grader"] ) || $_GET["grader"] == "psa"  ){
 ?>
@@ -582,7 +886,7 @@
         </div>
     </div>
 <?php
-        } 
+            } 
             elseif( isset( $_GET["grader"] ) && $_GET["grader"] == "sgc"  ){
 ?>
     <div class="row mx-3">
@@ -616,12 +920,6 @@
     </div>
 <?php    
             }
-        } else {
-?>
-    <H1 style="color: black;">Coming Soon</H1>
-    <a href="/grading">Log Cards for Grading</a>
-<?php 
-        }
     }
 ?>
 
