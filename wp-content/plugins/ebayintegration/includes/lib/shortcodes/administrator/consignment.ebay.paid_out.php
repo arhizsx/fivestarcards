fbdc2d80e9a047017367c5982c4d7f1ca3c2dbff
arhizsx <arhizsx@gmail.com>
2024-09-15 16:07:30 +0800

    routes

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.paid_out.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/administrator/consignment.ebay.paid_out.php
@@ -99,11 +99,6 @@ $users = get_users( $args );
                             <?php print_r( $title ); ?>
                         </a>
                     </div>
-                    <div class="sku text-small">SKU: <?php echo $item->sku ?></div>
-                    <div class="item_id text-small">Item ID: <?php echo $item->item_id ?></div>
-                    <?php $listing = $data["Item"]["ListingType"] == "Chinese" ? "Auction" : $data["Item"]["ListingType"]; ?>
-                    <div class="item_id text-small">Listing Type: <?php echo $listing; ?></div>                    
-                    <div class="item_id text-small">ID: <?php echo $item->id ?></div>                    
 
                     
                 </td>       
