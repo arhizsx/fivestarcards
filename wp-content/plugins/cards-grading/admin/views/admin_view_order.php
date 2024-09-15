f08f2d3c3a2e648e2f64c133d06bf8a80fa4b440
arhizsx <arhizsx@gmail.com>
2024-09-15 10:37:06 +0800

    routes

--- wp-content/plugins/cards-grading/admin/views/admin_view_order.php
+++ wp-content/plugins/cards-grading/admin/views/admin_view_order.php
@@ -468,7 +468,7 @@ if( $grading_order_id > 0 ){
                 <tr class='admin-graded-row' data-post_id='" + post_id + "'>
                     <td><?php echo $post->ID ?></td>
                     <td><input type="text" class="form-control" value="<?php echo $db_row_data["title"] ?>"></td>
-                    <td><input type="text" class="form-control" value="<?php echo $db_row_data["grade"] ?>"></td>
+                    <td><input type="text" class="" value="<?php echo $db_row_data["grade"] ?>"></td>
                     <td><a href='<?php echo $db_row_data["certImgFront"] ?>'><img width='100px' src='<?php echo $db_row_data["certImgFront"] ?>' target='_blank'></a><a href='<?php echo $db_row_data["certImgBack"] ?>' target='_blank'><img width='100px' src='<?php echo $db_row_data["certImgBack"] ?>'></a></td>
                     <td><?php echo $db_row_data["certificate_number"] ?></td>
                 </tr>
