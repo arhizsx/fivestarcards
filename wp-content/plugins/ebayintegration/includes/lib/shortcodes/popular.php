ac05e2dffb83b0e41891683e015c3efa1c53b4bb
arhizsx <arhizsx@gmail.com>
2024-09-29 03:27:48 +0800

    Hot

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/popular.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/popular.php
@@ -54,14 +54,14 @@ LIMIT 6;
                                 <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $item->GalleryURL ?>">
                             </a>
                         </div>
-                        <div class="px-2 mt-2 d-flex justify-content-between" style="font-size: 12px; color: black; font-weight:bold;">
-                            <div>Bids: <?php echo $item->BidCount; ?></div>
-                            <div class="text-end">$<?php echo $item->CurrentPrice; ?></div>
-                        </div>
                         <div class="itemTitle">
                             <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
                                 <?php echo $item->Title; ?>
                             </a>
+                        </div>
+                        <div class="px-2 mt-2 d-flex justify-content-between" style="font-size: 12px; color: black; font-weight:bold;">
+                            <div>Bids: <?php echo $item->BidCount; ?></div>
+                            <div class="text-end">$<?php echo $item->CurrentPrice; ?></div>
                         </div>
                     </div>
                 </div>
@@ -89,14 +89,14 @@ LIMIT 6;
                                 <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $item->GalleryURL ?>">
                             </a>
                         </div>
-                        <div class="px-2 mt-2 d-flex justify-content-between" style="font-size: 12px; color: black; font-weight:bold;">
-                            <div>Watchers: <?php echo $item->WatchCount; ?></div>
-                            <div class="text-end">$<?php echo $item->CurrentPrice; ?></div>
-                        </div>
                         <div class="itemTitle">
                             <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
                                 <?php echo $item->Title; ?>
                             </a>
+                        </div>
+                        <div class="px-2 mt-2 d-flex justify-content-between" style="font-size: 12px; color: black; font-weight:bold;">
+                            <div>Watchers: <?php echo $item->WatchCount; ?></div>
+                            <div class="text-end">$<?php echo $item->CurrentPrice; ?></div>
                         </div>
                     </div>
                 </div>
