fa271e613573f4a77d4bca05c2b589ea0e4ad093
arhizsx <arhizsx@gmail.com>
2024-09-28 11:06:59 +0800

    Hot

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.auction.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.auction.php
@@ -107,7 +107,7 @@ $hot = $this->wpdb->get_results ( "
     foreach($hot as $item){ 
     ?>
     <div class="col-md-3">
-        <img width="100%" src="<?php echo $item->GalleryURL ?>">
+        <img width="50%" src="<?php echo $item->GalleryURL ?>">
     </div>
     <?php 
     }
