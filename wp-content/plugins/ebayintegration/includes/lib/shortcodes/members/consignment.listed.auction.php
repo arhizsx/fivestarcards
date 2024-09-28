32eed62dcc4684632069a01fc1109f8e103a0235
arhizsx <arhizsx@gmail.com>
2024-09-28 11:11:48 +0800

    Hot

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.auction.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.auction.php
@@ -108,8 +108,8 @@ $hot = $this->wpdb->get_results ( "
     ?>
     <div class="col-md-3 mb-3 text-center">
         <img height="80px" src="<?php echo $item->GalleryURL ?>">
-        <div><?php echo $item->Title; ?></div>
         <div><?php echo $item->CurrentPrice; ?></div>
+        <div><?php echo $item->Title; ?></div>
     </div>
     <?php 
     }
