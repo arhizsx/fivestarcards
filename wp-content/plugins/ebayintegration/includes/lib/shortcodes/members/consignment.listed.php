31552d4c39ce52c4a86bcfc88d81a0488a7f69d0
arhizsx <arhizsx@gmail.com>
2024-09-28 11:30:57 +0800

    Hot

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/members/consignment.listed.php
@@ -56,8 +56,8 @@ LIMIT 6;
             foreach($hot as $item){ 
             ?>
             <div class="col-md-2 mb-3 text-center">
-                <div class="height: 80px;">
-                    <img style="margin-top: auto; margin-bottom: auto; height: 80px;" src="<?php echo $item->GalleryURL ?>">
+                <div class="height: 120px;">
+                    <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $item->GalleryURL ?>">
                 </div>
                 <div>$<?php echo $item->CurrentPrice; ?></div>
                 <div style="font-size: 10px;">
