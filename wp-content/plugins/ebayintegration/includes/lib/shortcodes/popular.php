0a7b8fcc8a7fa50b49867986d695d87a93a6cac7
arhizsx <arhizsx@gmail.com>
2024-10-21 15:05:09 +0800

    Store

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/popular.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/popular.php
@@ -47,12 +47,23 @@ LIMIT 8;
             <div class="row">
                 <?php 
                 foreach($hot as $item){ 
+
+                    if( str_contains( $item->GalleryURL, ".webp" )){
+                        $img = str_replace("s-l140.webp","s-l600.webp", $item->GalleryURL);
+                    }
+                    elseif( str_contains( $item->GalleryURL, ".jpg" )){
+                        $img = str_replace("s-l140.jpg","s-l600.jpg", $item->GalleryURL);                        
+                    }
+                    elseif( str_contains( $item->GalleryURL, ".png" )){
+                        $img = str_replace("s-l140.png","s-l600.png", $item->GalleryURL);                        
+                    }
+
                 ?>
                 <div class="d-flex align-items-start col-md-3 col-sm-6 mb-3 text-center">
                     <div>
                         <div style="height: 200px; min-height: 200px; max-height: 200px;">
                             <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
-                                <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $item->GalleryURL ?>">
+                                <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $img ?>">
                             </a>
                         </div>
                         <div class="itemTitle">
@@ -83,12 +94,23 @@ LIMIT 8;
             <div class="row">
                 <?php 
                 foreach($popular as $item){ 
+
+                    if( str_contains( $item->GalleryURL, ".webp" )){
+                        $img = str_replace("s-l140.webp","s-l600.webp", $item->GalleryURL);
+                    }
+                    elseif( str_contains( $item->GalleryURL, ".jpg" )){
+                        $img = str_replace("s-l140.jpg","s-l600.jpg", $item->GalleryURL);                        
+                    }
+                    elseif( str_contains( $item->GalleryURL, ".png" )){
+                        $img = str_replace("s-l140.png","s-l600.png", $item->GalleryURL);                        
+                    }
+
                 ?>
                 <div class="d-flex align-items-start col-md-3 col-sm-6 mb-3 text-center">
                     <div>
                         <div style="height: 200px; min-height: 200px; max-height: 200px;">
                             <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
-                                <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $item->GalleryURL ?>">
+                                <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $img ?>">
                             </a>
                         </div>
                         <div class="itemTitle">
