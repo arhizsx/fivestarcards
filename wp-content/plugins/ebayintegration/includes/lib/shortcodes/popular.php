730174dda65408f8bbe0e4212acfe336045fea57
arhizsx <arhizsx@gmail.com>
2024-10-25 19:03:45 +0800

    shop

--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/popular.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/popular.php
@@ -42,13 +42,12 @@ LIMIT 8;
                 <div class="col">
                     <H1 style="color: black; margin-bottom: 0px;">Hot eBay Auctions</H1>
                     <p>You will be redirected to an ebay listing</p>
-
+                    <div style="font-size: 9px; gray; text-center">
+                    Disclaimer: This site contains links that may result in a small commission if purchases are made through them. These links help support the content provided, at no additional cost to you. Thank you for your support.
+                    </div>                
                 </div>
             </div>
             <div class="row">
-                <div class="col" style="font-size: 9px; gray; text-center">
-                Disclaimer: This site contains links that may result in a small commission if purchases are made through them. These links help support the content provided, at no additional cost to you. Thank you for your support.
-                </div>                
             </div>
             <div class="row">
                 <?php 
@@ -68,12 +67,12 @@ LIMIT 8;
                 <div class="d-flex align-items-start col-md-3 col-sm-6 mb-3 text-center">
                     <div>
                         <div style="height: 200px; min-height: 200px; max-height: 200px;">
-                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
+                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339086187&customid=popular&toolid=10001&mkevt=1" target="_blank">
                                 <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $img ?>">
                             </a>
                         </div>
                         <div class="itemTitle">
-                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
+                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339086187&customid=popular&toolid=10001&mkevt=1" target="_blank">
                                 <?php echo $item->Title; ?>
                             </a>
                         </div>
@@ -95,12 +94,12 @@ LIMIT 8;
                 <div class="col">
                     <H1 style="color: black; margin-bottom: 0px;">Popular eBay Items</H1>
                     <p>You will be redirected to an ebay listing</p>
+                    <div style="font-size: 9px; gray; text-center">
+                    Disclaimer: This site contains links that may result in a small commission if purchases are made through them. These links help support the content provided, at no additional cost to you. Thank you for your support.
+                    </div>                
                 </div>
             </div>
             <!-- <div class="row">
-                <div class="col" style="font-size: 9px; gray; text-center">
-                Disclaimer: This site contains links that may result in a small commission if purchases are made through them. These links help support the content provided, at no additional cost to you. Thank you for your support.
-                </div>                
             </div> -->
             <div class="row">
                 <?php 
@@ -120,12 +119,12 @@ LIMIT 8;
                 <div class="d-flex align-items-start col-md-3 col-sm-6 mb-3 text-center">
                     <div>
                         <div style="height: 200px; min-height: 200px; max-height: 200px;">
-                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
+                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339086187&customid=popular&toolid=10001&mkevt=1" target="_blank">
                                 <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $img ?>">
                             </a>
                         </div>
                         <div class="itemTitle">
-                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339081621&customid=&toolid=10001&mkevt=1" target="_blank">
+                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339086187&customid=popular&toolid=10001&mkevt=1" target="_blank">
                                 <?php echo $item->Title; ?>
                             </a>
                         </div>
--- wp-content/plugins/ebayintegration/includes/lib/shortcodes/store-auction.php
+++ wp-content/plugins/ebayintegration/includes/lib/shortcodes/store-auction.php
@@ -19,15 +19,15 @@ $hot = $this->wpdb->get_results ( "
 </style>
 <div class="container-fluid px-3">
     <div class="row">
-        <!-- <div class="col" style="font-size: 9px; gray; text-center">
-        Disclaimer: This site contains links that may result in a small commission if purchases are made through them. These links help support the content provided, at no additional cost to you. Thank you for your support.
-        </div>                 -->
     </div>
     <div class="row">
         <div class="col">
             <div class="row">
                 <div class="col">
                     <H1 style="color: black; margin-bottom: 0px;">Hot eBay Auctions</H1>
+                    <div style="font-size: 9px; gray; text-center">
+                    Disclaimer: This site contains links that may result in a small commission if purchases are made through them. These links help support the content provided, at no additional cost to you. Thank you for your support.
+                    </div>                
                 </div>
             </div>
             <div class="row">
@@ -48,8 +48,9 @@ $hot = $this->wpdb->get_results ( "
                 ?>
                 <div class="d-flex align-items-start col-md-3 col-sm-6 mb-3 text-center">
                     <div>
+
                         <div style="height: 200px; min-height: 200px; max-height: 200px;">
-                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339085460&customid=store&toolid=10001&mkevt=1" target="_blank">
+                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339086184&customid=auction&toolid=10001&mkevt=1" target="_blank">
                                 <img style="margin-top: auto; margin-bottom: auto; height: 100%;" src="<?php echo $img ?>">
                             </a>
                         </div>
@@ -58,7 +59,7 @@ $hot = $this->wpdb->get_results ( "
                             <div class="text-end">$<?php echo $item->CurrentPrice; ?></div>
                         </div>
                         <div class="itemTitle">
-                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339085460&customid=store&toolid=10001&mkevt=1" target="_blank">
+                            <a href="<?php echo $item->ViewItemURL ?>?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339086184&customid=auction&toolid=10001&mkevt=1" target="_blank">
                                 <?php echo $item->Title; ?>
                             </a>
                         </div>
